5be6aef1d24ce2fa86de5abfd1c12898c440d9fe
zobo101 <zobo101@163.com>
2023-07-20 17:39:11 +0800

    ip整理

--- app/src/main/java/com/xcjh/app/net/ApiComService.kt
+++ app/src/main/java/com/xcjh/app/net/ApiComService.kt
@@ -1,10 +1,8 @@
 package com.xcjh.app.net
 
-import com.xcjh.base_lib.bean.ApiArticleRes
-import com.xcjh.base_lib.bean.ApiResponse
 import com.xcjh.app.bean.*
+import com.xcjh.base_lib.bean.ApiResponse
 import okhttp3.MultipartBody
-import org.json.JSONObject
 import retrofit2.http.*
 
 /**
@@ -13,16 +11,17 @@ import retrofit2.http.*
 interface ApiComService {
 
     companion object {
-        //test
+        //dev
         const val SERVER_URL_CENTER = "http://192.168.101.3:9488/center/apis/"//中台测试
         const val SERVER_URL = "http://192.168.101.7:6001/apis/"//app通用 开发
-
-        // const val SERVER_URL = "https://test.holdem.news/apis/"//app通用 测试
+        //test
+//         const val SERVER_URL_CENTER =  "https://www.2web3.net/test-user-center/center/apis/"//中台测试
+//         const val SERVER_URL = "https://test.holdem.news/apis/"//app通用 测试
         //正式
-        /* const val SERVER_URL_CENTER = "https://www.2web3.net/user-center/center/apis/"//中台正式
-         const val SERVER_URL = "https://holdem.news/apis/"//app通用 正式*/
+//         const val SERVER_URL_CENTER = "https://www.2web3.net/user-center/center/apis/"//中台正式
+//         const val SERVER_URL = "https://holdem.news/apis/"//app通用 正式
+
         const val SERVER_URL_EMAIL = "https://www.2web3.net/user-user/"//邮箱验证
-        //const val SERVER_URL = "http://192.168.102.39:50000/chatgenius/"
     }
 
     /**
