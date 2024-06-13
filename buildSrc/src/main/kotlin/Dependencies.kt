71b87296fad33de4d6f39c40ab6aa7a69e264dce
baoyuedong <baoyuedong@luxury.com>
2024-06-13 17:33:29 +0800

    新的game_sdk_new模块

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,27 @@
+object Versions {
+    const val minSdk = 24
+    const val targetSdk = 33
+    const val compileSdk = 33
+}
+
+object Libs {
+    const val core_ktx = "androidx.core:core-ktx:1.8.0"
+    const val appcompat = "androidx.appcompat:appcompat:1.6.1"
+    const val material = "com.google.android.material:material:1.5.0"
+    const val junit = "junit:junit:4.13.2"
+    const val ext_junit = "androidx.test.ext:junit:1.1.3"
+    const val espresso_core = "androidx.test.espresso:espresso-core:3.4.0"
+    const val immersionbar = "com.geyifeng.immersionbar:immersionbar:3.2.2"
+    const val blankj = "com.blankj:utilcodex:1.31.1"
+    const val viewModelScope = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1"
+    const val lifecyleScope = "androidx.lifecycle:lifecycle-runtime-ktx:2.5.1"
+    const val androidx_startup = "androidx.startup:startup-runtime:1.1.1"
+    const val MMKV = "com.tencent:mmkv:1.0.23"
+    const val google_gson = "com.google.code.gson:gson:2.8.9"
+
+
+    //阿里路由框架
+    const val arouter_api = "com.alibaba:arouter-api:1.4.1"
+    const val arouter_compiler = "com.alibaba:arouter-compiler:1.2.2"
+
+}
