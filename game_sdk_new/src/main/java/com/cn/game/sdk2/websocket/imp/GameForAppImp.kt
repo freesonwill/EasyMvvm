8b7378d859c0f1940f94b0aeedeee07cc106b2af
mac-name <mac_name@gmail.com>
2024-06-19 17:39:45 +0800

    websocket-修改app调用方法

--- game_sdk_new/src/main/java/com/cn/game/sdk2/websocket/imp/GameForAppImp.kt
+++ game_sdk_new/src/main/java/com/cn/game/sdk2/websocket/imp/GameForAppImp.kt
@@ -57,6 +57,8 @@ object GameSDK : IGameForApp {
     override fun enterLive(liveId: String, gameIds: List<Int>, data: String) {
         val req = GameReq.EnterGroup.newBuilder()
         var index = 0
+        var index1 = 0
+        var index2 = 0
         gameIds.forEach {
             req.setMiniGameIds(index, it)
             index++
