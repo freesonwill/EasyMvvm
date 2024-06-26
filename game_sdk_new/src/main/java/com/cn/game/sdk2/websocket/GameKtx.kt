6232a0c50da4e198460216b22acc9cb58b64c973
mac-name <mac_name@gmail.com>
2024-06-26 17:24:14 +0800

    websocket-修改重练

--- game_sdk_new/src/main/java/com/cn/game/sdk2/websocket/GameKtx.kt
+++ game_sdk_new/src/main/java/com/cn/game/sdk2/websocket/GameKtx.kt
@@ -61,10 +61,9 @@ var WEB_SOCKET_URL = "wss://ws.qxe68.com:7001/api/game/5702" ///test
 /**
  * 仅记录用户当前状态，用于重连服务器处理
  */
-//92:ZyBmhNCJ   87:MHxIHlYM  93:Ufx3Dy8y 94:0aPEwiYK
-var token = "92:ZyBmhNCJ"
 var isLogin: Boolean = false
-var isEnterRoom = false;
+var isEnterRoom = false
+var isCanReconnect = true
 /**
  * data层使用，view不管
  */
--- game_sdk_new/src/main/java/com/cn/game/sdk2/websocket/GameSocketManager.kt
+++ game_sdk_new/src/main/java/com/cn/game/sdk2/websocket/GameSocketManager.kt
@@ -69,7 +69,7 @@ class GameSocketManager private constructor() : OnMessageListener {
                             if (it.readyState == ReadyState.OPEN) {
                                 gameMassageManager?.ping()
                             }//正常发送心跳
-                            if (it.isClosed) it.re()
+                            if (it.isClosed && isCanReconnect) it.re()
                         }
                     } else {
                         client?.let {
