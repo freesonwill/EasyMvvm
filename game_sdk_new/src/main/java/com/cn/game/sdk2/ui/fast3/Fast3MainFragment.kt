fff89526a7870b17b7c0f3f3d882626e33c3fb3c
111 <huang0818vip@>
2024-07-03 16:20:41 +0800

    下注ok按钮声音修改、续压加倍按钮点击修改

--- game_sdk_new/src/main/java/com/cn/game/sdk2/ui/fast3/Fast3MainFragment.kt
+++ game_sdk_new/src/main/java/com/cn/game/sdk2/ui/fast3/Fast3MainFragment.kt
@@ -855,9 +855,9 @@ class Fast3MainFragment : BaseVmDbFragment<Fast3ViewModel, FragFast3HomeBinding>
                 }
             }
             //加倍
-            ivMultiple2.clickNoRepeat {
+            ivMultiple2.setOnClickListener {
                 if (gameAboutModel.currentAgainDoubleState.value != GameAboutModel.AgainDoubleState.DOUBLE) {
-                    return@clickNoRepeat
+                    return@setOnClickListener
                 }
                 PromptSoundPlay.btnPlayMedia()
                 GameSocketManager.getInstance()?.getGameService()
@@ -884,9 +884,9 @@ class Fast3MainFragment : BaseVmDbFragment<Fast3ViewModel, FragFast3HomeBinding>
                     }
             }
             //续压
-            ivXuya.clickNoRepeat {
+            ivXuya.setOnClickListener {
                 if (gameAboutModel.currentAgainDoubleState.value != GameAboutModel.AgainDoubleState.AGAIN) {
-                    return@clickNoRepeat
+                    return@setOnClickListener
                 }
                 PromptSoundPlay.btnPlayMedia()
                 val map = GameSocketManager.getInstance()?.getGameService()?.againBetting()
