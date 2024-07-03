70671dc2f010be11c19b23d46e33b0fb918a3313
xcjhskill <1221312@132>
2024-07-03 19:51:00 +0800

    细节优化

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -26,29 +26,52 @@ class MainActivity : AppCompatActivity() {
         var llshow = findViewById<LinearLayout>(R.id.llshow)
         var btnXiu = findViewById<Button>(R.id.btnXiu)
 
+        GameApp.setSocketStatesCallback(object : GameApp.SocketStatesCallback{
+            override fun onOpen() {
+                btnOpen.post{
+                    btnOpen.text = "服务器连接成功,点击登录"
+                }
+            }
+
+            override fun onClose(isNeedReconnect: Boolean) {
+                btnOpen.post{
+                    if(isNeedReconnect){
+                        btnOpen.text = "正在重新连接服务器"
+                    }else{
+                        btnOpen.text = "token失效"
+                        btnOpen.isClickable = true
+                    }
+                }
+            }
+        })
         btnOpen.setOnClickListener {
             //MyGameManager.showFastView(this)
             //ViewHelper.showFastView(this)
-
+            btnOpen.isClickable = false;
             if(gameAboutModel.isLoginSuccess.value == true){
-                GameApp.enterLive("1213", listOf(1), "")
-                ViewHelper.showFastView(this)
-                ViewHelper.showFastViewOverlay(this)
+
             }else{
                 //92:ZyBmhNCJ   87:MHxIHlYM
                 GameApp.login(
                     token, "wali-internal", true
                 )
+                btnOpen.text = "正在登录"
             }
         }
         gameAboutModel.isLoginSuccess.observe(this){result->
             if(result){
                 btnOpen.text = "进入直播间"
+            }else{
+                btnOpen.isClickable = true
+                btnOpen.text = "登录失败"
             }
         }
         gameAboutModel.isEnterGroup.observe(this){result->
             if(result){
                 btnOpen.text = "已进入直播间"
+                //GameApp.enterLive("1213", listOf(1), "")
+                ViewHelper.showFastView(this)
+                ViewHelper.showFastViewOverlay(this)
             }
         }
 
