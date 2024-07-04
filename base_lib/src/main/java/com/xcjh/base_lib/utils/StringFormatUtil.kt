6f24a4e6a78aae9e9106616b0044a4a0a170af9a
xcjhskill <1221312@132>
2024-07-04 18:22:02 +0800

    开奖动画，余额动画更新

--- base_lib/src/main/java/com/xcjh/base_lib/utils/StringFormatUtil.kt
+++ base_lib/src/main/java/com/xcjh/base_lib/utils/StringFormatUtil.kt
@@ -8,9 +8,12 @@ import java.util.Locale
 class StringFormatUtil {
     companion object{
         val decimalFormatMax2 : DecimalFormat = DecimalFormat("#0.##", DecimalFormatSymbols.getInstance(Locale.ENGLISH))
+        val decimalFormat2 : DecimalFormat = DecimalFormat("#0.00", DecimalFormatSymbols.getInstance(Locale.ENGLISH))
+
     }
 
     init {
         decimalFormatMax2.roundingMode = RoundingMode.FLOOR
+        decimalFormat2.roundingMode = RoundingMode.FLOOR
     }
 }
