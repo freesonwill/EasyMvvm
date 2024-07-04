8d74acb53054d78fc259dd78034f52aa2fa19e49
xcjhskill <1221312@132>
2024-07-04 14:20:10 +0800

    资源替换

--- game_sdk_new/src/main/java/com/cn/game/sdk2/websocket/GameKtx.kt
+++ game_sdk_new/src/main/java/com/cn/game/sdk2/websocket/GameKtx.kt
@@ -70,8 +70,9 @@ var isEnableSound = true
 /**
  * 仅记录用户当前状态，用于重连服务器处理
  */
+//测试打包专用 99:mFGB4ljy
 //92:ZyBmhNCJ   87:MHxIHlYM  93:Ufx3Dy8y 94:0aPEwiYK   金额少：97:QwM8xr0x
-var token = "87:MHxIHlYM"
+var token = "99:mFGB4ljy"
 var isLogin = false
 var isEnterRoom = false
 
