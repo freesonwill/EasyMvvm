10c699cfb9732eec161887ae458ec835640243e2
zhangsan <zhangsan@guesswhat.com>
2024-07-09 18:54:48 +0800

    fixbug: 1.筹码左右增加阴影

--- game_sdk_new/src/main/java/com/cn/game/sdk2/ui/fast3/Fast3MainFragment.kt
+++ game_sdk_new/src/main/java/com/cn/game/sdk2/ui/fast3/Fast3MainFragment.kt
@@ -357,6 +357,7 @@ class Fast3MainFragment : BaseVmDbFragment<Fast3ViewModel, FragFast3HomeBinding>
             //开奖时取消临时下注的
             mDatabind.apply {
                 txtHomeStatic.text = getString(R.string.g_f3_dealing)
+                return@apply
                 //隐藏筹码牌动画
                 startBetteRecyclerShowOrHideAnim(isShow = false, onEnd = {
                     //注区
@@ -777,7 +778,6 @@ class Fast3MainFragment : BaseVmDbFragment<Fast3ViewModel, FragFast3HomeBinding>
                 }
             }.models = mViewModel.noteList
         }
-
         //历史结果
         mDatabind.rvHomeHistory.apply {
             itemAnimator = null
