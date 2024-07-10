d5415c00308616408eedcccaffe7f8b8e15fcc43
zhangsan <zhangsan@guesswhat.com>
2024-07-10 18:45:17 +0800

    fixbug： 1.toast动画

--- game_sdk_new/src/main/java/com/cn/game/sdk2/ui/view/Fast3Toast.kt
+++ game_sdk_new/src/main/java/com/cn/game/sdk2/ui/view/Fast3Toast.kt
@@ -1,13 +1,22 @@
 package com.cn.game.sdk2.ui.view
 
+import android.animation.Animator
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.animation.ValueAnimator
 import android.content.Context
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.RelativeLayout
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.animation.addListener
 import com.cn.game.sdk2.R
 import com.cn.game.sdk2.databinding.ToastLayoutBinding
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class Fast3Toast @JvmOverloads constructor(
     context: Context,
@@ -20,7 +29,7 @@ class Fast3Toast @JvmOverloads constructor(
     }
     private var binding: ToastLayoutBinding = ToastLayoutBinding.inflate(LayoutInflater.from(context), this, true)
     private var dismissRunnable:Runnable? = null
-
+    private var animator:Animator? = null
 
     fun showToastNormal(msg: CharSequence, duration: Long) {
         dismissToast()
@@ -31,11 +40,44 @@ class Fast3Toast @JvmOverloads constructor(
         lp.topToTop = R.id.viewPagerNew
         lp.bottomToBottom = R.id.viewPagerNew
         anchorView.addView(this, lp)
+        playAnim(true)
         dismissRunnable = kotlinx.coroutines.Runnable {
-            dismissToast()
-            dismissRunnable = null
+            playAnim(false){
+                dismissToast()
+                dismissRunnable = null
+            }
         }
         postDelayed(dismissRunnable, duration)
+    }
+
+    //Todo 改造协程
+    private fun playAnim(show:Boolean,onEnd:(()->Unit)? = null){
+        animator.let {
+            animator?.cancel()
+            val start = if(show) 0f else 1f;
+            val end = if(!show) 0f else 1f;
+            ValueAnimator.ofFloat(start,end).apply {
+                duration = 150
+                addUpdateListener { animation ->
+                    val p = animation.animatedValue as Float
+                    scaleX = p
+                    scaleY = p
+                }
+                addListener(
+                    onStart = {
+                        scaleX = start
+                        scaleY = start
+                        animator = it
+                    },
+                    onCancel = { animator = null},
+                    onEnd = {
+                        animator = null
+                        onEnd?.invoke()
+                    }
+                )
+                start()
+            }
+        }
     }
 
     fun dismissToast() {
