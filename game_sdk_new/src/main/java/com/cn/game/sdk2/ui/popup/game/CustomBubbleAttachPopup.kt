cd77179da8c128b86dd52cb987a17977840dfaf0
Link Hsieh <link.hsieh@skylinetw.com>
2024-08-23 16:59:04 +0800

    refactor : move the code of more list from mainfragment

--- game_sdk_new/src/main/java/com/cn/game/sdk2/ui/popup/game/CustomBubbleAttachPopup.kt
+++ game_sdk_new/src/main/java/com/cn/game/sdk2/ui/popup/game/CustomBubbleAttachPopup.kt
@@ -1,26 +1,17 @@
 package com.cn.game.sdk2.ui.popup.game
 
 import android.content.Context
-import android.content.ContextWrapper
 import androidx.core.view.isVisible
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import com.cn.game.sdk2.R
-import com.cn.game.sdk2.databinding.DialogHomeXpopupContainerBinding
 import com.cn.game.sdk2.databinding.PopupCustomBubbleAttachBinding
-import com.cn.game.sdk2.ui.popup.HomeXPopupDialog.Companion.TAG
-import com.cn.game.sdk2.ui.page.fast3.Fast3GameHallFragment
 import com.cn.game.sdk2.ui.helper.ViewHelper
+import com.cn.game.sdk2.ui.view.game.GameListView
 import com.cn.game.sdk2.websocket.appListener
 import com.cn.game.sdk2.websocket.gameAboutModel
 import com.cn.game.sdk2.websocket.runOnUiThread
 import com.lxj.xpopup.XPopup
 import com.lxj.xpopup.core.AttachPopupView
-import com.lxj.xpopup.core.BasePopupView
-import com.lxj.xpopup.core.BottomPopupView
 import com.lxj.xpopup.enums.PopupAnimation
-import com.lxj.xpopup.interfaces.SimpleCallback
-import com.xcjh.base_lib2.utils.LogUtils
 import com.xcjh.base_lib2.utils.view.clickNoRepeat
 
 
@@ -29,18 +20,14 @@ import com.xcjh.base_lib2.utils.view.clickNoRepeat
  */
 class CustomBubbleAttachPopup(content: Context) : AttachPopupView(content){
 
+    private var listener: OnCustomBubbleAttachPopupListener? = null
+
     override fun getImplLayoutId(): Int {
         return R.layout.popup_custom_bubble_attach
     }
 
     override fun onCreate() {
         super.onCreate()
-        /*setBubbleBgColor(ContextCompat.getColor(context,R.color.c_ffffff))
-        setBubbleShadowSize(2)
-        setArrowWidth(XPopupUtils.dp2px(context, 8f))
-        setArrowHeight(XPopupUtils.dp2px(context, 8f)).setBubbleRadius(10)
-        setArrowRadius(XPopupUtils.dp2px(context, 2f))*/
-
         PopupCustomBubbleAttachBinding.bind(popupImplView).apply {
             rlPopClickRecords.isVisible = !gameAboutModel.simplifyMoreButtons
             rlPopClickService.isVisible = !gameAboutModel.simplifyMoreButtons
@@ -57,10 +44,9 @@ class CustomBubbleAttachPopup(content: Context) : AttachPopupView(content){
                 }
             }
 
-            rlPopClickSwitchGame.clickNoRepeat(true,0) {
+            rlPopClickSwitchGame.clickNoRepeat(true) {
                 delayDismiss(100)
-                customBubbleAttachListener?.switchGame()
-                //switchGame()
+                showGameList()
             }
             rlPopClickAssist.clickNoRepeat(true){
                 delayDismiss(100)
@@ -69,88 +55,27 @@ class CustomBubbleAttachPopup(content: Context) : AttachPopupView(content){
         }
     }
 
-    private fun switchGame(){
-        val popupView = object :BottomPopupView(context) {
-            //override fun getImplLayoutId(): Int  = R.layout.fragment_gamehall
-            override fun getImplLayoutId(): Int  = R.layout.dialog_home_xpopup_container
-
-            var binding: DialogHomeXpopupContainerBinding? = null
-            val fragment = Fast3GameHallFragment()
-            var oldFragment:Fragment? = null
-
-            override fun onCreate() {
-                super.onCreate()
-                binding = DialogHomeXpopupContainerBinding.bind(popupImplView)
-                oldFragment = fragmentManager.findFragmentByTag("HomeXPopupDialog")
-                val transaction = fragmentManager.beginTransaction();
-                if(oldFragment != null )transaction.hide(oldFragment!!)
-                transaction.add(R.id.fl_container, fragment,"switchGame").commit()
-                LogUtils.dTag(TAG, "onCreate")
-            }
-
-         /*   private fun showFragment(fragment: Fragment?, tag: String) {
-                if (fragment === mCurrentFragment) {
-                    return
-                }
-                val transaction = supportFragmentManager.beginTransaction()
-                if (mCurrentFragment != null) transaction.hide(mCurrentFragment!!)
-                if (!fragment!!.isAdded) {
-                    transaction.add(R.id.fragment, fragment, tag)
-                } else {
-                    transaction.show(fragment)
-                }
-                mCurrentFragment = fragment
-                transaction.commit()
-            }*/
-
-            override fun onDismiss() {
-                super.onDismiss()
-                binding = DialogHomeXpopupContainerBinding.bind(popupImplView)
-                val transaction = fragmentManager.beginTransaction()
-                if(oldFragment != null) transaction.show(oldFragment!!)
-                transaction.remove(fragment).commit()
-            }
-
-            private val fragmentManager
-                get() = run {
-                    when (context) {
-                        is FragmentActivity -> (context as FragmentActivity).supportFragmentManager
-                        is ContextWrapper -> (((context as ContextWrapper).baseContext) as Fragment).childFragmentManager
-                        else -> throw IllegalStateException("illegal context:$context ,type:${context.javaClass.simpleName}")
-                    }
-                }
-
-        }
-
+    private fun showGameList() {
+        val popupView = GameListView(context)
+        popupView.targetHeight = listener?.setSecondPopHeight() ?: 0
         XPopup.Builder(context)
-        //.isTouchThrough(true)
-        .setPopupCallback(object : SimpleCallback() {
-            override fun onCreated(popupView: BasePopupView?) {
-                super.onCreated(popupView)
-            }
-            override fun onDismiss(popupView: BasePopupView?) {
-                super.onDismiss(popupView)
-            }
-        })
-        .popupAnimation(PopupAnimation.TranslateFromBottom)
-        .navigationBarColor(android.R.color.transparent)
-        .isViewMode(true)
-        .hasShadowBg(false) // 去掉半透明背景
-        .enableDrag(true)
-        .dismissOnTouchOutside(false)
-        .asCustom(popupView)
-        .show()
+            .isTouchThrough(false)
+            .popupAnimation(PopupAnimation.TranslateFromBottom)
+            .navigationBarColor(android.R.color.transparent)
+            .animationDuration(100)//默认300ms
+            .isViewMode(true)
+            .hasShadowBg(false) // 去掉半透明背景
+            .enableDrag(true)
+            .dismissOnTouchOutside(true)
+            .asCustom(popupView)
+            .show()
     }
 
-    var customBubbleAttachListener: CustomBubbleAttachListener?=null
-
-    interface  CustomBubbleAttachListener{
-        /**
-         * 切换游戏
-         */
-        fun  switchGame()
+    fun setOnCustomBubbleAttachPopupListener(listener: OnCustomBubbleAttachPopupListener) {
+        this.listener = listener
     }
 
-
-
+    interface OnCustomBubbleAttachPopupListener {
+        fun setSecondPopHeight(): Int
+    }
 }
