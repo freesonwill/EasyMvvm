44f16472b6470d85f22c51d380848747d3f5c448
joseph.tseng <joseph.tseng@skylinetw.com>
2024-08-23 17:01:33 +0800

    refactor: remove useless class

--- game_sdk_new/src/main/java/com/cn/game/sdk2/base/BaseGameVpFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.cn.game.sdk2.base
-
-import androidx.databinding.ViewDataBinding
-import com.xcjh.base_lib2.base.BaseViewModel
-
-abstract class BaseGameVpFragment <VM : BaseViewModel, VB : ViewDataBinding> : BaseGameFragment<VM, VB>() {
-    abstract val typeId: Long
-
-}
