3bad315dcceb5b38342f2c2bfeddd6e4c0269899
Link Hsieh <link.hsieh@skylinetw.com>
2024-09-03 16:12:49 +0800

    refactor : modify param name of IconUtils

--- game_sdk_new/src/main/java/com/cn/game/sdk2/utils/IconUtils.kt
+++ game_sdk_new/src/main/java/com/cn/game/sdk2/utils/IconUtils.kt
@@ -6,23 +6,23 @@ import com.xcjh.base_lib2.ModuleInitializer
 object IconUtils {
 
     /***
-     * @key String ex. "R.mipmap.ic_launcher"
+     * @key String ex. game_sdk_icon_select
      * @value Int ex. R.mipmap.ic_launcher
      */
     private val iconMap = mutableMapOf<String, Int?>()
     private val context: Context
         get() = ModuleInitializer.application
 
-    fun getIcon(key: String): Int {
-        var id = iconMap[key]
+    fun getIcon(name: String): Int {
+        var id = iconMap[name]
         if (id == null) {
             id = context.resources.getIdentifier(
-                key,
+                name,
                 "mipmap",
                 context.packageName
             )
             if (id != 0 && id != -1) {
-                iconMap[key] = id
+                iconMap[name] = id
             }
         }
         return id
