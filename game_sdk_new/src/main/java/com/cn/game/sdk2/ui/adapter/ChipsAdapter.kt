8f54ab1eb6321801c87023f3fd6899bf6369d66a
Link Hsieh <link.hsieh@skylinetw.com>
2024-09-04 16:18:47 +0800

    feat : add ChipsViewHolder

--- game_sdk_new/src/main/java/com/cn/game/sdk2/ui/adapter/ChipsAdapter.kt
+++ game_sdk_new/src/main/java/com/cn/game/sdk2/ui/adapter/ChipsAdapter.kt
@@ -1,49 +1,22 @@
 package com.cn.game.sdk2.ui.adapter
 
-import android.animation.ObjectAnimator
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import com.cn.game.sdk2.data.bean.SelectAnnotationBean
 import com.cn.game.sdk2.databinding.ItemAnnotationListBinding
 import com.cn.game.sdk2.ui.compare.ChipsCompare
-import com.cn.game.sdk2.ui.viewholder.BaseViewHolder
-import com.cn.game.sdk2.utils.IconUtils
-import com.cn.game.sdk2.utils.ext.DensityExt.dp2px
-import com.cn.game.sdk2.websocket.gameAboutModel
+import com.cn.game.sdk2.ui.viewholder.ChipsViewHolder
 
-class ChipsAdapter : BaseAdapter<SelectAnnotationBean, BaseViewHolder, ItemAnnotationListBinding>(
+class ChipsAdapter : BaseAdapter<SelectAnnotationBean, ChipsViewHolder, ItemAnnotationListBinding>(
     ChipsCompare()
 ) {
 
     override fun convertPlus(
-        holder: BaseViewHolder,
+        holder: ChipsViewHolder,
         binding: ItemAnnotationListBinding,
         item: SelectAnnotationBean
     ) {
-        val id = if ((gameAboutModel.tempBalance.value ?: 0) < item.money) {
-            IconUtils.getIcon("game_sdk_icon_shortage_" + item.moneyPinyin)
-        } else {
-            if (item.select) {
-                IconUtils.getIcon("game_sdk_icon_select_" + item.moneyPinyin)
-            } else {
-                IconUtils.getIcon("game_sdk_icon_no_" + item.moneyPinyin)
-            }
-        }
-        if (id != 0) {
-            binding.ivShowBg.setImageResource(id)
-        }
-        //Log.d(TAG, "onBind-->${layoutPosition},bean:${bean}")
-        if (item.select) {
-            if (binding.ivShowBg.translationY == 0f) {
-                val anim =
-                    ObjectAnimator.ofFloat(binding.ivShowBg, "translationY", -holder.itemView.context.dp2px(5).toFloat())
-                anim.duration = 100L
-                anim.start()
-            }
-        } else {
-            binding.ivShowBg.translationY = 0f
-        }
+        holder.init(item)
     }
 
     override fun createViewBinding(
@@ -57,7 +30,7 @@ class ChipsAdapter : BaseAdapter<SelectAnnotationBean, BaseViewHolder, ItemAnnot
     override fun createViewHolder(
         binding: ItemAnnotationListBinding,
         viewType: Int
-    ): BaseViewHolder {
-        return BaseViewHolder(binding)
+    ): ChipsViewHolder {
+        return ChipsViewHolder(binding)
     }
 }
