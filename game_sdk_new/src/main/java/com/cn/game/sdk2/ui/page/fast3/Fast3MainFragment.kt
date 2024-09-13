83c9c29c88c250fba4dcb5f79a57b49966f3cd1a
Link Hsieh <link.hsieh@skylinetw.com>
2024-09-13 16:00:58 +0800

    feat : add viewpager2 over scroll

--- game_sdk_new/src/main/java/com/cn/game/sdk2/ui/page/fast3/Fast3MainFragment.kt
+++ game_sdk_new/src/main/java/com/cn/game/sdk2/ui/page/fast3/Fast3MainFragment.kt
@@ -44,6 +44,7 @@ import com.cn.game.sdk2.utils.ext.ViewExt.getCenterPoint
 import com.cn.game.sdk2.utils.ext.ViewExt.isAdd
 import com.cn.game.sdk2.utils.ext.ViewExt.locationOnScreen
 import com.cn.game.sdk2.utils.ext.removeTips
+import com.cn.game.sdk2.utils.ext.setOverScrollModeExt
 import com.cn.game.sdk2.utils.tool.PromptSoundPlay
 import com.cn.game.sdk2.websocket.appListener
 import com.cn.game.sdk2.websocket.bean.BettingRecordBean
@@ -64,6 +65,7 @@ import com.xcjh.base_lib2.utils.view.clickNoRepeat
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import me.everything.android.ui.overscroll.OverScrollDecoratorHelper
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 
@@ -115,6 +117,9 @@ class Fast3MainFragment : BaseFragment<Fast3ViewModel, FragFast3HomeBinding>() {
                 LogUtils.dTag(TAG, "Fast3MainFragment load costMills1:$it")
             }
             viewPagerNew.adapter = PagerAdapter(childFragmentManager, lifecycle, gamePageList)
+            viewPagerNew.setOverScrollModeExt(
+                BottomPopupView.OVER_SCROLL_IF_CONTENT_SCROLLS,
+                OverScrollDecoratorHelper.ORIENTATION_HORIZONTAL)
             TabLayoutMediator(tlGame, viewPagerNew) { tab, position ->
                 val tabView = tab.view
                     tab.text = gamePageList[position].title
