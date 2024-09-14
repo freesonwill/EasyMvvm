a7ce5d198e1fbfe61ce08ed216ba59c1e1852174
zhangsan <zhangsan@youguess.com>
2024-09-14 11:30:52 +0700

    bugfix: 游戏大厅放缩比率统一为ios的0.9倍

--- game_sdk_new/src/main/java/com/cn/game/sdk2/ui/adapter/GameHallItemAdapter.kt
+++ game_sdk_new/src/main/java/com/cn/game/sdk2/ui/adapter/GameHallItemAdapter.kt
@@ -21,7 +21,7 @@ import java.io.File
 class GameHallItemAdapter : BaseAdapter<GameHallItem, BaseViewHolder, ItemGamehallPageItemBinding>(
     GameHallItemCompare()
 ) {
-    private val IMAGE_SCALE_RATIO = 0.8f
+    private val IMAGE_SCALE_RATIO = 0.9f
     private val IMAGE_SCALE_DURATION = 300L
     private var scaleXAnimation: ObjectAnimator? = null
     private var scaleYAnimation: ObjectAnimator? = null
