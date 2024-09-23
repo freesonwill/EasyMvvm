f3a74ec7f4ee234e335ac1e914a6e1d825e134ad
Jeremy <jeremy.huang@skylinetw.com>
2024-09-23 10:00:42 +0800

    chore: improve viewPager switch animation

--- game_sdk_new/src/main/java/com/cn/game/sdk2/ui/helper/AnimHelper.kt
+++ game_sdk_new/src/main/java/com/cn/game/sdk2/ui/helper/AnimHelper.kt
@@ -6,17 +6,20 @@ import android.animation.PropertyValuesHolder
 import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.animation.addListener
 import androidx.core.animation.doOnEnd
+import androidx.core.view.drawToBitmap
 import androidx.core.view.isVisible
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentContainerView
-import androidx.fragment.app.FragmentManager
 import androidx.viewpager2.widget.ViewPager2
 import com.cn.game.sdk2.utils.ext.CommonExt.formatRealMoney
 import com.xcjh.base_lib2.utils.StringFormatUtil.Companion.decimalFormat2
 import com.xcjh.base_lib2.utils.StringFormatUtil.Companion.decimalFormatMax2
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import java.math.BigDecimal
 
 object AnimHelper {
@@ -71,60 +74,58 @@ object AnimHelper {
      *  執行客制ViewPager轉場動畫
      *
      *  @param targetPosition 欲轉跳的頁面Index
-     *  @param fragmentManger FragmentManager
-     *  @param prevFragment 前一個Fragment
      *  @param viewPager ViewPager
      *  @param fakeViewPager 用來顯示前一個Fragment的View
      */
     fun doDirectViewPagerAnim(
         targetPosition: Int,
-        fragmentManger: FragmentManager,
-        prevFragment: Fragment,
         viewPager: ViewPager2,
-        fakeViewPager: FragmentContainerView
-    ) {
+        fakeViewPager: ImageView
+    ): Job? {
         val prevPosition = viewPager.currentItem
-        if(prevPosition == targetPosition) return
+        if(prevPosition == targetPosition) return null
 
-        val isPrev = prevPosition < targetPosition
-        val width = viewPager.width
+        return CoroutineScope(Dispatchers.Main).launch {
+            val isPrev = prevPosition < targetPosition
+            val width = viewPager.width
 
-        // 把要移除畫面的fragment放到fakeViewPager上
-        fragmentManger.beginTransaction()
-            .replace(fakeViewPager.id, prevFragment)
-            .commitNow()
-        fakeViewPager.isVisible = true
-
-        // 將ViewPager移動到指定位置
-        viewPager.setCurrentItem(targetPosition, false)
-
-        // 構建動畫
-        val animationList =
-            listOf(
-                Triple(viewPager, if (isPrev) width * 1f else width * -1f, 0f),
-                Triple(fakeViewPager, 0f, if (isPrev) width * -1f else width * 1f)
-            ).map {
-                ObjectAnimator.ofFloat(it.first, "translationX", it.second, it.third)
+            // 將ViewPager截圖並設置給FakeViewPager
+            fakeViewPager.apply {
+                setImageBitmap(viewPager.drawToBitmap())
+                isVisible = true
             }
 
-        // 執行動畫
-        AnimatorSet().apply {
-            playTogether(animationList)
-            addListener(
-                onStart = {
-                    fakeViewPager.translationX = 0f
-                    viewPager.translationX =
-                        if(isPrev) width * -1f
-                        else width * 1f
-                },
-                onEnd = {
-                    fakeViewPager.isVisible = false
-                    fragmentManger.beginTransaction()
-                        .remove(prevFragment)
-                        .commitNow()
+            // 將ViewPager移動到指定位置
+            viewPager.setCurrentItem(targetPosition, false)
+
+            // 構建動畫
+            val animationList =
+                listOf(
+                    Triple(viewPager, if (isPrev) width * 1f else width * -1f, 0f),
+                    Triple(fakeViewPager, 0f, if (isPrev) width * -1f else width * 1f)
+                ).map {
+                    ObjectAnimator.ofFloat(it.first, "translationX", it.second, it.third)
                 }
-            )
-            start()
+
+            // 執行動畫
+            AnimatorSet().apply {
+                playTogether(animationList)
+                addListener(
+                    onStart = {
+                        fakeViewPager.translationX = 0f
+                        viewPager.translationX =
+                            if(isPrev) width * -1f
+                            else width * 1f
+                    },
+                    onEnd = {
+                        fakeViewPager.apply {
+                            isVisible = false
+                            setImageResource(android.R.color.transparent)
+                        }
+                    }
+                )
+                start()
+            }
         }
     }
 
