94d5161308185e91bf4419109f4b41727b66c0b3
zhangsan <zhangsan@youguess.com>
2025-03-13 17:17:22 +0700

    feature: change package name to "com.walisport.app"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.wls_android"
+    namespace = "com.walisport.app"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.wls_android"
+        applicationId = "com.walisport.app"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -40,6 +40,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
