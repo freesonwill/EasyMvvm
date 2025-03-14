cedbccb72830c1f3294aa0233e8b9b74acea7aee
wangzai <dafeix64@gmail.com>
2025-03-14 18:06:08 +0700

    接入ijkplayer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(project(":module_home"))
     implementation(project(":module_login"))
     implementation(project(":module_setting"))
+    implementation(project(":videoplayer"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,8 +7,10 @@ pluginManagement {
                 includeGroupByRegex("androidx.*")
             }
         }
+        jcenter()
         mavenCentral()
         gradlePluginPortal()
+
     }
 }
 dependencyResolutionManagement {
@@ -16,6 +18,14 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        jcenter()
+        maven(url = "https://jitpack.io")
+        maven(url = "https://repo1.maven.org/maven2/")
+        maven(url = "https://developer.huawei.com/repo/")
+        maven(url = "https://maven.aliyun.com/repository/public/")
+        //阿里云jcenter仓库
+        maven(url = "https://maven.aliyun.com/repository/jcenter")
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
     }
 }
 
@@ -27,3 +37,4 @@ include(":module_login")
 include(":module_home")
 include(":module_setting")
 include(":videoplayer")
+
