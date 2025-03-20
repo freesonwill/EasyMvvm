be0a4041b659b77567359fad97f4026d65d63166
xiaoyang <xiaoyang@abc.com>
2025-03-20 17:06:47 +0700

    feature:集成AppInit模块初始化框架

--- lib_common/build.gradle.kts
+++ lib_common/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     api(libs.material)
     api(libs.androidx.activity)
     api(libs.androidx.constraintlayout)
+    implementation(libs.mmkv)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
