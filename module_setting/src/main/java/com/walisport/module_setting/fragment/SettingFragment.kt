c030055bcc8301a647275332832072c22f60d46b
xiaoyang <xiaoyang@abc.com>
2025-04-01 14:40:43 +0700

    feature:Navigation导航整合到设置模块，让设置模块的界面之间按照规范过渡跳转 - https://linear.app/forgames/issue/WLS-37

---
+++ module_setting/src/main/java/com/walisport/module_setting/fragment/SettingFragment.kt
@@ -0,0 +1,41 @@
+package com.walisport.module_setting.fragment
+
+import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import com.walisport.lib_base.ui.BaseFragment
+import com.walisport.lib_base.ui.viewBind
+import com.walisport.module_setting.R
+import com.walisport.module_setting.data.SettingViewModel
+import com.walisport.module_setting.databinding.FragmentSettingBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+/**
+ * 设置界面
+ */
+
+class SettingFragment : BaseFragment<SettingViewModel, FragmentSettingBinding>() {
+
+    override val mBinding: FragmentSettingBinding by viewBind()
+    override val mViewModel: SettingViewModel by viewModel()
+
+    override fun initView(savedInstanceState: Bundle?) {
+    }
+
+    override fun initListener() {
+        mBinding.relSettingOdds.setOnClickListener {
+
+        }
+        mBinding.relSettingNotice.setOnClickListener {
+            findNavController().navigate(R.id.action_settingFragment_to_noticedFragment)
+        }
+        mBinding.relSettingBg.setOnClickListener {
+            findNavController().navigate(R.id.action_settingFragment_to_backgroundFragment)
+        }
+        mBinding.relSettingLanguage.setOnClickListener {
+            findNavController().navigate(R.id.action_settingFragment_to_languageFragment)
+        }
+    }
+
+    override fun createObserver() {
+    }
+}
