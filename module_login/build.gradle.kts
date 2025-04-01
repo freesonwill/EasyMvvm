25147e9767503a967ad11e09a003a3dfdb1a6955
wangzai <dafeix64@gmail.com>
2025-04-01 17:42:29 +0700

    调整login工程包名，去掉名称中间的下划线

--- module_login/build.gradle.kts
+++ module_login/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 apply(from = rootProject.file("gradle/flavor.gradle"))
 android {
-    namespace = "com.walisport.module_login"
+    namespace = "com.walisport.module.login"
     compileSdk = 34
 
     defaultConfig {
