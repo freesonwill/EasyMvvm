357ee494282bf7d28c632876d6a439c3424c2ca9
wangzai <dafeix64@gmail.com>
2025-04-01 22:27:05 +0700

    直播页视频播放

--- module_live/src/main/kotlin/com/walisport/module/live/ui/viewmodel/LiveVideoViewModel.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/viewmodel/LiveVideoViewModel.kt
@@ -1,6 +1,10 @@
 package com.walisport.module.live.ui.viewmodel
 
+import android.view.View
+import androidx.lifecycle.MutableLiveData
 import com.walisport.lib_base.data.viewmodel.BaseViewModel
 
 class LiveVideoViewModel : BaseViewModel() {
+
+    val videoPlayVisible = MutableLiveData(View.VISIBLE)
 }
