1dfe3cf43a2aca10b66743849631af9c03e5aaa8
wangzai <dafeix64@gmail.com>
2025-04-02 15:31:39 +0700

    直播页视频播放

--- module_live/src/main/kotlin/com/walisport/module/live/ui/viewmodel/LiveVideoViewModel.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/viewmodel/LiveVideoViewModel.kt
@@ -7,4 +7,22 @@ import com.walisport.lib_base.data.viewmodel.BaseViewModel
 class LiveVideoViewModel : BaseViewModel() {
 
     val videoPlayVisible = MutableLiveData(View.VISIBLE)
+
+    val statusVisible = MutableLiveData(View.INVISIBLE)
+
+    val playerAUrl = MutableLiveData<String>("")
+    val playerAName = MutableLiveData<String>("")
+
+    val playerBUrl = MutableLiveData<String>("")
+    val playerBName = MutableLiveData<String>("")
+
+
+    val titleText = MutableLiveData<String>("")
+    val titleTextColor = MutableLiveData<Int>(com.walisport.lib_common.R.color.white)
+    val titleTextSize = MutableLiveData<Int>(com.walisport.lib_common.R.dimen.sp_17)
+
+    val subTitleText = MutableLiveData<String>("")
+    val subTitleTextColor = MutableLiveData<Int>(com.walisport.lib_common.R.color.color_929298)
+    val subTitleTextSize = MutableLiveData<Int>(com.walisport.lib_common.R.dimen.sp_14)
+
 }
