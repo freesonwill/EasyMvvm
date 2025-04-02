810d37083bde241a8791c0e0e755e165565ce569
wenxi <russelllin465@gmail.com>
2025-04-02 18:00:16 +0700

    feature:  添加换肤包

---
+++ lib_skin/src/main/java/com/walisport/lib/skin/res/SportSkinBuildInResourceLoader.kt
@@ -0,0 +1,60 @@
+package com.walisport.lib.skin.res
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import androidx.core.content.res.ResourcesCompat
+import com.walisport.lib.skin.util.ResUtils
+import com.walisport.lib.skin.widget.helper.SportSkinHelper
+
+class SportSkinBuildInResourceLoader(val skinName: String) : SportSkinResourceLoader {
+
+
+    override fun getColor(context: Context, resId: Int): Int {
+        val targetId = getTargetResourceId(context, resId,)
+        if (targetId != SportSkinHelper.INVALID_ID) {
+            return ResourcesCompat.getColor(context.resources, targetId, context.theme)
+        }
+        return SportSkinHelper.INVALID_ID
+    }
+
+    override fun getColorStateList(context: Context, resId: Int): ColorStateList? {
+        val targetId = getTargetResourceId(context, resId)
+        if (targetId != SportSkinHelper.INVALID_ID) {
+            return ResourcesCompat.getColorStateList(context.resources, targetId, context.theme)
+        }
+        return null
+    }
+
+    override fun getDrawable(context: Context, resId: Int): Drawable? {
+        val targetId = getTargetResourceId(context, resId)
+        if (targetId != SportSkinHelper.INVALID_ID) {
+            val type = context.resources.getResourceTypeName(resId)
+            if(type == "color"){
+                return ColorDrawable(context.getColor(targetId))
+            }
+
+            return ResourcesCompat.getDrawable(context.resources, targetId, context.theme)
+        }
+        return null
+    }
+
+    override fun getTargetResourceId(context: Context, resId: Int): Int {
+        return try {
+            if(skinName.isEmpty()){
+                return resId
+            }
+            val resName =  context.resources.getResourceEntryName(resId) + "_" + skinName
+            val type = context.resources.getResourceTypeName(resId)
+            val targetResId = ResUtils.getResourceId(context,resName,type)
+            if(targetResId == 0){
+                return resId
+            }
+            return targetResId
+        } catch (e: Exception) {
+            e.printStackTrace()
+            SportSkinHelper.INVALID_ID
+        }
+    }
+}
