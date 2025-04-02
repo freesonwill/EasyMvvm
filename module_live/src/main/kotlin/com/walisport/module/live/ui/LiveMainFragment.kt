5ccb9044496e67c26fa45fd57ff3ea0cca2a482d
wangzai <dafeix64@gmail.com>
2025-04-02 16:37:15 +0700

    直播页fragment

---
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/LiveMainFragment.kt
@@ -0,0 +1,26 @@
+package com.walisport.module.live.ui
+
+import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import com.walisport.lib_base.ui.BaseFragment
+import com.walisport.lib_base.ui.viewBind
+import com.walisport.module.live.databinding.FragmentLiveMainBinding
+import com.walisport.module.live.ui.viewmodel.LiveMainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class LiveMainFragment : BaseFragment<LiveMainViewModel, FragmentLiveMainBinding>() {
+    override val mBinding: FragmentLiveMainBinding by viewBind()
+
+    override val mViewModel: LiveMainViewModel by viewModel()
+
+
+    override fun initView(savedInstanceState: Bundle?) {
+    }
+
+    override fun initListener() {
+        mBinding.ivBack.setOnClickListener { findNavController().navigateUp() }
+    }
+
+    override fun createObserver() {
+    }
+}
