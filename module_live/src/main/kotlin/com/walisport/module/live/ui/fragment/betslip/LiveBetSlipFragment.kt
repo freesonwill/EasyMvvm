fdf1553f88630c2b1c397b033c1bd3d7ccceee0b
wenxi <russelllin465@gmail.com>
2025-04-02 15:14:32 +0700

    feature: 1.新增autosize插件，解决和设计图的适配问题               2.新增直播间注单页面

---
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/fragment/betslip/LiveBetSlipFragment.kt
@@ -0,0 +1,21 @@
+package com.walisport.module.live.ui.fragment.betslip
+
+import android.os.Bundle
+import com.walisport.lib_base.ui.BaseFragment
+import com.walisport.lib_base.ui.viewBind
+import com.walisport.module.live.databinding.FragmentLiveBetSlipLayoutBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class LiveBetSlipFragment : BaseFragment<LiveBetSlipViewModel, FragmentLiveBetSlipLayoutBinding>() {
+    override val mBinding: FragmentLiveBetSlipLayoutBinding by viewBind()
+    override val mViewModel: LiveBetSlipViewModel by viewModel()
+
+    override fun initView(savedInstanceState: Bundle?) {
+    }
+
+    override fun initListener() {
+    }
+
+    override fun createObserver() {
+    }
+}
---
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/fragment/betslip/LiveBetSlipViewModel.kt
@@ -0,0 +1,6 @@
+package com.walisport.module.live.ui.fragment.betslip
+
+import com.walisport.lib_base.data.viewmodel.BaseViewModel
+
+class LiveBetSlipViewModel:BaseViewModel() {
+}
