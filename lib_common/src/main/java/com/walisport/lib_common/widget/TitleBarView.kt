835359023031ba0565238c0188a3145bb915d1bb
aquan <aquan@example.com>
2025-04-02 12:03:36 +0700

    feta：WLS-30 1:自定义标题栏

--- lib_common/src/main/java/com/walisport/lib_common/widget/TitleBarView.kt
+++ lib_common/src/main/java/com/walisport/lib_common/widget/TitleBarView.kt
@@ -1,5 +1,81 @@
 package com.walisport.lib_common.widget
 
-class TitleBarView {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.appcompat.widget.Toolbar
+import com.walisport.lib_common.R
+import com.walisport.lib_common.databinding.TittleBarDefaultBinding
+import com.walisport.lib_common.databinding.TittleBarSearchBinding
+
+class TitleBarView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : Toolbar(context, attrs, defStyleAttr) {
+
+
+    /**
+     * 通用标题
+     * @param titleName 标题名称
+     * @param callback 返回
+     */
+    fun loadGeneralTitleBar(titleName: String, callback: () -> Unit) {
+        val binding = TittleBarDefaultBinding.inflate(LayoutInflater.from(context), this, true)
+        binding.apply {
+            tvTitleName.text = titleName
+            ivBack.setOnClickListener{
+                callback()
+            }
+        }
+    }
+
+    /**
+     * 通用标题
+     * @param titleName 标题名称
+     * @param callback 返回
+     * @param callbackRight 右边按钮点击回调
+     */
+    fun loadTitleBarShowRight(titleName: String, callback: () -> Unit,callbackRight:() ->Unit) {
+        val binding = TittleBarDefaultBinding.inflate(LayoutInflater.from(context), this, true)
+        binding.apply {
+            tvTitleName.text = titleName
+            ivBack.setOnClickListener{ callback() }
+            tvTitleRight.setOnClickListener{  callbackRight() }
+        }
+    }
+
+    /**
+     * 搜索标题
+     * @param hintText 搜索框提示
+     * @param callback 返回
+     * @param callbackSearch 搜索
+     */
+    fun loadSearchTitleBar(hintText: String, callback: () -> Unit,callbackSearch: (String) -> Unit) {
+        val binding = TittleBarSearchBinding.inflate(LayoutInflater.from(context), this, true)
+        binding.apply {
+            ceSearch.hint = hintText
+            ivBack.setOnClickListener {
+                callback()
+            }
+            tvSearchText.setOnClickListener{
+                callbackSearch(ceSearch.text.toString())
+            }
+        }
+    }
+
+    /**
+     * 直播标题
+     * @param leagueImgUrl 联赛图片
+     * @param competitionName 联赛对站 A VS B
+     * @param money 剩余总金额
+     * @param callback 返回
+     * @param callbackCompetition 下拉切换
+     */
+    fun loadLiveTitleBar(leagueImgUrl: String,competitionName:String,money:String, callback: () -> Unit,callbackCompetition: (String) -> Unit) {
+
+
+
+    }
 
 }
