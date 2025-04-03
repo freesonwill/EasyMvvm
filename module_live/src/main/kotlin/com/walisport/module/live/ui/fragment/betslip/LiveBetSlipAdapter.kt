f68e5554fc2e6f243bc3208f7f86332f4beb69c5
wenxi <russelllin465@gmail.com>
2025-04-03 16:50:11 +0700

    feature: 未结算页面初步搭建完成

---
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/fragment/betslip/LiveBetSlipAdapter.kt
@@ -0,0 +1,84 @@
+package com.walisport.module.live.ui.fragment.betslip
+
+import android.annotation.SuppressLint
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.viewbinding.ViewBinding
+import com.walisport.lib_common.ui.adapter.BaseAdapter
+import com.walisport.lib_common.ui.adapter.BaseViewHolder
+import com.walisport.lib_common.utils.ext.DimensionExt.dp2px
+import com.walisport.module.live.R
+import com.walisport.module.live.databinding.AdapterLiveBetSlipMenuItemLayoutBinding
+
+class LiveBetSlipAdapter(compare: DiffUtil.ItemCallback<String>) :
+    BaseAdapter<String, LiveBetSlipAdapter.LiveBetSlipViewHolder, ViewBinding>(
+        compare
+    ) {
+    private var selected: Int = 0
+
+    inner class LiveBetSlipViewHolder(binding: ViewBinding) : BaseViewHolder(binding) {
+        private val viewBinding: AdapterLiveBetSlipMenuItemLayoutBinding =
+            binding as AdapterLiveBetSlipMenuItemLayoutBinding
+
+        init {
+            setOnClickListener()
+        }
+
+        private fun setOnClickListener() {
+            viewBinding.tvMenu.setOnClickListener {
+                val position: Int = it.tag as Int
+                updateSelected(position)
+            }
+        }
+
+        fun updateItem(position: Int) {
+            viewBinding.tvMenu.setBackgroundResource(if (selected == position) R.drawable.shape_betslip_selected else R.drawable.shape_betslip_normal)
+            viewBinding.tvMenu.setTextColor(
+                ContextCompat.getColor(
+                    viewBinding.tvMenu.context,
+                    if (selected == position) R.color.text_theme else R.color.secondary_text
+                )
+            )
+            viewBinding.tvMenu.text = getItem(position)
+            viewBinding.tvMenu.width = if (position <= 2) 72.dp2px else 60.dp2px
+        }
+
+    }
+
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun updateSelected(value: Int) {
+        val lastSelected = selected
+        if (selected == value) {
+            return
+        }
+        selected = value
+        notifyItemChanged(lastSelected)
+        notifyItemChanged(value)
+        Log.i("aaa", "lastSelected $lastSelected   value $value")
+    }
+
+    override fun convertPlus(holder: LiveBetSlipViewHolder, binding: ViewBinding, position: Int) {
+        holder.updateItem(position)
+    }
+
+    override fun createViewBinding(
+        inflater: LayoutInflater,
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewBinding {
+        val binding = AdapterLiveBetSlipMenuItemLayoutBinding.inflate(inflater)
+        return binding
+    }
+
+    override fun createViewHolder(
+        binding: ViewBinding,
+        viewType: Int
+    ): LiveBetSlipViewHolder {
+        val holder = LiveBetSlipViewHolder(binding)
+        return holder
+    }
+}
--- module_live/src/main/kotlin/com/walisport/module/live/ui/fragment/betslip/LiveBetSlipFragment.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/fragment/betslip/LiveBetSlipFragment.kt
@@ -1,8 +1,12 @@
 package com.walisport.module.live.ui.fragment.betslip
 
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.walisport.lib.base.ui.BaseFragment
 import com.walisport.lib.base.ui.viewBind
+import com.walisport.module.live.R
 import com.walisport.module.live.databinding.FragmentLiveBetSlipLayoutBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -11,6 +15,23 @@ class LiveBetSlipFragment : BaseFragment<LiveBetSlipViewModel, FragmentLiveBetSl
     override val mViewModel: LiveBetSlipViewModel by viewModel()
 
     override fun initView(savedInstanceState: Bundle?) {
+        initMenu()
+    }
+
+    private fun initMenu() {
+        val array = resources.getStringArray(R.array.bet_slip_menus)
+        val adapter = LiveBetSlipAdapter(object : DiffUtil.ItemCallback<String>() {
+            override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+            return oldItem == newItem
+            }
+        })
+        adapter.submitList(array.toList())
+        mBinding.betslipRecycler.layoutManager = LinearLayoutManager(context)
+        mBinding.betslipRecycler.adapter = adapter
     }
 
     override fun initListener() {
