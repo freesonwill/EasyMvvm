7437a39aab761aa4bfa9b43807267949ca8035f2
xiaoyang <xiaoyang@abc.com>
2025-04-04 15:13:05 +0700

    feature:背景设置页背景切换功能重构 - https://linear.app/forgames/issue/WLS-72

--- module_setting/src/main/java/com/walisport/module/setting/data/BackgroundViewModel.kt
+++ module_setting/src/main/java/com/walisport/module/setting/data/BackgroundViewModel.kt
@@ -1,6 +1,19 @@
 package com.walisport.module.setting.data
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.walisport.lib.base.data.viewmodel.BaseViewModel
+import org.koin.core.component.inject
+import org.koin.core.parameter.parametersOf
 
 class BackgroundViewModel : BaseViewModel() {
+
+    private val repository: SettingRepository by inject { parametersOf(viewModelScope) }
+    val skinType = MutableLiveData("")
+
+    //设置皮肤背景
+    fun setSkinType(type: String) {
+        repository.setSkinType(type)
+        skinType.value = type
+    }
 }
