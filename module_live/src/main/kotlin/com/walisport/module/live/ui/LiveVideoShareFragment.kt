38781a0906971fbafa504be62ca2cab579901b43
wangzai <dafeix64@gmail.com>
2025-04-10 16:55:38 +0700

    横屏播放时的视频分享页

--- module_live/src/main/kotlin/com/walisport/module/live/ui/LiveVideoShareFragment.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/LiveVideoShareFragment.kt
@@ -5,10 +5,16 @@ import com.walisport.lib.base.ui.BaseFragment
 import com.walisport.lib.base.utils.LogUtilsExt.logd
 import com.walisport.lib.common.utils.ext.clickNoRepeat
 import com.walisport.module.live.databinding.FragmentLiveShareBinding
+import com.walisport.module.live.ui.LiveVideoLandscapeFragment.Companion.LANDSCAPE_HEIGHT
+import com.walisport.module.live.ui.LiveVideoLandscapeFragment.Companion.LANDSCAPE_WIDTH
+import com.walisport.module.live.ui.LiveVideoLandscapeFragment.Companion.PORTRAIT_HEIGHT
+import com.walisport.module.live.ui.LiveVideoLandscapeFragment.Companion.PORTRAIT_WIDTH
 import com.walisport.module.live.ui.viewmodel.LiveVideoViewModel
+import me.jessyan.autosize.AutoSizeConfig
+import me.jessyan.autosize.internal.CancelAdapt
 import kotlin.reflect.KClass
 
-class LiveVideoShareFragment : BaseFragment<LiveVideoViewModel, FragmentLiveShareBinding>() {
+class LiveVideoShareFragment : BaseFragment<LiveVideoViewModel, FragmentLiveShareBinding>() , CancelAdapt{
     override val vbClass: KClass<FragmentLiveShareBinding> = FragmentLiveShareBinding::class
     override val vmClass: KClass<LiveVideoViewModel> = LiveVideoViewModel::class
 
@@ -28,16 +34,8 @@ class LiveVideoShareFragment : BaseFragment<LiveVideoViewModel, FragmentLiveShar
 
     override fun onResume() {
         super.onResume()
-//        //使用横屏时到宽高
-//        AutoSizeConfig.getInstance().setDesignWidthInDp(LANDSCAPE_WIDTH)
-//        AutoSizeConfig.getInstance().setDesignHeightInDp(LANDSCAPE_HEIGHT)
-    }
+        mBinding.root.fitsSystemWindows = false
 
-    override fun onPause() {
-        super.onPause()
-//        //恢复竖屏，宽高也要回到竖屏时到宽高
-//        AutoSizeConfig.getInstance().setDesignWidthInDp(PORTRAIT_WIDTH)
-//        AutoSizeConfig.getInstance().setDesignHeightInDp(PORTRAIT_HEIGHT)
     }
 
     companion object {
