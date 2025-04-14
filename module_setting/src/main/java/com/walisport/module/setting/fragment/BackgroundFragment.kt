f154bfa03680c22715a05f5ad306c1268147c0d3
xiaoyang <xiaoyang@abc.com>
2025-04-14 11:22:53 +0700

    fix:设置模块背景设置接入皮肤切换功能 - https://linear.app/forgames/issue/WLS-118

--- module_setting/src/main/java/com/walisport/module/setting/fragment/BackgroundFragment.kt
+++ module_setting/src/main/java/com/walisport/module/setting/fragment/BackgroundFragment.kt
@@ -1,15 +1,13 @@
 package com.walisport.module.setting.fragment
 
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import androidx.navigation.fragment.findNavController
 import arch.cayenne.lib.base.ui.BaseFragment
 import arch.cayenne.lib.common.utils.ext.DimensionExt.dp2px
-import arch.cayenne.lib.common.utils.ext.DimensionExt.px2dp
-import arch.cayenne.lib.common.utils.ext.DimensionExt.px2sp
 import arch.cayenne.lib.common.utils.ext.clickNoRepeat
 import com.walisport.module.setting.data.BackgroundViewModel
+import com.walisport.module.setting.data.SkinType
 import com.walisport.module.setting.databinding.FragmentBackgroundBinding
 import com.walisport.module.setting.databinding.TittleBarBackgroundBinding
 import kotlin.reflect.KClass
@@ -19,73 +17,76 @@ import kotlin.reflect.KClass
  */
 
 class BackgroundFragment : BaseFragment<BackgroundViewModel, FragmentBackgroundBinding>() {
+
     override val vbClass: KClass<FragmentBackgroundBinding> = FragmentBackgroundBinding::class
     override val vmClass: KClass<BackgroundViewModel> = BackgroundViewModel::class
-
-
-    companion object {
-        const val SKIN_CLASSIC = "CLASSIC"           //经典
-        const val SKIN_BLACK_BLUE = "BLACK_BLUE"     //黑蓝
-        const val SKIN_BLACK_GREEN = "BLACK_GREEN"   //黑绿
-        const val SKIN_BLACK_RED = "BLACK_RED"       //黑红
-        const val SKIN_WHITE_BLUE = "WHITE_BLUE"     //白蓝
-        const val SKIN_WHITE_GREEN = "WHITE_GREEN"   //白绿
-    }
+    private var skinType: String = "CLASSIC"
 
     override fun initView(savedInstanceState: Bundle?) {
-        val skinType = mViewModel.getSkinType()
-        changeAppSkin(skinType)
+        skinType = mViewModel.getSkinData()
+        changeSkinType(skinType)
         val binding = TittleBarBackgroundBinding.inflate(LayoutInflater.from(context), mBinding.root, false)
-        binding.barroot.layoutParams.width = resources.displayMetrics.widthPixels-(20.dp2px)//获取屏幕高度
+        binding.barRoot.layoutParams.width = resources.displayMetrics.widthPixels - 20.dp2px
         mBinding.titleBar.loadDynamicsTitleBar(binding.root)
         binding.apply {
-            tvBack.clickNoRepeat {  findNavController().navigateUp() }
-            tvTitleRight.clickNoRepeat {  findNavController().navigateUp() }
+            tvBack.clickNoRepeat {
+                findNavController().navigateUp()
+            }
+            tvTitleRight.clickNoRepeat {
+                mViewModel.setSkinData(skinType)
+                findNavController().navigateUp()
+            }
         }
     }
 
     override fun initListener() {
         mBinding.layClassic.clickNoRepeat {
-            mViewModel.setSkinType(SKIN_CLASSIC)
+            skinType = SkinType.SKIN_CLASSIC.value
+            mViewModel.setSkinType(SkinType.SKIN_CLASSIC)
         }
         mBinding.layBlackBlue.clickNoRepeat {
-            mViewModel.setSkinType(SKIN_BLACK_BLUE)
+            skinType = SkinType.SKIN_BLACK_BLUE.value
+            mViewModel.setSkinType(SkinType.SKIN_BLACK_BLUE)
         }
         mBinding.layBlackRed.clickNoRepeat {
-            mViewModel.setSkinType(SKIN_BLACK_RED)
+            skinType = SkinType.SKIN_BLACK_RED.value
+            mViewModel.setSkinType(SkinType.SKIN_BLACK_RED)
         }
         mBinding.layBlackGreen.clickNoRepeat {
-            mViewModel.setSkinType(SKIN_BLACK_GREEN)
+            skinType = SkinType.SKIN_BLACK_GREEN.value
+            mViewModel.setSkinType(SkinType.SKIN_BLACK_GREEN)
         }
         mBinding.layWhiteGreen.clickNoRepeat {
-            mViewModel.setSkinType(SKIN_WHITE_GREEN)
+            skinType = SkinType.SKIN_WHITE_GREEN.value
+            mViewModel.setSkinType(SkinType.SKIN_WHITE_GREEN)
         }
         mBinding.layWhiteBlue.clickNoRepeat {
-            mViewModel.setSkinType(SKIN_WHITE_BLUE)
+            skinType = SkinType.SKIN_WHITE_BLUE.value
+            mViewModel.setSkinType(SkinType.SKIN_WHITE_BLUE)
         }
     }
 
     override fun createObserver() {
         mViewModel.skinType.observe(this) {
-            changeAppSkin(it)
+            changeSkinType(it)
         }
     }
 
-    private fun changeAppSkin(type: String) {
-        if (type == "") return
-        mBinding.radioClassic.isChecked = false
-        mBinding.radioBlackBlue.isChecked = false
-        mBinding.radioBlackRed.isChecked = false
-        mBinding.radioBlackGreen.isChecked = false
-        mBinding.radioWhiteGreen.isChecked = false
-        mBinding.radioWhiteBlue.isChecked = false
+    private fun changeSkinType(type: String) {
+        if ("" == type) return
+        mBinding.radioClassic.isSelected = false
+        mBinding.radioBlackBlue.isSelected = false
+        mBinding.radioBlackRed.isSelected = false
+        mBinding.radioBlackGreen.isSelected = false
+        mBinding.radioWhiteGreen.isSelected = false
+        mBinding.radioWhiteBlue.isSelected = false
         when (type) {
-            SKIN_CLASSIC -> mBinding.radioClassic.isChecked = true
-            SKIN_BLACK_BLUE -> mBinding.radioBlackBlue.isChecked = true
-            SKIN_BLACK_GREEN -> mBinding.radioBlackGreen.isChecked = true
-            SKIN_BLACK_RED -> mBinding.radioBlackRed.isChecked = true
-            SKIN_WHITE_BLUE -> mBinding.radioWhiteBlue.isChecked = true
-            SKIN_WHITE_GREEN -> mBinding.radioWhiteGreen.isChecked = true
+            SkinType.SKIN_CLASSIC.value -> mBinding.radioClassic.isSelected = true
+            SkinType.SKIN_BLACK_BLUE.value -> mBinding.radioBlackBlue.isSelected = true
+            SkinType.SKIN_BLACK_GREEN.value -> mBinding.radioBlackGreen.isSelected = true
+            SkinType.SKIN_BLACK_RED.value -> mBinding.radioBlackRed.isSelected = true
+            SkinType.SKIN_WHITE_BLUE.value -> mBinding.radioWhiteBlue.isSelected = true
+            SkinType.SKIN_WHITE_GREEN.value -> mBinding.radioWhiteGreen.isSelected = true
         }
     }
 }
