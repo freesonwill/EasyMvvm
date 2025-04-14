e5687e46a559c2041f5a6a290d61e39c3d36e708
zhangsan <zhangsan@youguess.com>
2025-04-14 11:24:40 +0700

    feat: add ExampleBean and ExampleDao for GameDatabase

---
+++ lib_database/src/main/java/arch/cayenne/lib/database/entity/ExampleBean.kt
@@ -0,0 +1,30 @@
+package arch.cayenne.lib.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+
+/**
+ * @author: zhangsan
+ * @date: 2025/4/14 10:35
+ * @description:
+ */
+@Entity(tableName = "SimpleBean")
+data class ExampleBean(
+    @PrimaryKey
+    val id: Int,
+    var colorType: ColorTypeEnum
+)
+
+enum class ColorTypeEnum {
+    RED, GREEN, YELLOW
+}
+
+class ColorTypeConverter {
+    @TypeConverter
+    fun fromTypeEnum(value: ColorTypeEnum): Int = value.ordinal
+
+    @TypeConverter
+    fun toTypeEnum(value: Int): ColorTypeEnum = ColorTypeEnum.entries[value]
+}
+
