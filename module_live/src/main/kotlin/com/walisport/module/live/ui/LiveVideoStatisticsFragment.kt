4dd76e0266302caf35f7cbe4bd5634fca51e6608
xiaoyang <xiaoyang@abc.com>
2025-04-15 15:09:06 +0700

    fix:直播-视频全屏播放时右侧的技术统计功能 - https://linear.app/forgames/issue/WLS-141

--- module_live/src/main/kotlin/com/walisport/module/live/ui/LiveVideoStatisticsFragment.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/LiveVideoStatisticsFragment.kt
@@ -2,9 +2,6 @@ package com.walisport.module.live.ui
 
 import android.os.Bundle
 import arch.cayenne.lib.base.ui.BaseFragment
-import arch.cayenne.lib.base.utils.LogUtilsExt.logd
-import arch.cayenne.lib.common.utils.ext.clickNoRepeat
-import com.walisport.module.live.databinding.FragmentLiveShareBinding
 import com.walisport.module.live.databinding.FragmentLiveStatisticsBinding
 import com.walisport.module.live.ui.viewmodel.LiveVideoViewModel
 import me.jessyan.autosize.internal.CancelAdapt
@@ -13,12 +10,13 @@ import kotlin.reflect.KClass
 /**
  * 视频横屏播放时的赛况页
  */
-class LiveVideoStatisticsFragment : BaseFragment<LiveVideoViewModel, FragmentLiveStatisticsBinding>() , CancelAdapt{
+class LiveVideoStatisticsFragment : BaseFragment<LiveVideoViewModel, FragmentLiveStatisticsBinding>(), CancelAdapt {
+
     override val vbClass: KClass<FragmentLiveStatisticsBinding> = FragmentLiveStatisticsBinding::class
     override val vmClass: KClass<LiveVideoViewModel> = LiveVideoViewModel::class
 
     override fun initView(savedInstanceState: Bundle?) {
-
+        mBinding.viewTechStatic.setFullScreenMode()
     }
 
     override fun initListener() {
@@ -26,7 +24,15 @@ class LiveVideoStatisticsFragment : BaseFragment<LiveVideoViewModel, FragmentLiv
     }
 
     override fun createObserver() {
-
+        mViewModel.liveVideoBean.observe(this){
+            //推送websocket数据发生变化时更新界面数据
+            mBinding.viewTechStatic.setTeamName("法国", "阿根廷")
+            mBinding.viewTechStatic.setScore("2:2")
+            mBinding.viewTechStatic.setAttackData(8, 5)
+            mBinding.viewTechStatic.setDangerAttackData(10, 12)
+            mBinding.viewTechStatic.setBallControlData(20, 13)
+            mBinding.viewTechStatic.setHomeAwayData(3, 4, 5, 3, 2, 4)
+        }
     }
 
     override fun onResume() {
