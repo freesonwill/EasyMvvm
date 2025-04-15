3e4e098c98e9b431bd7f53ec7e8cde5d339d9533
wangzai <dafeix64@gmail.com>
2025-04-16 00:04:37 +0700

    在直播详情页展示视频源选择页面

--- module_live/src/main/kotlin/com/walisport/module/live/ui/LiveVideoPortraitSourceFragment.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/LiveVideoPortraitSourceFragment.kt
@@ -2,17 +2,28 @@ package com.walisport.module.live.ui
 
 import android.os.Bundle
 import arch.cayenne.lib.base.ui.PositionedDialogFragment
+import arch.cayenne.lib.base.utils.LogUtilsExt.logd
+import arch.cayenne.lib.common.utils.ext.clickNoRepeat
 import com.walisport.module.live.databinding.FragmentLivePortraitSourceBinding
-import com.walisport.module.live.databinding.FragmentLiveVideoBinding
 import com.walisport.module.live.ui.viewmodel.LiveVideoViewModel
 import kotlin.reflect.KClass
 
-class LiveVideoPortraitSourceFragment:PositionedDialogFragment<LiveVideoViewModel, FragmentLivePortraitSourceBinding>() {
+class LiveVideoPortraitSourceFragment :
+    PositionedDialogFragment<LiveVideoViewModel, FragmentLivePortraitSourceBinding>() {
 
-    override val vbClass: KClass<FragmentLivePortraitSourceBinding> = FragmentLivePortraitSourceBinding::class
+    override val vbClass: KClass<FragmentLivePortraitSourceBinding> =
+        FragmentLivePortraitSourceBinding::class
     override val vmClass: KClass<LiveVideoViewModel> = LiveVideoViewModel::class
 
+
     override fun initView(savedInstanceState: Bundle?) {
+
+        with(mBinding){
+            ivClose.clickNoRepeat{
+                "mBinding.ivClose click".logd("LiveVideoPortraitSourceFragment")
+            }
+        }
+
     }
 
     override fun initListener() {
