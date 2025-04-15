496209d5d646b4f0ac3a8c50fc0db443df96f3f2
xiaoyang <xiaoyang@abc.com>
2025-04-15 16:25:39 +0700

    fix:赛况页联赛日程背景根据服务器下发数据动态改变 - https://linear.app/forgames/issue/WLS-144

--- module_live/src/main/kotlin/com/walisport/module/live/data/model/LeagueMatchBean.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/data/model/LeagueMatchBean.kt
@@ -6,6 +6,13 @@ package com.walisport.module.live.data.model
 
 data class LeagueMatchBean @JvmOverloads constructor(
     val id: Int = 0,
+    val match: MatchBean,
+    val startColor: String,//线性渐变开始色
+    val endColor: String     //线性渐变结束色
+)
+
+data class MatchBean @JvmOverloads constructor(
+    val id: Int = 0,
     val isWeek: Boolean,
     val weekDay: String,
     val timeStamp: Long,
@@ -13,6 +20,4 @@ data class LeagueMatchBean @JvmOverloads constructor(
     val awayTeamLogo: String,
     val homeTeamName: String,
     val awayTeamName: String,
-) {
-
-}
+)
--- module_live/src/main/kotlin/com/walisport/module/live/ui/viewmodel/LeagueViewModel.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/viewmodel/LeagueViewModel.kt
@@ -1,6 +1,17 @@
 package com.walisport.module.live.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import arch.cayenne.lib.base.data.viewmodel.BaseViewModel
+import com.walisport.module.live.data.model.LeagueMatchBean
 
 class LeagueViewModel : BaseViewModel() {
+
+    private val _leagueData = MutableLiveData<LeagueMatchBean>()
+    val leagueData: LiveData<LeagueMatchBean> get() = _leagueData
+
+
+    fun setData(bean: LeagueMatchBean) {
+        _leagueData.value = bean
+    }
 }
