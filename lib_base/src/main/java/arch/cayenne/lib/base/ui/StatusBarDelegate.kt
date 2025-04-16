112612be7c40e46e03421bd3a595c1cf25bf94da
tiger <aquan@aquan>
2025-04-16 18:30:42 +0700

    feature:WLS-166 1:状态栏优化,在全屏后返回上一级界面没有退出全屏模式

--- lib_base/src/main/java/arch/cayenne/lib/base/ui/StatusBarDelegate.kt
+++ lib_base/src/main/java/arch/cayenne/lib/base/ui/StatusBarDelegate.kt
@@ -24,6 +24,7 @@ class StatusBarDelegate(private val activity: Activity) : IStatusBar {
             immersionBar.hideBar(BarHide.FLAG_HIDE_BAR) //状态栏隐藏
         } else {
             immersionBar.statusBarColor(config.statusBarColor)//设置状态栏颜色
+            immersionBar.hideBar(BarHide.FLAG_SHOW_BAR) //状态栏隐藏
                 .fullScreen(false) //启用全屏模式
                // .hideBar(BarHide.FLAG_HIDE_NAVIGATION_BAR)   // 隐藏虚拟导航栏
                 .navigationBarColor(config.statusBarColor) // 设置虚拟导航栏颜色
