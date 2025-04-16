00c1b01eb56b6f093c8e689f5370b032ffd8a180
wangzai <dafeix64@gmail.com>
2025-04-16 15:47:29 +0700

    搜索module初始化

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(project(":module_setting"))
     implementation(project(":lib_videoplayer"))
     implementation(project(":module_live"))
+    implementation(project(":module_search"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.appcompat)
