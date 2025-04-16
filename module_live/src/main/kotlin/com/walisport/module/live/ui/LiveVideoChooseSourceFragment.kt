4bddc849e77cb660032b2b2f62a7670c0508d3ee
wangzai <dafeix64@gmail.com>
2025-04-16 10:03:15 +0700

    在直播详情页展示视频源选择页面

--- module_live/src/main/kotlin/com/walisport/module/live/ui/LiveVideoChooseSourceFragment.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/LiveVideoChooseSourceFragment.kt
@@ -3,10 +3,9 @@ package com.walisport.module.live.ui
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import arch.cayenne.lib.base.ui.BaseFragment
-import arch.cayenne.lib.base.utils.LogUtilsExt.logd
 import arch.cayenne.lib.common.utils.ext.DimensionExt.dp2px
 import com.walisport.module.live.compare.VideoSourceCompare
-import com.walisport.module.live.databinding.FragmentLiveChooseSourceBinding
+import com.walisport.module.live.databinding.FragmentLiveSourceLandscapeBinding
 import com.walisport.module.live.ui.adapter.LiveVideoSourceAdapter
 import com.walisport.module.live.ui.viewmodel.LiveVideoViewModel
 import me.jessyan.autosize.internal.CancelAdapt
@@ -16,9 +15,9 @@ import kotlin.reflect.KClass
  * 视频横屏播放时的选择数据源页
  */
 class LiveVideoChooseSourceFragment :
-    BaseFragment<LiveVideoViewModel, FragmentLiveChooseSourceBinding>(), CancelAdapt {
-    override val vbClass: KClass<FragmentLiveChooseSourceBinding> =
-        FragmentLiveChooseSourceBinding::class
+    BaseFragment<LiveVideoViewModel, FragmentLiveSourceLandscapeBinding>(), CancelAdapt {
+    override val vbClass: KClass<FragmentLiveSourceLandscapeBinding> =
+        FragmentLiveSourceLandscapeBinding::class
     override val vmClass: KClass<LiveVideoViewModel> = LiveVideoViewModel::class
 
     override fun initView(savedInstanceState: Bundle?) {
