82e70669393b8fd61c0f98a56a817cb4e0a6ba36
wangzai <dafeix64@gmail.com>
2025-04-16 11:13:22 +0700

    在直播详情页展示视频源选择页面

--- module_live/src/main/kotlin/com/walisport/module/live/ui/LiveVideoSourcePortraitFragment.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/LiveVideoSourcePortraitFragment.kt
@@ -1,8 +1,13 @@
 package com.walisport.module.live.ui
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 import arch.cayenne.lib.base.ui.PositionedDialogFragment
+import arch.cayenne.lib.common.utils.ext.DimensionExt.dp2px
 import arch.cayenne.lib.common.utils.ext.clickNoRepeat
 import com.walisport.module.live.compare.VideoSourceCompare
 import com.walisport.module.live.databinding.FragmentLiveSourcePortraitBinding
@@ -32,6 +37,7 @@ class LiveVideoSourcePortraitFragment :
                 adapter = LiveVideoSourceHorizontalAdapter(VideoSourceCompare()).apply {
                     post {
 //                        addItemDecoration(LinearSpacingItemDecoration(16.dp2px, 8.dp2px))
+                        addItemDecoration( HorizontalItemDecoration())
                         submitList(mViewModel.sources.value)
                     }
 
@@ -67,6 +73,28 @@ class LiveVideoSourcePortraitFragment :
                     }
                 }
             }
+        }
+    }
+
+    class HorizontalItemDecoration(
+        private val spacing: Int = 12.dp2px ,         // 常规间距大小（像素）
+        private val leftRight: Int = 16.dp2px,         // 左右边距（像素）
+        private val bottomSpacing: Int = 6.dp2px,   // 最后一个 item 的右边距离（像素）
+    ) : ItemDecoration() {
+        override fun getItemOffsets(
+            outRect: Rect,
+            view: View,
+            parent: RecyclerView,
+            state: RecyclerView.State
+        ) {
+            val position = parent.getChildAdapterPosition(view) // item 位置
+            val itemCount = parent.adapter?.itemCount ?: 0 // 总 item 数
+
+            // 包含边缘的情况
+            outRect.top = 0
+            outRect.bottom = 0
+            outRect.left = if (position==0) leftRight else 0
+            outRect.right = if (position == itemCount - 1) bottomSpacing else spacing
         }
     }
 }
