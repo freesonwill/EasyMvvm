87bf46a56b5670121f23427fefc4d1cba6edaf11
tiger <aquan@aquan>
2025-04-22 16:20:51 +0700

    feature:WLS-207 1:直播-阵容列表

--- module_live/src/main/kotlin/com/walisport/module/live/ui/LiveLineupFragment.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/LiveLineupFragment.kt
@@ -1,18 +1,25 @@
 package com.walisport.module.live.ui
 
+import android.annotation.SuppressLint
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.constraintlayout.widget.ConstraintLayout
 import arch.cayenne.lib.base.ui.BaseFragment
 import arch.cayenne.lib.base.ui.viewBind
 import arch.cayenne.lib.base.utils.LogUtils
+import arch.cayenne.lib.common.databinding.TittleBarDefaultBinding
 import arch.cayenne.lib.common.extension.sharedViewModel
 import com.walisport.module.live.R
 import com.walisport.module.live.databinding.FragmentLiveLineupBinding
 import com.walisport.module.live.databinding.FragmentLiveMainBinding
+import com.walisport.module.live.databinding.LineupHeadBinding
+import com.walisport.module.live.databinding.LineupHeadBlueBinding
 import com.walisport.module.live.ui.viewmodel.LiveLineupViewModel
 import com.walisport.module.live.viewmodel.LiveMainViewModel
 import galaxy.client.proto.Sloth
@@ -29,7 +36,8 @@ class LiveLineupFragment : BaseFragment<LiveLineupViewModel, FragmentLiveLineupB
     private val mainViewModel: LiveMainViewModel by sharedViewModel<LiveMainViewModel, LiveMainFragment>()
 
     override fun initView(savedInstanceState: Bundle?) {
-        mViewModel.geMatchLineupDetail(mainViewModel.matchId)
+//        mViewModel.geMatchLineupDetail(mainViewModel.matchId)
+        mViewModel.geMatchLineupDetail(458436)
     }
 
     override fun initListener() {
@@ -40,13 +48,68 @@ class LiveLineupFragment : BaseFragment<LiveLineupViewModel, FragmentLiveLineupB
         mViewModel.matchLineupDetail.observe(viewLifecycleOwner) {
             it?.let {
                 upData(it)
-            }?:run {
+            } ?: run {
 
             }
         }
     }
 
+    // repeated Player home = 6;        // 主队阵型球员列表
+    //  repeated Player away = 7;        // 客队阵型球员列表
+    @SuppressLint("MissingInflatedId")
     private fun upData(data: Sloth.MatchLineupDetail) {
-        LogUtils.dTag(TAG,"MatchLineupDetail----->${data}")
-    }
+        LogUtils.dTag(TAG, "MatchLineupDetail----->${data}")
+        data.homeOrBuilderList.forEach { i ->
+            //是否是首发
+            if(i.first==1) {
+                val newView = LayoutInflater.from(context).inflate(R.layout.lineup_head, mBinding.sclLineupItemTop, false)
+                val sclLineupItem = newView.findViewById<LinearLayoutCompat>(R.id.clLineupHead)
+                val x = mViewModel.lineupArrangementX(
+                    sclLineupItem.width,
+                    mBinding.sclLineupItemTop.width,
+                    i.x
+                )
+                val y = mViewModel.lineupArrangementY(
+                    sclLineupItem.height,
+                    mBinding.sclLineupItemTop.height,
+                    i.y
+                )
+                val params = RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+                ).apply {
+                    leftMargin = x // X 坐标
+                    topMargin = y // Y 坐标
+                }
+                newView.layoutParams = params
+                mBinding.sclLineupItemTop.addView(sclLineupItem)
+            }
+        }
+        data.awayOrBuilderList.forEach { i ->
+            //是否是首发
+            if(i.first==1) {
+                val newView = LayoutInflater.from(context).inflate(R.layout.lineup_head_blue, mBinding.sclLineupItemBottom, false)
+                val sclLineupItem = newView.findViewById<ConstraintLayout>(R.id.clLineupHead)
+                val x = mViewModel.lineupArrangementX(
+                    sclLineupItem.width,
+                    mBinding.sclLineupItemBottom.width,
+                    i.x
+                )
+                val y = mViewModel.lineupArrangementY(
+                    sclLineupItem.height,
+                    mBinding.sclLineupItemBottom.height,
+                    i.y
+                )
+                val params = RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+                ).apply {
+                    leftMargin = x // X 坐标
+                    topMargin = y // Y 坐标
+                }
+                newView.layoutParams = params
+                mBinding.sclLineupItemBottom.addView(newView)
+            }
+
+    }}
 }
--- module_live/src/main/kotlin/com/walisport/module/live/ui/viewmodel/LiveLineupViewModel.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/viewmodel/LiveLineupViewModel.kt
@@ -9,13 +9,46 @@ import galaxy.client.proto.Sloth
 import kotlinx.coroutines.launch
 import org.koin.core.component.inject
 import org.koin.core.parameter.parametersOf
+
 class LiveLineupViewModel : BaseViewModel() {
     private val repository: LiveLineupRepository by inject { parametersOf(viewModelScope) }
     private val _matchLineupDetail = MutableLiveData<Sloth.MatchLineupDetail?>()
     val matchLineupDetail: LiveData<Sloth.MatchLineupDetail?> = _matchLineupDetail
-    fun geMatchLineupDetail(matchId: Long ){
+    fun geMatchLineupDetail(matchId: Long) {
         viewModelScope.launch {
-            _matchLineupDetail.value =  repository.getMatchLiveReq(matchId)
+            _matchLineupDetail.value = repository.getMatchLiveReq(matchId)
         }
+    }
+
+    /**
+     * 阵容排列
+     * xMax = 100
+     * yMin = 100
+     * //队员头像名字控件宽高/2
+     * headViewHeight =  50/2
+     * headViewWidth = 50/2
+     * //获取到一份控件的xy轴
+     * sX = 1920
+     * sY = 2330
+     * viewHeight = sX/100
+     * viewWidth =sY/100
+     * //根据后台返回的xy 例如; x = 12 y=50 显示具体位置
+     * serviceX = 12
+     * serviceY = 50
+     * x = screenX*screenX-viewHeight
+     * y = screenY*screenY-viewWidth
+     */
+    fun lineupArrangementX(headViewNumber: Int, viewNumber: Int, serviceNumber: Int): Int {
+        val max = 100//最大份数
+        val oneViewNumber = viewNumber / max//每一份的像素
+        val headNumber = headViewNumber / 2//队员头像名字控件高度
+        return oneViewNumber * serviceNumber - headNumber-headNumber
+    }
+
+    fun lineupArrangementY(headViewNumber: Int, viewNumber: Int, serviceNumber: Int): Int {
+        val max = 100//最大份数
+        val oneViewNumber = viewNumber / max//每一份的像素
+        val headNumber = headViewNumber / 2//队员头像名字控件高度
+        return oneViewNumber * serviceNumber - headNumber
     }
 }
