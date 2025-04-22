65577657fb361d093faac43e68f651415b2e18c7
joseph.tseng <joseph.tseng@skylinetw.com>
2025-04-22 16:40:23 +0800

    chore:刪除無用ext

--- module_home/src/main/java/arch/cayenne/module/home/extension/DataExtension.kt
+++
@@ -1,9 +0,0 @@
-package arch.cayenne.module.home.extension
-
-fun String.getHomeScore(): String {
-    return if (this.contains(":")) this.substringBefore(":").trim() else ""
-}
-
-fun String.getAwayScore(): String {
-    return if (this.contains(":")) this.substringAfter(":").trim() else ""
-}
