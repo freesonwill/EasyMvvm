f548befc373818c5daea0207f7c5d428e6a2ff00
xiaoyang <xiaoyang@abc.com>
2025-04-22 13:19:12 +0700

    feature:factory改成singleton后引发的闪退

--- lib_common/src/main/java/arch/cayenne/lib/common/CommonModuleInitializer.kt
+++ lib_common/src/main/java/arch/cayenne/lib/common/CommonModuleInitializer.kt
@@ -1,13 +1,13 @@
 package arch.cayenne.lib.common
 
 import android.content.Context
-import androidx.startup.Initializer
 import arch.cayenne.lib.base.data.DefaultInitializer
 import arch.cayenne.lib.common.data.UserDataManager
 import com.tencent.mmkv.MMKV
 import org.koin.core.context.loadKoinModules
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import java.util.Collections.singleton
 
 class CommonModuleInitializer : DefaultInitializer<String> {
 
@@ -20,6 +20,6 @@ class CommonModuleInitializer : DefaultInitializer<String> {
     }
 
     private val moduleList: List<Module> = listOf(module {
-        factory { UserDataManager() }
+        singleton { UserDataManager() }
     })
 }
