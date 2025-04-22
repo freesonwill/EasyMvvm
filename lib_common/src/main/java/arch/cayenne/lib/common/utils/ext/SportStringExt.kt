6b0fb782df0afb09f92da4526f7054f91704aedf
joseph.tseng <joseph.tseng@skylinetw.com>
2025-04-22 18:08:24 +0800

    feature:餘額顯示

--- lib_common/src/main/java/arch/cayenne/lib/common/utils/ext/SportStringExt.kt
+++ lib_common/src/main/java/arch/cayenne/lib/common/utils/ext/SportStringExt.kt
@@ -2,6 +2,7 @@ package arch.cayenne.lib.common.utils.ext
 
 import java.math.BigDecimal
 import java.math.RoundingMode
+import java.text.DecimalFormat
 
 object SportStringExt {
 
@@ -49,6 +50,17 @@ object SportStringExt {
 
     fun String.getAwayScore(): String {
         return if (this.contains(":")) this.substringAfter(":").trim() else ""
+    }
+
+    fun String.toDecimalNumber(decimalPlaces: Int = 2): String? {
+        return try {
+            val value = this.toDouble()
+            val pattern = "#,##0." + "0".repeat(decimalPlaces)  // e.g., "#,##0.00"
+            val decimalFormat = DecimalFormat(pattern)
+            decimalFormat.format(value)
+        } catch (e: NumberFormatException) {
+            null
+        }
     }
 
 }
