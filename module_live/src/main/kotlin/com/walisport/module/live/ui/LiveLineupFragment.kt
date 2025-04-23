4f70b48f4b59a7647a10c734d12a3dad039666a3
tiger <aquan@aquan>
2025-04-23 10:43:48 +0700

    feature:WLS-207 1:直播间-阵容排列算法及数据对接

--- module_live/src/main/kotlin/com/walisport/module/live/ui/LiveLineupFragment.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/LiveLineupFragment.kt
@@ -1,36 +1,23 @@
 package com.walisport.module.live.ui
 
 import android.annotation.SuppressLint
-import android.annotation.SuppressLint
 import androidx.fragment.app.viewModels
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
-import androidx.appcompat.widget.LinearLayoutCompat
-import androidx.constraintlayout.widget.ConstraintLayout
 import arch.cayenne.lib.base.ui.BaseFragment
-import arch.cayenne.lib.base.ui.viewBind
 import arch.cayenne.lib.base.utils.LogUtils
-import arch.cayenne.lib.common.databinding.TittleBarDefaultBinding
 import arch.cayenne.lib.common.extension.sharedViewModel
+import arch.cayenne.lib.common.ui.widget.DynamicStateLayout
 import arch.cayenne.lib.common.utils.ext.DimensionExt.dp2px
-import arch.cayenne.lib.skin.widget.SportTextView
+import arch.cayenne.lib.common.utils.ext.ResourceExt.getString
 import com.bumptech.glide.Glide
 import com.walisport.module.live.R
 import com.walisport.module.live.databinding.FragmentLiveLineupBinding
-import com.walisport.module.live.databinding.FragmentLiveMainBinding
-import com.walisport.module.live.databinding.LineupHeadBinding
-import com.walisport.module.live.databinding.LineupHeadBlueBinding
 import com.walisport.module.live.ui.viewmodel.LiveLineupViewModel
 import com.walisport.module.live.viewmodel.LiveMainViewModel
 import galaxy.client.proto.Sloth
-import okio.ByteString.Companion.encodeUtf8
-import org.koin.androidx.viewmodel.ext.android.viewModel
 import kotlin.reflect.KClass
 
 /**
@@ -62,8 +49,8 @@ class LiveLineupFragment : BaseFragment<LiveLineupViewModel, FragmentLiveLineupB
     //  repeated Player away = 7;        // 客队阵型球员列表
     @SuppressLint("MissingInflatedId", "CutPasteId")
     private fun upData(data: Sloth.MatchLineupDetail) {
-        Glide.with(this).load(data.homeLogo).error(R.drawable.lineup_head).into( mBinding.ivNationalFlagTop)
-        Glide.with(this).load(data.awayLogo).error(R.drawable.lineup_head).into( mBinding.ivNationalFlagBottom)
+        Glide.with(this).load(data.homeLogo).error(R.drawable.icon_lineuup_head).into( mBinding.ivNationalFlagTop)
+        Glide.with(this).load(data.awayLogo).error(R.drawable.icon_lineuup_head).into( mBinding.ivNationalFlagBottom)
         mBinding.tvNationalNameTop.text = data.homeFormation
         mBinding.tvNationalNameBottom.text = data.awayFormation
         val headHeightViewNumber = 44.dp2px
@@ -134,11 +121,4 @@ class LiveLineupFragment : BaseFragment<LiveLineupViewModel, FragmentLiveLineupB
             }
 
         }}
-import android.widget.ImageView
-import android.widget.RelativeLayout
-import android.widget.TextView
-import androidx.appcompat.widget.LinearLayoutCompat
-import androidx.constraintlayout.widget.ConstraintLayout
-import arch.cayenne.lib.common.databinding.TittleBarDefaultBinding
-import okio.ByteString.Companion.encodeUtf8
 }
