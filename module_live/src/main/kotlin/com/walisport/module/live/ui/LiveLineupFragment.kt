e6dfe95a272277c52e41b84485cb32a68bec5fd6
tiger <aquan@aquan>
2025-04-24 16:54:30 +0700

    feature:WLS-232 1:直播间-首页数据对接 2:直播间-赛况数据对接

--- module_live/src/main/kotlin/com/walisport/module/live/ui/LiveLineupFragment.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/LiveLineupFragment.kt
@@ -65,16 +65,17 @@ class LiveLineupFragment : BaseFragment<LiveLineupViewModel, FragmentLiveLineupB
         Glide.with(this).load(data.awayLogo).into( mBinding.ivNationalFlagBottom)
         mBinding.tvNationalNameTop.text = data.homeFormation
         mBinding.tvNationalNameBottom.text = data.awayFormation
-
         Glide.with(this).load(data.homeLogo).into( mBinding.homeIncidentsLogo)
         Glide.with(this).load(data.awayLogo).into( mBinding.awayIncidentsLogo)
-        mBinding.homeIncidentsName.text = data.homeFormation
-        mBinding.awayIncidentsName.text = data.awayFormation
-
         Glide.with(this).load(data.homeLogo).into( mBinding.homeSubstituteLogo)
         Glide.with(this).load(data.awayLogo).into( mBinding.awaySubstituteLogo)
-        mBinding.homeSubstituteName.text = data.homeFormation
-        mBinding.awaySubstituteName.text = data.awayFormation
+        mainViewModel.match?.basicInfo.let {
+            mBinding.homeSubstituteName.text =it?.homeTeam
+            mBinding.awaySubstituteName.text = it?.awayTeam
+            mBinding.homeIncidentsName.text =it?.homeTeam
+            mBinding.awayIncidentsName.text = it?.awayTeam
+        }
+
         LogUtils.dTag(TAG, "MatchLineupDetail----->${data}")
         data.homeOrBuilderList.forEach { i ->
             //是否是首发
