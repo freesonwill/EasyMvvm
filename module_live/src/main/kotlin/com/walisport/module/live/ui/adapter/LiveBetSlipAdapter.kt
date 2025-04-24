307cb29238f24b2b47fe41b65af1df0acd690edb
wenxi <russelllin465@gmail.com>
2025-04-24 16:39:46 +0700

    feature: 注单页数据和页面适配 #WLS-203

--- module_live/src/main/kotlin/com/walisport/module/live/ui/adapter/LiveBetSlipAdapter.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/adapter/LiveBetSlipAdapter.kt
@@ -2,10 +2,15 @@ package com.walisport.module.live.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import arch.cayenne.lib.base.adapter.BaseAdapter
 import arch.cayenne.lib.base.viewholder.BaseViewHolder
+import com.walisport.module.live.R
 import com.walisport.module.live.compare.LiveBetSlipCompare
 import com.walisport.module.live.data.model.LiveBetSlipEnum
 import com.walisport.module.live.databinding.AdapterLiveBetSlipConfirmBinding
@@ -15,97 +20,55 @@ import com.walisport.module.live.databinding.AdapterLiveBetSlipSettledBinding
 import com.walisport.module.live.databinding.AdapterLiveBetSlipUnsettleBinding
 import galaxy.common.proto.Common
 import galaxy.common.proto.Common.Order
+import org.w3c.dom.Text
 
 class LiveBetSlipAdapter(type: LiveBetSlipEnum) :
     BaseAdapter<Common.Order, LiveBetSlipAdapter.LiveBetSlipViewHolder, ViewBinding>(
         LiveBetSlipCompare()
     ) {
     private val betSlipType = type
-    private val selectionAdapter = LiveBetSlipSelectionAdapter(betSlipType, true)
+    private val METHOD_INIT = 0
+    private val METHOD_UPDATE = 1
+    private val METHOD_ADAPTER = 2
 
-    inner class LiveBetSlipViewHolder(binding: ViewBinding) : BaseViewHolder(binding) {
 
-        fun initManager() {
-            val manager = LinearLayoutManager(binding.root.context)
-
-            when (binding) {
-                is AdapterLiveBetSlipUnsettleBinding -> {
-                    val nBinding = binding as AdapterLiveBetSlipUnsettleBinding
-                    nBinding.recyclerSelection.also {
-                        it.layoutManager = manager
-                        it.adapter = selectionAdapter
-                    }
-                }
-
-                is AdapterLiveBetSlipConfirmBinding -> {
-                    val nBinding = binding as AdapterLiveBetSlipConfirmBinding
-                    nBinding.recyclerSelection.also {
-                        it.layoutManager = manager
-                        it.adapter = selectionAdapter
-                    }
-                }
-
-                is AdapterLiveBetSlipSettledBinding -> {
-                    val nBinding = binding as AdapterLiveBetSlipSettledBinding
-                    nBinding.recyclerSelection.also {
-                        it.layoutManager = manager
-                        it.adapter = selectionAdapter
-                    }
-                }
-
-                is AdapterLiveBetSlipReserveBinding -> {
-                    val nBinding = binding as AdapterLiveBetSlipReserveBinding
-                    nBinding.recyclerSelection.also {
-                        it.layoutManager = manager
-                        it.adapter = selectionAdapter
-                    }
-                }
-
-                is AdapterLiveBetSlipInvalidBinding -> {
-                    val nBinding = binding as AdapterLiveBetSlipInvalidBinding
-                    nBinding.recyclerSelection.also {
-                        it.layoutManager = manager
-                        it.adapter = selectionAdapter
-                    }
-                }
+    private fun updateInvalid(
+        item: Order,
+        bind: AdapterLiveBetSlipInvalidBinding,
+        isReserve: Boolean
+    ) {
+        with(bind) {
+            if (isReserve) {
+                tvUnit1.text =
+                    ContextCompat.getString(bind.root.context, R.string.live_bet_reserve_odds)
+                tvUnit2.text =
+                    ContextCompat.getString(bind.root.context, R.string.live_bet_reserve_bet)
+                tvUnit3.text =
+                    ContextCompat.getString(bind.root.context, R.string.live_bet_except_max_win)
+                tvUnit4.isVisible = false
+                tvUnit4Value.isVisible = false
+//                tvUnit1Value.text = item.betId //预约赔率
+//                tvUnit2Value.text = item.odds  //预约投注
+//                tvUnit3Value.text = item.betAmount //预约最高可赢
+            } else {
+                tvUnit1.text = ContextCompat.getString(bind.root.context, R.string.live_bet_bet_num)
+                tvUnit2.text = ContextCompat.getString(bind.root.context, R.string.live_bet_odds)
+                tvUnit3.text = ContextCompat.getString(bind.root.context, R.string.live_bet_on)
+                tvUnit4.text =
+                    ContextCompat.getString(bind.root.context, R.string.live_bet_except_max_win)
+                tvUnit1Value.text = item.betId
+                tvUnit2Value.text = item.odds
+                tvUnit3Value.text = item.betAmount
+//            TODO 预计最高可赢
+                tvUnit4Value.text = item.returnAmount
             }
-        }
-
-        fun initAdapter() {
-            val list = arrayListOf(
-                Common.OrderSelection.newBuilder().build(),
-                Common.OrderSelection.newBuilder().build(),
-                Common.OrderSelection.newBuilder().build()
-            )
-            selectionAdapter.submitList(list)
-        }
-
-
-        fun updateUnSettle(order: Common.Order) {
-            val nBinding = binding as AdapterLiveBetSlipUnsettleBinding
-            with(nBinding) {
-                val selection = getSelections(order).first()
-                val match = getMatch(selection)
-//                Glide.with(root.context).load(match.tournamentIcon).into(betUnsettledIvBall)
-//                betUnsettledTvRace.text = match.matchName
-//                betUnsettledTvIntroduce.text = selection.selectionName
-//                betUnsettledTvStatus.text = selection.marketName
-//                betUnsettledTvStart.text =  "${match.startTime}"
-//                betUnsettledTvAodds.text = selection.odds
-//
-//                betUnsettledTvBetcodeValue.text = order.betId
-//                betUnsettledTvOddsValue.text = order.odds
-//                betUnsettledTvBettingValue.text = order.betAmount
-//                betUnsettledTvExceptValue.text = order.returnAmount
-            }
-
         }
 
     }
 
-    override fun convertPlus(holder: LiveBetSlipViewHolder, binding: ViewBinding, position: Int) {
-        holder.initAdapter()
 
+    override fun convertPlus(holder: LiveBetSlipViewHolder, binding: ViewBinding, position: Int) {
+        holder.managerView(METHOD_ADAPTER,position,getItem(position))
     }
 
     override fun createViewBinding(
@@ -148,7 +111,7 @@ class LiveBetSlipAdapter(type: LiveBetSlipEnum) :
 
     override fun createViewHolder(binding: ViewBinding, viewType: Int): LiveBetSlipViewHolder {
         val holder = LiveBetSlipViewHolder(binding)
-        holder.initManager()
+        holder.managerView(METHOD_INIT, -1)
         return holder
     }
 
@@ -157,5 +120,129 @@ class LiveBetSlipAdapter(type: LiveBetSlipEnum) :
     private fun getMatch(selection: Common.OrderSelection) = selection.matchBasic
 
     private fun getEarlySettlePrice(order: Order) = order.earlySettlePrice
+
+    inner class LiveBetSlipViewHolder(binding: ViewBinding) : BaseViewHolder(binding) {
+
+        fun managerView(method: Int, position: Int, item: Order? = null) {
+            when (binding) {
+                is AdapterLiveBetSlipUnsettleBinding -> {
+                    val nBinding = binding as AdapterLiveBetSlipUnsettleBinding
+                    when (method) {
+                        METHOD_INIT -> initRecycler(nBinding.recyclerSelection)
+                        METHOD_ADAPTER -> initAdapter(nBinding.recyclerSelection)
+                        METHOD_UPDATE -> item?.let { nItem ->
+                            nBinding.also {
+                                updateData(
+                                    nItem,
+                                    tvCode = it.betUnsettledTvBetcodeValue,
+                                    tvOdds = it.betUnsettledTvOddsValue,
+                                    tvBet = it.betUnsettledTvBettingValue,
+                                    tvExpectMaxWin = it.betUnsettledTvExceptValue
+                                )
+                            }
+                        }
+
+                        else -> {}
+                    }
+                }
+
+                is AdapterLiveBetSlipConfirmBinding -> {
+                    val nBinding = binding as AdapterLiveBetSlipConfirmBinding
+                    when (method) {
+                        METHOD_INIT -> initRecycler(nBinding.recyclerSelection)
+                        METHOD_ADAPTER -> initAdapter(nBinding.recyclerSelection)
+                        METHOD_UPDATE -> item?.let { nItem ->
+                            nBinding.also {
+                                updateData(
+                                    nItem,
+                                    tvCode = it.betConfirmTvBetcodeValue,
+                                    tvOdds = it.betConfirmTvOddsValue,
+                                    tvBet = it.betConfirmTvBettingValue,
+                                    tvExpectMaxWin = it.betConfirmTvExceptValue
+                                )
+                            }
+                        }
+
+                        else -> {}
+                    }
+                }
+
+                is AdapterLiveBetSlipSettledBinding -> {
+                    val nBinding = binding as AdapterLiveBetSlipSettledBinding
+                    when (method) {
+                        METHOD_INIT -> initRecycler(nBinding.recyclerSelection)
+                        METHOD_ADAPTER -> initAdapter(nBinding.recyclerSelection)
+                        METHOD_UPDATE -> item?.let { nItem ->
+                            nBinding.also {
+                                updateData(
+                                    nItem,
+                                    tvCode = it.betSettledTvBetcodeValue,
+                                    tvOdds = it.betSettledTvOddsValue,
+                                    tvBet = it.betSettledTvBettingValue,
+                                    tvExpectMaxWin = it.betSettledTvExceptValue
+                                )
+                            }
+                        }
+
+                        else -> {}
+                    }
+                }
+
+                is AdapterLiveBetSlipInvalidBinding -> {
+                    val nBinding = binding as AdapterLiveBetSlipInvalidBinding
+                    when (method) {
+                        METHOD_INIT -> initRecycler(nBinding.recyclerSelection)
+                        METHOD_ADAPTER -> initAdapter(nBinding.recyclerSelection)
+                        METHOD_UPDATE -> item?.let { updateInvalid(item, nBinding, false) }
+                        else -> {}
+                    }
+                }
+
+                is AdapterLiveBetSlipReserveBinding -> {
+                }
+            }
+        }
+
+        fun initRecycler(recyclerView: RecyclerView) {
+            val manager = LinearLayoutManager(binding.root.context)
+            val adapter = LiveBetSlipSelectionAdapter(betSlipType)
+            recyclerView.also {
+                it.layoutManager = manager
+                it.itemAnimator = null
+                it.adapter = adapter
+            }
+        }
+
+        fun initAdapter(recyclerView: RecyclerView) {
+            val list = arrayListOf(
+                Common.OrderSelection.newBuilder().build(),
+                Common.OrderSelection.newBuilder().build(),
+                Common.OrderSelection.newBuilder().build()
+            )
+            recyclerView.adapter?.let {
+                val adapter = it as LiveBetSlipSelectionAdapter
+                adapter.submitList(list)
+            }
+        }
+
+
+        private fun updateData(
+            order: Order,
+            tvCode: TextView? = null,
+            tvOdds: TextView? = null,
+            tvBet: TextView? = null,
+            tvExpectMaxWin: TextView? = null,
+            tvPartEarlySettled: TextView? = null,
+            tvWinLoseAmount: TextView? = null
+        ) {
+            tvCode?.text = order.betId
+            tvOdds?.text = order.odds
+            tvBet?.text = order.betAmount
+//            TODO 预计最高可赢
+            tvExpectMaxWin?.text = order.returnAmount
+            tvPartEarlySettled?.text = order.earlySettlePrice.price
+            tvWinLoseAmount?.text = "0" // order.betAmount-order.returnAmount
+        }
+    }
 
 }
--- module_live/src/main/kotlin/com/walisport/module/live/ui/adapter/LiveBetSlipSelectionAdapter.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/adapter/LiveBetSlipSelectionAdapter.kt
@@ -1,13 +1,18 @@
 package com.walisport.module.live.ui.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.constraintlayout.widget.Group
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.viewbinding.ViewBinding
 import arch.cayenne.lib.base.adapter.BaseAdapter
 import arch.cayenne.lib.base.viewholder.BaseViewHolder
 import arch.cayenne.lib.skin.widget.SportView
+import com.walisport.module.live.R
 import com.walisport.module.live.compare.LiveBetSlipSelectionCompare
 import com.walisport.module.live.data.model.LiveBetSlipEnum
 import com.walisport.module.live.databinding.ItemLiveBetSlipConfirmBinding
@@ -15,42 +20,104 @@ import com.walisport.module.live.databinding.ItemLiveBetSlipInvalidBinding
 import com.walisport.module.live.databinding.ItemLiveBetSlipReserveBinding
 import com.walisport.module.live.databinding.ItemLiveBetSlipSettledBinding
 import com.walisport.module.live.databinding.ItemLiveBetSlipUnsettleBinding
+import com.walisport.module.live.utils.LiveDateUtil
 import galaxy.common.proto.Common
+import org.w3c.dom.Text
 
-class LiveBetSlipSelectionAdapter(betSlipType: LiveBetSlipEnum, val showGradient: Boolean) :
+class LiveBetSlipSelectionAdapter(betSlipType: LiveBetSlipEnum) :
     BaseAdapter<Common.OrderSelection, LiveBetSlipSelectionAdapter.LiveBetSlipSelectionViewHolder, ViewBinding>(
         LiveBetSlipSelectionCompare()
     ) {
-    val betType = betSlipType
-    val gradient = showGradient
+    private val betType = betSlipType
+    private var gradient = false
+
+    fun updateGradient(flag: Boolean) {
+        this.gradient = flag
+    }
+
 
     inner class LiveBetSlipSelectionViewHolder(binding: ViewBinding) : BaseViewHolder(binding) {
 
-        fun initView(position: Int, count: Int) {
+        fun initView(item: Common.OrderSelection, position: Int, count: Int) {
             when (binding) {
                 is ItemLiveBetSlipUnsettleBinding -> {
                     val nBinding = binding as ItemLiveBetSlipUnsettleBinding
-                    configView(nBinding.line, nBinding.groupGradient, position, count)
+                    configView(
+                        nBinding.line, nBinding.groupGradient, position, count
+                    )
+                    nBinding.also {
+                        updateData(
+                            item,
+                            it.betUnsettledIvBall,
+                            it.betUnsettledTvRace,
+                            it.betUnsettledTvIntroduce,
+                            it.betUnsettledTvAodds,
+                            it.betUnsettledTvStatus,
+                            it.betUnsettledTvScore,
+                            it.betUnsettledTvStart
+                        )
+                    }
                 }
 
                 is ItemLiveBetSlipConfirmBinding -> {
                     val nBinding = binding as ItemLiveBetSlipConfirmBinding
-                    configView(nBinding.line, nBinding.groupGradient, position, count)
+                    configView(
+                        nBinding.line, nBinding.groupGradient, position, count
+                    )
+                    nBinding.also {
+                        updateData(
+                            item,
+                            it.betConfirmIvBall,
+                            it.betConfirmTvRace,
+                            it.betConfirmTvIntroduce,
+                            it.betConfirmTvAodds,
+                            it.betConfirmTvStatus,
+                            it.betConfirmTvScore,
+                            it.betConfirmTvStart
+                        )
+                    }
                 }
 
                 is ItemLiveBetSlipSettledBinding -> {
                     val nBinding = binding as ItemLiveBetSlipSettledBinding
-                    configView(nBinding.line, nBinding.groupGradient, position, count)
+                    configView(
+                        nBinding.line, nBinding.groupGradient, position, count
+                    )
+                    nBinding.also {
+                        updateData(
+                            item,
+                            it.betSettledIvBall,
+                            it.betSettledTvRace,
+                            it.betSettledTvIntroduce,
+                            it.betSettledTvAodds,
+                            it.betSettledTvStatus,
+                            it.betSettledTvScore,
+                            tvScore1 = it.betSettledTvScore1
+                        )
+                    }
                 }
 
                 is ItemLiveBetSlipReserveBinding -> {
-                    val nBinding = binding as ItemLiveBetSlipReserveBinding
-                    configView(nBinding.line, nBinding.groupGradient, position, count)
+
                 }
 
                 is ItemLiveBetSlipInvalidBinding -> {
                     val nBinding = binding as ItemLiveBetSlipInvalidBinding
-                    configView(nBinding.line, nBinding.groupGradient, position, count)
+                    configView(
+                        nBinding.line, nBinding.groupGradient, position, count
+                    )
+                    nBinding.also {
+                        updateData(
+                            item,
+                            it.betInvalidIvBall,
+                            it.betInvalidTvRace,
+                            it.betInvalidTvIntroduce,
+                            it.betInvalidTvAodds,
+                            it.betInvalidTvMatchStatus,
+                            it.betInvalidTvScore,
+                            it.betInvalidTvStart
+                        )
+                    }
                 }
             }
         }
@@ -60,59 +127,79 @@ class LiveBetSlipSelectionAdapter(betSlipType: LiveBetSlipEnum, val showGradient
             group.isVisible = position == 2 && gradient
         }
 
+        @SuppressLint("SetTextI18n")
+        private fun updateData(
+            item: Common.OrderSelection,
+            ivIcon: ImageView,
+            tvMatch: TextView,
+            tvSelection: TextView,
+            tvOdds: TextView,
+            tvStatus: TextView,
+            tvScore: TextView,
+            tvStart: TextView? = null,
+            tvScore1: TextView? = null,
+            isExpectedOdds: Boolean = false
+        ) {
+            //TODO icon没有数据
+            // Glide.with(root.context).load(match.tournamentIcon).into(betUnsettledIvBall)
+            val match = item.matchBasic
+            val expectedOdds = if (isExpectedOdds) ContextCompat.getString(
+                binding.root.context,
+                R.string.live_bet_except_odds
+            ) + "  " else ""
+
+            tvMatch.text = match.matchName
+            tvSelection.text = item.selectionName
+            tvOdds.text = expectedOdds + "@" + item.odds
+//                TODO 滚球不清楚
+            tvStatus.text = "滚球"
+            tvScore.text = ContextCompat.getString(
+                binding.root.context,
+                R.string.live_bet_full_handicap
+            ) + "  " + item.betScore
+            tvStart?.text =
+                LiveDateUtil.getMDHm(match.startTime)
+            tvScore1?.text = item.endScore
+        }
+
 
     }
 
     override fun convertPlus(
-        holder: LiveBetSlipSelectionViewHolder,
-        binding: ViewBinding,
-        position: Int
+        holder: LiveBetSlipSelectionViewHolder, binding: ViewBinding, position: Int
     ) {
-        holder.initView(position, itemCount)
+        holder.initView(getItem(position), position, itemCount)
     }
 
     override fun createViewBinding(
-        inflater: LayoutInflater,
-        parent: ViewGroup,
-        viewType: Int
+        inflater: LayoutInflater, parent: ViewGroup, viewType: Int
     ): ViewBinding {
         return when (betType) {
             LiveBetSlipEnum.UnSettled -> ItemLiveBetSlipUnsettleBinding.inflate(
-                inflater,
-                parent,
-                false
+                inflater, parent, false
             )
 
             LiveBetSlipEnum.Confirming -> ItemLiveBetSlipConfirmBinding.inflate(
-                inflater,
-                parent,
-                false
+                inflater, parent, false
             )
 
             LiveBetSlipEnum.Settled -> ItemLiveBetSlipSettledBinding.inflate(
-                inflater,
-                parent,
-                false
+                inflater, parent, false
             )
 
             LiveBetSlipEnum.Reserve -> ItemLiveBetSlipReserveBinding.inflate(
-                inflater,
-                parent,
-                false
+                inflater, parent, false
             )
 
             LiveBetSlipEnum.Invalid -> ItemLiveBetSlipInvalidBinding.inflate(
-                inflater,
-                parent,
-                false
+                inflater, parent, false
             )
         }
 
     }
 
     override fun createViewHolder(
-        binding: ViewBinding,
-        viewType: Int
+        binding: ViewBinding, viewType: Int
     ): LiveBetSlipSelectionViewHolder {
         val holder = LiveBetSlipSelectionViewHolder(binding)
         return holder
