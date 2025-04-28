0aac591171b081daa81224cd6f221b3061c91a3e
joseph.tseng <joseph.tseng@skylinetw.com>
2025-04-28 11:44:33 +0800

    fix: 修正早盤observe date問題, 傳入playType

--- module_home/src/main/java/arch/cayenne/module/home/ui/fragment/MatchListPagerFragment.kt
+++ module_home/src/main/java/arch/cayenne/module/home/ui/fragment/MatchListPagerFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import arch.cayenne.lib.base.ui.BaseFragment
+import arch.cayenne.lib.base.utils.LogUtilsExt.logd
 import arch.cayenne.lib.common.utils.ext.DimensionExt.dp2px
 import arch.cayenne.lib.common.utils.ext.NavigationExt.navigate
 import arch.cayenne.lib.common.utils.ext.sharedViewModel
@@ -13,31 +14,30 @@ import arch.cayenne.lib.database.entity.MatchWithMarkets
 import arch.cayenne.lib.database.entity.SelectionBean
 import arch.cayenne.module.bet.ui.fragment.BetSheetFragment
 import arch.cayenne.module.home.databinding.FragmentMatchListPagerBinding
+import arch.cayenne.module.home.enums.PlayType
 import arch.cayenne.module.home.ui.adapter.MatchItemAdapter
 import arch.cayenne.module.home.utils.MatchCardItemDecoration
 import arch.cayenne.module.home.viewmodel.HomeViewModel
 import arch.cayenne.module.home.viewmodel.HomeViewModel.Companion.TOURNAMENT_ALL_ID
-import arch.cayenne.module.home.viewmodel.TodayGameListViewModel
+import arch.cayenne.module.home.viewmodel.MatchListViewModel
 import kotlinx.coroutines.launch
 import kotlin.reflect.KClass
 
 class MatchListPagerFragment :
-    BaseFragment<TodayGameListViewModel, FragmentMatchListPagerBinding>() {
+    BaseFragment<MatchListViewModel, FragmentMatchListPagerBinding>() {
     override val vbClass: KClass<FragmentMatchListPagerBinding> =
         FragmentMatchListPagerBinding::class
-    override val vmClass: KClass<TodayGameListViewModel> = TodayGameListViewModel::class
+    override val vmClass: KClass<MatchListViewModel> = MatchListViewModel::class
     private val homeViewModel: HomeViewModel by sharedViewModel<HomeViewModel, NewHomeFragment>()
     private lateinit var matchAdapter: MatchItemAdapter
     private var leagueId: Int = -1
-    private var selectedDate: String? = null
-    private var isEarly: Boolean = false
+    private var playTypeId: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
+            playTypeId = it.getInt(ARG_PLAY_TYPE_ID)
             leagueId = it.getInt(ARG_LEAGUE_ID, TOURNAMENT_ALL_ID)
-            selectedDate = it.getString(ARG_DATE)
-            isEarly = !selectedDate.isNullOrEmpty()
         }
     }
 
@@ -77,6 +77,8 @@ class MatchListPagerFragment :
 
     override fun createObserver() {
         homeViewModel.selectedDate.observe(viewLifecycleOwner) { date ->
+            "selectedDate: $date".logd()
+            refreshListByDate()
             if (date.isNullOrEmpty()) {
                 //切換後選回全部
             } else {
@@ -89,30 +91,35 @@ class MatchListPagerFragment :
         }
     }
 
+    private fun refreshListByDate() {
+        //TODO 早盤更新選中的日期列表
+    }
+
     override fun initData() {
         arguments?.apply {
-            //TODO 早盤日期要資料
-            if (!isEarly) {
-                //今日
-            } else {
-                //早盤初始化在全部賽事
-            }
             mViewModel.setTournamentId(leagueId)
             mViewModel.setSportId(this.getInt(ARG_SPORT_ID))
+        }
+        //TODO 早盤日期要資料
+        if (playTypeId == PlayType.TODAY.id) {
+            //今日
+        } else if (playTypeId == PlayType.EARLY.id) {
+            //早盤初始化在全部賽事, viewmodel中比對startTime拿資料
         }
         mViewModel.getCurrentMatch()
     }
 
     companion object {
         private const val ARG_SPORT_ID = "sport_id"
+        private const val ARG_PLAY_TYPE_ID = "play_type_id"
         private const val ARG_LEAGUE_ID = "arg_league_id"
         private const val ARG_DATE = "arg_date"
-        fun newInstance(sportId: Int, leagueId: Int, date: String? = null): MatchListPagerFragment {
+        fun newInstance(sportId: Int, playTypeId: Int, leagueId: Int): MatchListPagerFragment {
             return MatchListPagerFragment().apply {
                 arguments = Bundle().apply {
                     putInt(ARG_SPORT_ID, sportId)
+                    putInt(ARG_PLAY_TYPE_ID, playTypeId)
                     putInt(ARG_LEAGUE_ID, leagueId)
-                    putString(ARG_DATE, date)
                 }
             }
         }
