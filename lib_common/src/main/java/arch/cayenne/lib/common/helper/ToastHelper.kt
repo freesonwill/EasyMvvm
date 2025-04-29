4d7ee744142926b94767efdff2dbde8f4be4e114
Link Hsieh <link.hsieh@skylinetw.com>
2025-04-29 11:31:28 +0800

    feat : add custom view for toast

--- lib_common/src/main/java/arch/cayenne/lib/common/helper/ToastHelper.kt
+++ lib_common/src/main/java/arch/cayenne/lib/common/helper/ToastHelper.kt
@@ -1,13 +1,23 @@
 package arch.cayenne.lib.common.helper
 
+import android.animation.ValueAnimator
 import android.app.Activity
 import android.content.Context
+import android.graphics.PixelFormat
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.Toast
+import android.view.WindowManager
+import androidx.core.animation.addListener
 import androidx.fragment.app.Fragment
 import arch.cayenne.lib.common.databinding.ToastLayoutBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
 
 internal class ToastHelper private constructor() {
     companion object {
@@ -15,62 +25,110 @@ internal class ToastHelper private constructor() {
         val instance: ToastHelper by lazy { ToastHelper() }
     }
 
-    private var toast: Toast? = null
+    private var toastJob: Job? = null
 
     /***
      * 預設toast
      * @param context
      * @param msg
      */
-    fun showDefaultToast(context: Context, msg: String) {
-        if (toast != null) {
-            toast?.cancel()
+    fun showDefaultToast(context: Context, msg: String, duration: Long) {
+        if (toastJob != null) {
+            return
         }
         val inflater = LayoutInflater.from(context)
         val layout = ToastLayoutBinding.inflate(inflater, null, false)
 
         layout.toastText.text = msg
-        showToast(layout.root)
+        showToast(layout.root, duration)
     }
 
     /***
      * 自定義toast
      * @param view 需先自行實作view
      */
-    fun showCustomToast(view: View) {
-        if (toast != null) {
-            toast?.cancel()
+    fun showCustomToast(view: View, duration: Long) {
+        if (toastJob != null) {
+            return
         }
-        showToast(view)
+        showToast(view, duration)
     }
 
-    private fun showToast(view: View) {
-        Toast(view.context).apply {
-            duration = Toast.LENGTH_SHORT
-            this.view = view
-            setGravity(Gravity.CENTER, 0, 0)
-            show()
-            // 移除參考以允許下一次顯示（Toast.LENGTH_SHORT 約 2s）
-            view.postDelayed({
-                toast = null
-            }, 2000L)
+    private fun showToast(view: View, duration: Long) {
+        val wm = view.context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val layoutParams = WindowManager.LayoutParams()
+
+        // 设置参数
+        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT
+        layoutParams.width = WindowManager.LayoutParams.WRAP_CONTENT
+        layoutParams.format = PixelFormat.TRANSLUCENT
+        layoutParams.flags = (WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                or WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+        layoutParams.gravity = Gravity.CENTER
+        wm.addView(view, layoutParams)
+
+        toastJob = CoroutineScope(Dispatchers.Main).launch {
+            playAnim(view, true)
+            delay(duration)
+            playAnim(view, false)
+            wm.removeView(view)
+            toastJob = null
+        }
+    }
+
+    private suspend fun playAnim(view: View, show: Boolean): Int {
+        return suspendCancellableCoroutine { continuation ->
+            view.apply {
+                val start = if (show) 0f else 1f
+                val end = if (!show) 0f else 1f
+                val anim = ValueAnimator.ofFloat(start, end).apply {
+                    duration = 150
+                    addUpdateListener { animation ->
+                        val p = animation.animatedValue as Float
+                        scaleX = p
+                        alpha = p
+                        scaleY = p
+                    }
+                    var isCanceled = false
+                    addListener(
+                        onStart = {
+                            scaleX = start
+                            alpha = start
+                            scaleY = start
+                        },
+                        onCancel = {
+                            isCanceled = true
+                            continuation.resume(-1)
+                        },
+                        onEnd = {
+                            if (isCanceled) return@addListener
+                            continuation.resume(200)
+                        }
+                    )
+                    start()
+                }
+                continuation.invokeOnCancellation {
+                    if (anim.isRunning) anim.cancel()
+                }
+            }
         }
     }
 
 }
 
-fun Fragment.showToast(msg: String) {
-    ToastHelper.instance.showDefaultToast(requireContext(), msg)
+fun Fragment.showToast(msg: String, duration: Long = 2_000L) {
+    ToastHelper.instance.showDefaultToast(requireContext(), msg, duration)
 }
 
-fun Fragment.showToast(view: View) {
-    ToastHelper.instance.showCustomToast(view)
+fun Fragment.showToast(view: View, duration: Long = 2_000L) {
+    ToastHelper.instance.showCustomToast(view, duration)
 }
 
-fun Activity.showToast(msg: String) {
-    ToastHelper.instance.showDefaultToast(this, msg)
+fun Activity.showToast(msg: String, duration: Long = 2_000L) {
+    ToastHelper.instance.showDefaultToast(this, msg, duration)
 }
 
-fun Activity.showToast(view: View) {
-    ToastHelper.instance.showCustomToast(view)
+fun Activity.showToast(view: View, duration: Long = 2_000L) {
+    ToastHelper.instance.showCustomToast(view, duration)
 }
