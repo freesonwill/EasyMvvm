74ab4406e0296bca5d7ff96dabf15b02f32908e0
wangzai <dafeix64@gmail.com>
2025-05-01 20:17:28 +0700

    反馈页UI

--- module_feedback/src/main/kotlin/com/walisport/module/feedback/ui/fragment/FeedbackMainFragment.kt
+++ module_feedback/src/main/kotlin/com/walisport/module/feedback/ui/fragment/FeedbackMainFragment.kt
@@ -1,7 +1,10 @@
 package com.walisport.module.feedback.ui.fragment
 
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
+import arch.cayenne.lib.common.utils.ext.ResourceExt.getString
+import com.walisport.module.feedback.R
 import com.walisport.module.feedback.databinding.FragmentFeedbackMainBinding
 import com.walisport.module.feedback.ui.viewmodel.FeedbackMainViewModel
 import kotlin.reflect.KClass
@@ -15,7 +18,13 @@ class FeedbackMainFragment : BaseFragment<FeedbackMainViewModel, FragmentFeedbac
     override val vmClass: KClass<FeedbackMainViewModel> = FeedbackMainViewModel::class
 
     override fun initView(savedInstanceState: Bundle?) {
+        with(mBinding) {
+            titleBar.loadGeneralTitleBar(R.string.feedback_title.getString(), {
+                findNavController().navigateUp()
+            })
 
+
+        }
     }
 
 
