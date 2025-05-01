1aef78fc0a5d2227aebf78aa7d1659b8363eaae7
wangzai <dafeix64@gmail.com>
2025-05-01 14:46:45 +0700

    feedback的viewmodel及数据层

---
+++ module_feedback/src/main/kotlin/com/walisport/module/feedback/ui/viewmodel/FeedbackMainViewModel.kt
@@ -0,0 +1,16 @@
+package com.walisport.module.feedback.ui.viewmodel
+
+import arch.cayenne.lib.base.ui.viewmodel.BaseViewModel
+import com.walisport.module.feedback.data.FeedbackMainRepository
+import plugin.koin.KoinViewModel
+
+@KoinViewModel
+class FeedbackMainViewModel(private val repo: FeedbackMainRepository) : BaseViewModel() {
+
+
+    override fun initViewModel() {
+        super.initViewModel()
+    }
+
+
+}
