a44f59c32a53cae864dc350c3953f2e22268f0e7
aquan <aquan@aquan>
2025-05-06 17:10:27 +0700

    feature:WLS-302 1:直播间-比赛详情资料请求,存储

--- module_live/src/main/kotlin/com/walisport/module/live/data/LiveMainRepository.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/data/LiveMainRepository.kt
@@ -9,11 +9,11 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 
 class LiveMainRepository(
-    private val remoteManager: LiveRemoteManager,private val liveMatchDao: LiveMatchDao, private val database: GameDatabase
+    private val remoteManager: LiveRemoteManager,private val database: GameDatabase
 ) : BaseRepository() {
     override val scope: CoroutineScope = CoroutineScope(Dispatchers.IO)
     fun observeBalance(): Flow<Long> = database.infoDao().observeBalance()
-    fun observeMatchBean(matchId: Long) = liveMatchDao.observeMatchById(matchId)
+    fun observeMatchBean(matchId: Long) = database.liveMatchDao().observeMatchById(matchId)
 
     // 500-1003: 获取比赛详情
     suspend fun getMatchRes(matchId: Long) {
