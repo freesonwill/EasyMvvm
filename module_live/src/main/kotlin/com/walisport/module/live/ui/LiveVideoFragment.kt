d18ab85643b842e96a18127e1fcc9d222a296da8
wangzai <dafeix64@gmail.com>
2025-05-12 22:01:32 +0700

    更换视频播放器

--- module_live/src/main/kotlin/com/walisport/module/live/ui/LiveVideoFragment.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/LiveVideoFragment.kt
@@ -1,6 +1,5 @@
 package com.walisport.module.live.ui
 
-import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.util.TypedValue.COMPLEX_UNIT_PX
 import android.view.View
@@ -21,9 +20,6 @@ import com.walisport.module.live.data.constants.MatchStatus
 import com.walisport.module.live.databinding.FragmentLiveVideoBinding
 import com.walisport.module.live.ui.viewmodel.LiveVideoViewModel
 import com.xxx.qyplayer.PlayerMode
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlin.reflect.KClass
 
 
@@ -34,136 +30,31 @@ class LiveVideoFragment : BaseFragment<LiveVideoViewModel, FragmentLiveVideoBind
     override val vbClass: KClass<FragmentLiveVideoBinding> = FragmentLiveVideoBinding::class
     override val vmClass: KClass<LiveVideoViewModel> = LiveVideoViewModel::class
 
-    private val mPlayerMode = PlayerMode.FLUENCY
-    private lateinit var mGlobalConfig: GlobalConfig
-
 
     override fun initView(savedInstanceState: Bundle?) {
         mBinding.model = mViewModel
         mBinding.includedMatchNotInProgress.model = mViewModel
 
-        mBinding.videoView.apply {
-            init(mPlayerMode)
-            keepScreenOn = true
-
-        }
-
-        initPlayer()
-
-        //todo: 设置超时时间
-
-//        val mediaPlayer = mBinding.videoView.mediaPlayer
-//        if (mediaPlayer is IjkMediaPlayer) {
-//            mediaPlayer.setOption(
-//                IjkMediaPlayer.OPT_CATEGORY_FORMAT,
-//                "timeout",
-//                10000000
-//            ); // 10秒总超时（微秒）
-//            mediaPlayer
-//                .setOption(
-//                    IjkMediaPlayer.OPT_CATEGORY_FORMAT,
-//                    "connect_timeout",
-//                    5000
-//                ); // 5秒连接超时（毫秒）
-//            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "reconnect", 0); // 禁用自动重连
-//        }
-
-
-        //todo: 加载状态监听
-
-//        mBinding.videoView.setOnInfoListener { mp, what, extra ->
-//            when (what) {
-//                IMediaPlayer.MEDIA_INFO_BUFFERING_START -> {
-//                    // 视频开始缓冲（加载中）
-////                    "Buffering started".logd(TAG)
-//                    playingStatusLiveData.postValue(PlayStatus.Loading)
-//
-//                    // 启动协程，10秒超时
-//                    bufferingTimeoutJob = coroutineScope.launch {
-//                        delay(10000)
-//                        playingStatusLiveData.postValue(PlayStatus.Error)
-//                    }
-//
-//                }
-//
-//                IMediaPlayer.MEDIA_INFO_BUFFERING_END -> {
-//                    // 视频缓冲结束（加载完成，可以播放）
-////                    "Buffering ended".logd(TAG)
-//                    playingStatusLiveData.postValue(PlayStatus.Playing)
-//                    bufferingTimeoutJob?.cancel()
-//
-//                }
-//
-//                IMediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START -> {
-//                    // 视频开始渲染（第一帧显示）
-////                    "Video rendering started".logd(TAG)
-//                    playingStatusLiveData.postValue(PlayStatus.Playing)
-//                    bufferingTimeoutJob?.cancel()
-//                }
-//
-//                IMediaPlayer.MEDIA_INFO_AUDIO_RENDERING_START -> {
-//                    // 音频开始渲染
-//                    //  LogUtils.i(TAG, "Audio rendering started")
-//                }
-//
-//                else -> {
-//                    "player info. what:${what}".logd(TAG)
-//                }
-//            }
-//
-//            true
-//        }
-
-
-        //todo: 加载失败监听
-//        mBinding.videoView.setOnErrorListener { mp, what, extra ->
-//            when (what) {
-//                IjkMediaPlayer.MEDIA_ERROR_IO -> {
-//                    "Error: Network I/O error (MEDIA_ERROR_IO), extra: $extra".logd(TAG)
-//                    playingStatusLiveData.postValue(PlayStatus.Error)
-//                }
-//
-//                IjkMediaPlayer.MEDIA_ERROR_MALFORMED -> {
-//                    "Error: Malformed stream (MEDIA_ERROR_MALFORMED), extra: $extra".logd(TAG)
-//                    playingStatusLiveData.postValue(PlayStatus.Error)
-//                }
-//
-//                IjkMediaPlayer.MEDIA_ERROR_UNSUPPORTED -> {
-//                    "Error: Unsupported format (MEDIA_ERROR_UNSUPPORTED), extra: $extra".logd(TAG)
-//                    playingStatusLiveData.postValue(PlayStatus.Error)
-//                }
-//
-//                IjkMediaPlayer.MEDIA_ERROR_TIMED_OUT -> {
-//                    "Error: Timeout (MEDIA_ERROR_TIMED_OUT), extra: $extra".logd(TAG)
-//                    playingStatusLiveData.postValue(PlayStatus.Error)
-//                }
-//
-//                else -> {
-//                    "Unknown error, what: $what, extra: $extra".logd(TAG)
-//                    playingStatusLiveData.postValue(PlayStatus.Error)
-//                }
-//            }
-//            // 返回 true 表示错误已处理，false 表示未处理
-//            true
-//        }
+        initVideoView()
     }
 
-    private fun initPlayer() {
-        mGlobalConfig = GlobalConfig(requireContext()).also {
-            if (!it.inited) { // 首次启动从本地播放器获取默认配置
-                it.transformFromPlayerConfig(mBinding.videoView.getConfig())
+    private fun initVideoView() {
+        mBinding.videoView.apply {
+            init(PlayerMode.FLUENCY)
+            keepScreenOn = true
+            setConfig(GlobalConfig(requireContext()).also {
+                if (!it.inited) { // 首次启动从本地播放器获取默认配置
+                    it.transformFromPlayerConfig(mBinding.videoView.getConfig())
 
-                // 更改底层默认配置。默认加密流，需要开启解密
-                it.isAudioDecrypt = false
-                it.isVideoDecrypt = false
-                it.isHWDecode = false
+                    // 更改底层默认配置。默认加密流，需要开启解密
+                    it.isAudioDecrypt = false
+                    it.isVideoDecrypt = false
+                    it.isHWDecode = false
 
-                it.inited = true
-            }
-        }
+                    it.inited = true
+                }
+            }.transformToPlayerConfig())
 
-        mBinding.videoView.run {
-            setConfig(mGlobalConfig.transformToPlayerConfig())
         }
 
     }
@@ -221,7 +112,7 @@ class LiveVideoFragment : BaseFragment<LiveVideoViewModel, FragmentLiveVideoBind
 
                     val playUrl = it.source.firstOrNull { ele -> ele.isPlaying }?.playUrl()
                     playUrl?.takeIf { url -> url.isNotEmpty() }?.let { url ->
-                        "url:${url}".logd("LiveVideoFragment")
+//                        "url:${url}".logd("LiveVideoFragment")
                         mBinding.videoView.setDataSource(url)
                         mBinding.videoView.prepare()
                     }
