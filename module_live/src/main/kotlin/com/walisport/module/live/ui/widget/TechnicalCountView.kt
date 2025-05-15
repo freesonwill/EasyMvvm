ca9d1d4dc16eb1fad950f1b2ac2028209784ac05
xiaoyang <xiaoyang@abc.com>
2025-05-15 09:53:53 +0700

    fix：直播详情页类型转化错误导致的Crash - https://linear.app/forgames/issue/WLS-392

--- module_live/src/main/kotlin/com/walisport/module/live/ui/widget/TechnicalCountView.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/widget/TechnicalCountView.kt
@@ -1,5 +1,6 @@
 package com.walisport.module.live.ui.widget
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
@@ -7,6 +8,7 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.widget.AppCompatTextView
+import arch.cayenne.lib.common.utils.ext.DimensionExt.dp2px
 import arch.cayenne.lib.skin.res.SkinnableResourceManager
 import arch.cayenne.lib.skin.widget.SkinnableLinearLayout
 import com.bumptech.glide.Glide
@@ -55,12 +57,14 @@ class TechnicalCountView @JvmOverloads constructor(
     }
 
     //设置比赛趋势蜡烛图数据
+    @SuppressLint("SetTextI18n")
     fun setTrendData(data: MatchTrendData) {
         mBinding.viewGoalTrend.setData(data)
         val size = data.data.size
         if (size > 90) {
             //当比赛时间超过90分钟时需重新绘制时间栏
-            refreshTimeLayout(size)
+            mBinding.lastTime.text = "$size'"
+            //refreshTimeLayout(size)
         }
     }
 
@@ -217,7 +221,9 @@ class TechnicalCountView @JvmOverloads constructor(
                     0 -> params.gravity = Gravity.START or Gravity.CENTER_VERTICAL
                     6 -> params.gravity = Gravity.END or Gravity.CENTER_VERTICAL
                     else -> {
-                        params.gravity = Gravity.CENTER
+                        val marginLeft = (15 * index * 2.dp2px - textView.width / 2f).toInt()
+                        params.setMargins(marginLeft, 0, 0, 0)
+                        params.gravity = Gravity.CENTER_VERTICAL
                     }
                 }
                 textView.layoutParams = params
