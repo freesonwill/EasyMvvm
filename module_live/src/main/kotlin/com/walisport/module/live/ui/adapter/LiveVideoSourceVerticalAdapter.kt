7438c9e4bc1d56349b9eb31a48519487a1c80c88
wangzai <dafeix64@gmail.com>
2025-05-15 16:20:18 +0700

    切换视频播放源后， 用lottie动画表示正在播放的视频源

--- module_live/src/main/kotlin/com/walisport/module/live/ui/adapter/LiveVideoSourceVerticalAdapter.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/adapter/LiveVideoSourceVerticalAdapter.kt
@@ -33,10 +33,12 @@ class LiveVideoSourceVerticalAdapter(compare: DiffUtil.ItemCallback<VideoSourceB
             viewBinding.tvTitle.text = item.title
             viewBinding.tvSubtitle.text = item.subTitle
 
-            viewBinding.animationView.visibility = if (item.isPlaying) {
-                View.VISIBLE
+            if (item.isPlaying) {
+                viewBinding.animationView.visibility = View.VISIBLE
+                viewBinding.animationView.playAnimation()
             } else {
-                View.GONE
+                viewBinding.animationView.visibility = View.GONE
+                viewBinding.animationView.pauseAnimation()
             }
 
             Glide.with(viewBinding.ivThumb).load(item.thumb)
