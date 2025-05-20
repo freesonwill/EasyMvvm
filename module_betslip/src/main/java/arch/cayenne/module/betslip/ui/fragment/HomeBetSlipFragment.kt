85e00c38097e72b7bbe96e951d6b0656919a4230
Link Hsieh <link.hsieh@skylinetw.com>
2025-05-20 16:24:26 +0800

    feat : 新增sport filter動畫

--- module_betslip/src/main/java/arch/cayenne/module/betslip/ui/fragment/HomeBetSlipFragment.kt
+++ module_betslip/src/main/java/arch/cayenne/module/betslip/ui/fragment/HomeBetSlipFragment.kt
@@ -109,23 +109,25 @@ class HomeBetSlipFragment: BaseFragment<HomeBetSlipViewModel, FragmentHomeBetsli
 
     private fun showSportFilter() {
         mViewModel.onSportFilter.value?.let {
-            val f = SportPickerFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(Config.VALUE_SELECTED_SPORT, it.sportId)
-                }
-            }
-            childFragmentManager.setFragmentResultListener(Config.KEY_RESULT, viewLifecycleOwner) { _, bundle ->
-                childFragmentManager.clearFragmentResultListener(Config.KEY_RESULT)
-                if (bundle.containsKey(Config.VALUE_SELECTED_SPORT)) {
-                    bundle.getInt(Config.VALUE_SELECTED_SPORT).let { id ->
-                        mViewModel.setSportFilter(id)
+            val lastFragment = childFragmentManager.findFragmentByTag(SportPickerFragment.TAG)
+            if (lastFragment == null) {
+                val f = SportPickerFragment().apply {
+                    arguments = Bundle().apply {
+                        putInt(Config.VALUE_SELECTED_SPORT, it.sportId)
                     }
                 }
-                childFragmentManager.beginTransaction().remove(f).commit()
+                childFragmentManager.setFragmentResultListener(Config.KEY_RESULT, viewLifecycleOwner) { _, bundle ->
+                    childFragmentManager.clearFragmentResultListener(Config.KEY_RESULT)
+                    if (bundle.containsKey(Config.VALUE_SELECTED_SPORT)) {
+                        bundle.getInt(Config.VALUE_SELECTED_SPORT).let { id ->
+                            mViewModel.setSportFilter(id)
+                        }
+                    }
+                }
+                childFragmentManager.beginTransaction()
+                    .replace(mBinding.fragmentContainer.id, f, f.javaClass.simpleName)
+                    .commit()
             }
-            childFragmentManager.beginTransaction()
-                .replace(mBinding.fragmentContainer.id, f, f.javaClass.simpleName)
-                .commit()
         }
     }
 }
--- module_betslip/src/main/java/arch/cayenne/module/betslip/ui/fragment/SportPickerFragment.kt
+++ module_betslip/src/main/java/arch/cayenne/module/betslip/ui/fragment/SportPickerFragment.kt
@@ -1,6 +1,8 @@
 package arch.cayenne.module.betslip.ui.fragment
 
 import android.os.Bundle
+import android.view.animation.DecelerateInterpolator
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.SimpleItemAnimator
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
 import arch.cayenne.module.betslip.data.constants.Config
@@ -11,10 +13,13 @@ import kotlin.reflect.KClass
 
 class SportPickerFragment: BaseFragment<SportPickerViewModel, FragmentSportPickerBinding>() {
 
+    companion object {
+        const val TAG = "SportPickerFragment"
+    }
+
     override val vbClass: KClass<FragmentSportPickerBinding> = FragmentSportPickerBinding::class
     override val vmClass: KClass<SportPickerViewModel> = SportPickerViewModel::class
 
-    private var anchorY = 0
     private val sportAdapter: SportPickerAdapter by lazy {
         SportPickerAdapter(object : SportPickerAdapter.SportPickerListener {
             override fun onSportSelected(id: Int) {
@@ -26,6 +31,8 @@ class SportPickerFragment: BaseFragment<SportPickerViewModel, FragmentSportPicke
     override fun initView(savedInstanceState: Bundle?) {
         mBinding.rvSport.adapter = sportAdapter
         (mBinding.rvSport.itemAnimator as? SimpleItemAnimator)?.supportsChangeAnimations = false
+
+        expandView()
     }
 
     override fun initData() {
@@ -42,6 +49,7 @@ class SportPickerFragment: BaseFragment<SportPickerViewModel, FragmentSportPicke
             parentFragmentManager.setFragmentResult(Config.KEY_RESULT, Bundle().apply {
                 putInt(Config.VALUE_SELECTED_SPORT, id)
             })
+            collapseView()
         }
     }
 
@@ -51,7 +59,36 @@ class SportPickerFragment: BaseFragment<SportPickerViewModel, FragmentSportPicke
         }
     }
 
-    fun setAnchorY(y: Int) {
-        anchorY = y
+    fun dismiss() {
+        mBinding.clFilter.isVisible = false
+        mBinding.clFilter.post {
+            parentFragmentManager.beginTransaction().remove(this).commit()
+        }
+    }
+
+    private fun expandView() {
+        val clFilter = mBinding.clFilter
+        val height = clFilter.height
+        clFilter.translationY = -height.toFloat()
+        clFilter.alpha = 0f
+
+        clFilter.animate()
+            .translationY(0f)
+            .alpha(1f)
+            .setDuration(300)
+            .setInterpolator(DecelerateInterpolator())
+            .start()
+    }
+
+    private fun collapseView() {
+        val clFilter = mBinding.clFilter
+        clFilter.animate()
+            .translationY(-clFilter.height.toFloat())
+            .alpha(0f)
+            .setDuration(300)
+            .withEndAction {
+                dismiss() // 或其他關閉處理
+            }
+            .start()
     }
 }
