c8e5835c38f7e57dc00ca010b5ed2746c18add74
Link Hsieh <link.hsieh@skylinetw.com>
2025-05-23 14:18:56 +0800

    refactor : 加深賠率彈窗背景

--- module_bet/src/main/java/arch/cayenne/module/bet/ui/fragment/ReserveDialogFragment.kt
+++ module_bet/src/main/java/arch/cayenne/module/bet/ui/fragment/ReserveDialogFragment.kt
@@ -48,6 +48,7 @@ class ReserveDialogFragment private constructor() : BaseDialogFragment<ReserveDi
     override fun onStart() {
         super.onStart()
         dialog?.window?.let {
+            it.setDimAmount(0.6f)
             it.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
             val positionX = requireArguments().getInt(POSITION_X, -1)
