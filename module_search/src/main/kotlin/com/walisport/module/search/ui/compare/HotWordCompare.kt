c893a4e284ef7e9a0082cf2d2439e94397c2b628
Jeremy <jeremy.huang@skylinetw.com>
2025-05-23 17:37:26 +0800

    feat: implement hot search feature
    WLS-473 熱門搜索

---
+++ module_search/src/main/kotlin/com/walisport/module/search/ui/compare/HotWordCompare.kt
@@ -0,0 +1,13 @@
+package com.walisport.module.search.ui.compare
+
+import androidx.recyclerview.widget.DiffUtil
+
+class HotWordCompare: DiffUtil.ItemCallback<String>() {
+    override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+}
