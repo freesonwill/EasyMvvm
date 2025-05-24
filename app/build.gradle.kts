52efd90b421e38855437eb7f2c9e366f30e1bb09
wangzai <dafeix64@gmail.com>
2025-05-24 15:09:44 +0700

    搭建 充值module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(project(":module_search"))
     implementation(project(":module_feedback"))
     implementation(project(":module_message"))
+    implementation(project(":module_topup"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.appcompat)
