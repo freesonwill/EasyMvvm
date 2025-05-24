f46963fef5a1f3d5fdd3b660e203638946ecfa36
wenxi <russelllin465@gmail.com>
2025-05-24 11:33:06 +0700

    featue: 1.backgrounHelper新增backgrounTint换肤 2.键盘换肤  #WLS-456

--- lib_skin/src/main/java/arch/cayenne/lib/skin/widget/helper/SkinnableBackGroundHelper.kt
+++ lib_skin/src/main/java/arch/cayenne/lib/skin/widget/helper/SkinnableBackGroundHelper.kt
@@ -2,9 +2,11 @@ package arch.cayenne.lib.skin.widget.helper
 
 import android.util.AttributeSet
 import android.view.View
+import arch.cayenne.lib.base.utils.ext.LogUtilsExt.logi
 import arch.cayenne.lib.skin.R
 
 class SkinnableBackGroundHelper(mView: View) : SkinnableHelper(mView) {
+    private var backgroundTintId:Int = INVALID_ID
 
     override fun loadFromAttributes(attrs: AttributeSet?, defStyleAttr: Int) {
         val a = mView.context.obtainStyledAttributes(
@@ -17,6 +19,9 @@ class SkinnableBackGroundHelper(mView: View) : SkinnableHelper(mView) {
             if (a.hasValue(R.styleable.SportBackgroundHelper_android_background)) {
                 mSrcId = a.getResourceId(R.styleable.SportBackgroundHelper_android_background, INVALID_ID)
             }
+            if(a.hasValue(R.styleable.SportBackgroundHelper_android_backgroundTint)){
+                backgroundTintId = a.getResourceId(R.styleable.SportBackgroundHelper_android_backgroundTint, INVALID_ID)
+            }
         } finally {
             a.recycle()
         }
@@ -26,17 +31,20 @@ class SkinnableBackGroundHelper(mView: View) : SkinnableHelper(mView) {
 
     override fun updateSkin() {
 
-        if (!checkResourceIdValid(mSrcId)) {
-            return
+        if (checkResourceIdValid(mSrcId)) {
+            val drawable = resourcesManager.getDrawable(context = mView.context,mSrcId)
+            if (drawable != null) {
+                val paddingLeft = mView.paddingLeft
+                val paddingTop = mView.paddingTop
+                val paddingRight = mView.paddingRight
+                val paddingBottom = mView.paddingBottom
+                mView.background =drawable
+                mView.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom)
+            }
         }
-        val drawable = resourcesManager.getDrawable(context = mView.context,mSrcId)
-        if (drawable != null) {
-            val paddingLeft = mView.paddingLeft
-            val paddingTop = mView.paddingTop
-            val paddingRight = mView.paddingRight
-            val paddingBottom = mView.paddingBottom
-            mView.background =drawable
-            mView.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom)
+        if(checkResourceIdValid(backgroundTintId)){
+            val backgroundTint = resourcesManager.getColorStateList(mView.context,backgroundTintId)
+            mView.backgroundTintList = backgroundTint
         }
 
     }
