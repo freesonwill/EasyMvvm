8e7b3dc0ce466a7787f73b06797edeab4c4856ab
joseph.tseng <joseph.tseng@skylinetw.com>
2025-05-27 17:24:50 +0800

    feat:比赛卡片区域，背景没有铺满，两侧留的有白条

--- module_home/src/main/java/arch/cayenne/module/home/ui/fragment/NewHomeFragment.kt
+++ module_home/src/main/java/arch/cayenne/module/home/ui/fragment/NewHomeFragment.kt
@@ -406,7 +406,7 @@ class NewHomeFragment : BaseFragment<HomeViewModel, FragmentNewHomeBinding>() {
                 for (i in 0 until tabStrip.childCount) {
                     tabStrip.getChildAt(i).apply {
                         layoutParams = LinearLayout.LayoutParams(56.dp2px, 50.dp2px).apply {
-                            setMargins(5.dp2px, 0, 0, 0)
+                            setMargins(4.dp2px, 0, 0, 0)
                         }
                         setPadding(0, 0, 0, 0)
                         setBackgroundResource(R.drawable.selector_date_tab_bg)
