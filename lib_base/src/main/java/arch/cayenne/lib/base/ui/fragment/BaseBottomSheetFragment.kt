40e135d77911dd57f87226f1678d924a8a828562
zhangsan <zhangsan@youguess.com>
2025-06-07 14:34:03 +0700

    feat: add onStart(),onResume(),onPause(),onStop() into UIBindDelegate

--- lib_base/src/main/java/arch/cayenne/lib/base/ui/fragment/BaseBottomSheetFragment.kt
+++ lib_base/src/main/java/arch/cayenne/lib/base/ui/fragment/BaseBottomSheetFragment.kt
@@ -117,7 +117,26 @@ abstract class BaseBottomSheetFragment<VM : BaseViewModel,VB : ViewBinding> : Bo
         super.onViewCreated(view, savedInstanceState)
         uiBind.onViewCreated(view,savedInstanceState)
     }
-
+    @CallSuper
+    override fun onStart() {
+        super.onStart()
+        uiBind.onStart()
+    }
+    @CallSuper
+    override fun onResume() {
+        super.onResume()
+        uiBind.onResume()
+    }
+    @CallSuper
+    override fun onPause() {
+        super.onPause()
+        uiBind.onPause()
+    }
+    @CallSuper
+    override fun onStop() {
+        super.onStop()
+        uiBind.onStop()
+    }
     @CallSuper
     override fun onDestroyView() {
         super.onDestroyView()
