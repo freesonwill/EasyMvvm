758007c8b8facb43e879565a5e688e4543972c8d
xiaoyang <xiaoyang@abc.com>
2025-06-09 15:02:05 +0700

    fix:Navigator fragment不销毁不调用onDestroyView问题 - https://linear.app/forgames/issue/WLS-699

--- lib_common/src/main/java/androidx/fragment/app/FragivityFragmentNavigator.kt
+++ lib_common/src/main/java/androidx/fragment/app/FragivityFragmentNavigator.kt
@@ -130,8 +130,7 @@ class FragivityFragmentNavigator(
             fragmentManager.primaryNavigationFragment
         }
 
-        //暂时注释，因为会影响联赛日程Fragment弹出动画，导致底部Fragment空白
-        //ft.setPrimaryNavigationFragment(fragment)
+        ft.setPrimaryNavigationFragment(fragment)
 
         val isSingleTopReplacement = !initialNavigation
             && navOptions != null && navOptions.shouldLaunchSingleTop()
