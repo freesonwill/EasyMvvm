b42140e4df83c02497e1ed7355225d67048e9aeb
wangzai <dafeix64@gmail.com>
2025-06-11 15:59:48 +0700

    bugfix. https://linear.app/forgames/issue/WLS-745 现象：横屏播放时， 如果视频区域放大缩小动画和视频加载态同时出现， 加载态没有和视频区域中心对齐 解法：增加FrameLayout容器，放置视频区域和加载态， 在FrameLayout容器上执行放大缩小动画

--- module_live/src/main/kotlin/com/walisport/module/live/ui/LiveVideoLandscapeFragment.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/LiveVideoLandscapeFragment.kt
@@ -399,53 +399,53 @@ class LiveVideoLandscapeFragment :
      */
     private fun enlarge(onEndAction: () -> Unit) {
         //width， height， marginStart, marginTop
-        val currentHeight = mBinding.videoViewContainer.measuredHeight
+        val currentHeight = mBinding.videoArea.measuredHeight
         val targetWidth = mBinding.root.measuredWidth
-        val currentWidth = mBinding.videoViewContainer.measuredWidth
+        val currentWidth = mBinding.videoArea.measuredWidth
         val targetHeight = mBinding.root.measuredHeight
         val currentMarginTop =
-            (mBinding.videoViewContainer.layoutParams as ConstraintLayout.LayoutParams).topMargin
+            (mBinding.videoArea.layoutParams as ConstraintLayout.LayoutParams).topMargin
         val targetMarginTop = 0
         val currentMarginStart =
-            (mBinding.videoViewContainer.layoutParams as ConstraintLayout.LayoutParams).marginStart
+            (mBinding.videoArea.layoutParams as ConstraintLayout.LayoutParams).marginStart
         val targetMarginStart = 0
 
         with(AnimatorSet()) {
             playTogether(
                 ValueAnimator.ofInt(currentHeight, targetHeight).apply {
                     addUpdateListener {
-                        val lp = mBinding.videoViewContainer.layoutParams
+                        val lp = mBinding.videoArea.layoutParams
                         lp.height = it.animatedValue as Int
 
-                        mBinding.videoViewContainer.layoutParams = lp
+                        mBinding.videoArea.layoutParams = lp
                     }
                 },
                 ValueAnimator.ofInt(currentWidth, targetWidth).apply {
                     addUpdateListener {
-                        val lp = mBinding.videoViewContainer.layoutParams
+                        val lp = mBinding.videoArea.layoutParams
                         lp.width = it.animatedValue as Int
 
-                        mBinding.videoViewContainer.layoutParams = lp
+                        mBinding.videoArea.layoutParams = lp
                     }
                 },
                 ValueAnimator.ofInt(currentMarginTop, targetMarginTop).apply {
                     addUpdateListener {
                         val lp =
-                            mBinding.videoViewContainer.layoutParams as ConstraintLayout.LayoutParams
+                            mBinding.videoArea.layoutParams as ConstraintLayout.LayoutParams
                         lp.topMargin = it.animatedValue as Int
 
-                        mBinding.videoViewContainer.layoutParams = lp
+                        mBinding.videoArea.layoutParams = lp
 
                     }
                 },
                 ValueAnimator.ofInt(currentMarginStart, targetMarginStart).apply {
                     addUpdateListener {
                         val lp =
-                            mBinding.videoViewContainer.layoutParams as ConstraintLayout.LayoutParams
+                            mBinding.videoArea.layoutParams as ConstraintLayout.LayoutParams
                         lp.marginStart = it.animatedValue as Int
                         lp.marginEnd = it.animatedValue as Int
 
-                        mBinding.videoViewContainer.layoutParams = lp
+                        mBinding.videoArea.layoutParams = lp
 
                     }
                 })
@@ -479,38 +479,38 @@ class LiveVideoLandscapeFragment :
             playTogether(
                 ValueAnimator.ofInt(currentHeight, targetHeight).apply {
                     addUpdateListener {
-                        val lp = mBinding.videoViewContainer.layoutParams
+                        val lp = mBinding.videoArea.layoutParams
                         lp.height = it.animatedValue as Int
 
-                        mBinding.videoViewContainer.layoutParams = lp
+                        mBinding.videoArea.layoutParams = lp
                     }
                 },
                 ValueAnimator.ofInt(currentWidth, targetWidth).apply {
                     addUpdateListener {
-                        val lp = mBinding.videoViewContainer.layoutParams
+                        val lp = mBinding.videoArea.layoutParams
                         lp.width = it.animatedValue as Int
 
-                        mBinding.videoViewContainer.layoutParams = lp
+                        mBinding.videoArea.layoutParams = lp
                     }
                 },
                 ValueAnimator.ofInt(currentMarginTop, targetMarginTop).apply {
                     addUpdateListener {
                         val lp =
-                            mBinding.videoViewContainer.layoutParams as ConstraintLayout.LayoutParams
+                            mBinding.videoArea.layoutParams as ConstraintLayout.LayoutParams
                         lp.topMargin = it.animatedValue as Int
 
-                        mBinding.videoViewContainer.layoutParams = lp
+                        mBinding.videoArea.layoutParams = lp
 
                     }
                 },
                 ValueAnimator.ofInt(currentMarginStart, targetHorizontalMargin).apply {
                     addUpdateListener {
                         val lp =
-                            mBinding.videoViewContainer.layoutParams as ConstraintLayout.LayoutParams
+                            mBinding.videoArea.layoutParams as ConstraintLayout.LayoutParams
                         lp.marginStart = it.animatedValue as Int
                         lp.marginEnd = it.animatedValue as Int
 
-                        mBinding.videoViewContainer.layoutParams = lp
+                        mBinding.videoArea.layoutParams = lp
 
                     }
                 })
