d6af37566e2431623121fac2b5280eb5c2b46ca9
KC.Tsai <KC.Tsai@skylinetw.com>
2025-06-13 15:56:34 +0800

    fix: add highlight functionality to tournament search
    - https://linear.app/forgames/issue/WLS-772
    
    - 『冠军』页面进行搜索时，符合关键词的文字没有高亮

--- module_home/src/main/java/arch/cayenne/module/home/data/TournamentListItem.kt
+++ module_home/src/main/java/arch/cayenne/module/home/data/TournamentListItem.kt
@@ -4,6 +4,6 @@ import arch.cayenne.lib.database.entity.BaseTournamentData
 
 sealed class TournamentListItem {
     data class Header(val letter: Char) : TournamentListItem()
-    data class TournamentItem(val tournament: BaseTournamentData) : TournamentListItem()
+    data class TournamentItem(val tournament: BaseTournamentData, val highlightStart: Int?, val highlightEnd: Int?) : TournamentListItem()
     data object FooterView : TournamentListItem()
 }
--- module_home/src/main/java/arch/cayenne/module/home/ui/adapter/TournamentItemViewHolder.kt
+++ module_home/src/main/java/arch/cayenne/module/home/ui/adapter/TournamentItemViewHolder.kt
@@ -1,5 +1,11 @@
 package arch.cayenne.module.home.ui.adapter
 
+import android.content.Context
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
 import arch.cayenne.lib.base.ui.adapter.BaseViewHolder
 import arch.cayenne.lib.database.entity.BaseTournamentData
 import arch.cayenne.module.home.R
@@ -17,10 +23,31 @@ class TournamentItemViewHolder(
                 .placeholder(R.drawable.ic_default_tournament)
                 .error(R.drawable.ic_default_tournament)
                 .into(tvSectionIcon)
-            tvSectionName.text = item.tournament.name
+            val spannable = SpannableString(item.tournament.name)
+            if (item.highlightStart != null && item.highlightEnd != null && item.highlightStart >= 0 && item.highlightEnd <= spannable.length) {
+                spannable.withColorRes(
+                    context = root.context,
+                    start = item.highlightStart,
+                    end = item.highlightEnd,
+                    colorRes = arch.cayenne.lib.common.R.color.search_btn
+                )
+            }
+            tvSectionName.text = spannable
             root.setOnClickListener {
                 onClick(item.tournament)
             }
         }
+    }
+}
+
+fun SpannableString.withColorRes(
+    context: Context,
+    start: Int,
+    end: Int,
+    @ColorRes colorRes: Int
+): SpannableString {
+    val color = ContextCompat.getColor(context, colorRes)
+    return this.apply {
+        setSpan(ForegroundColorSpan(color), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
     }
 }
