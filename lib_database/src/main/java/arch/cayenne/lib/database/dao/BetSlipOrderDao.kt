82f573abb42afb066da3e60e334ee5fc3e7274e0
Link Hsieh <link.hsieh@skylinetw.com>
2025-06-16 16:40:48 +0800

    feat : 轉移reserve bean到db

---
+++ lib_database/src/main/java/arch/cayenne/lib/database/dao/BetSlipOrderDao.kt
@@ -0,0 +1,7 @@
+package arch.cayenne.lib.database.dao
+
+import androidx.room.Dao
+import arch.cayenne.lib.database.entity.BetSlipOrderBean
+
+@Dao
+abstract class BetSlipOrderDao : BaseDao<BetSlipOrderBean>()
