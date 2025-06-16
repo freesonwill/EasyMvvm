75cc3cba914f22e6de070c1f0c2a3e6f59219310
Jeremy <jeremy.huang@skylinetw.com>
2025-06-16 10:31:40 +0800

    fix: display lock icon when match betting is stopped
    #WLS-803 精準搜尋頁當比賽為不可投注時收藏按鈕應為鎖頭

--- module_search/src/main/kotlin/com/walisport/module/search/ui/fragment/SearchResultDirectMatchFragment.kt
+++ module_search/src/main/kotlin/com/walisport/module/search/ui/fragment/SearchResultDirectMatchFragment.kt
@@ -6,11 +6,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -30,13 +27,12 @@ import com.walisport.module.search.data.model.SearchResultTeamBean
 import com.walisport.module.search.data.model.SearchResultTournamentBean
 import com.walisport.module.search.databinding.FragmentSearchResultDirectMatchBinding
 import com.walisport.module.search.ui.adapter.SearchResultRaceAdapter
-import com.walisport.module.search.ui.viewmodel.SearchResultDirectMatchViewModel
+import com.walisport.module.search.ui.fragment.SearchDatePickerFragment.Companion.DATE_PICKER_RESULT_END
 import com.walisport.module.search.ui.fragment.SearchDatePickerFragment.Companion.DATE_PICKER_RESULT_KEY
 import com.walisport.module.search.ui.fragment.SearchDatePickerFragment.Companion.DATE_PICKER_RESULT_START
-import com.walisport.module.search.ui.fragment.SearchDatePickerFragment.Companion.DATE_PICKER_RESULT_END
 import com.walisport.module.search.ui.fragment.SearchDatePickerFragment.Companion.DATE_PICKER_RESULT_TIME_IN_MILLIS
+import com.walisport.module.search.ui.viewmodel.SearchResultDirectMatchViewModel
 import com.walisport.module.search.ui.viewmodel.SearchViewModel
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Date
