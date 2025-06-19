a0c730b585bc99e62a7cc97efe5b9c1dc64c34b2
Jeremy <jeremy.huang@skylinetw.com>
2025-06-19 15:47:47 +0800

    feat: add black background overlay when DatePicker is displayed
    #WLS-867 比照H5展開日期選擇器時加入黑色透明背景

--- module_search/src/main/kotlin/com/walisport/module/search/ui/viewmodel/SearchDatePickerViewModel.kt
+++ module_search/src/main/kotlin/com/walisport/module/search/ui/viewmodel/SearchDatePickerViewModel.kt
@@ -4,4 +4,12 @@ import arch.cayenne.lib.base.ui.viewmodel.BaseViewModel
 import plugin.koin.KoinViewModel
 
 @KoinViewModel
-class SearchDatePickerViewModel: BaseViewModel()
+class SearchDatePickerViewModel: BaseViewModel() {
+    private var _isMaskClickable = true
+    val isMaskClickable: Boolean
+        get() = _isMaskClickable
+
+    fun setMaskClickable(clickable: Boolean) {
+        _isMaskClickable = clickable
+    }
+}
