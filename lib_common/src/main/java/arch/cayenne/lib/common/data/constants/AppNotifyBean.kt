c2bc15f895dc7e175ffaa9510a65b99981fe2bfd
xiaoyang <xiaoyang@abc.com>
2025-06-24 11:02:07 +0700

    fix：APP消息推送同步最新服务器端proto文件改动 - https://linear.app/forgames/issue/WLS-900

--- lib_common/src/main/java/arch/cayenne/lib/common/data/constants/AppNotifyBean.kt
+++ lib_common/src/main/java/arch/cayenne/lib/common/data/constants/AppNotifyBean.kt
@@ -3,6 +3,7 @@ package arch.cayenne.lib.common.data.constants
 data class AppNotifyBean(
     val type: Int,
     val sportId: Int,
+    val matchId: Long,
     val title: String,
     val content: String
 )
