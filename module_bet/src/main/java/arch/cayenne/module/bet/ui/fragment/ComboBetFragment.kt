670fa40d13223040e55e67b05192873cbaa972cd
Link Hsieh <link.hsieh@skylinetw.com>
2025-06-24 18:06:48 +0800

    fix : 修復投注單3變2時，高度未變化

--- module_bet/src/main/java/arch/cayenne/module/bet/ui/fragment/ComboBetFragment.kt
+++ module_bet/src/main/java/arch/cayenne/module/bet/ui/fragment/ComboBetFragment.kt
@@ -147,7 +147,14 @@ class ComboBetFragment : BaseFragment<ComboBetViewModel, FragmentComboBetBinding
                     null
                 )
             } else {
-                betSelectionAdapter.submitList(it)
+                val forceUpdateLayoutHeight = it.size == 2 && betSelectionAdapter.itemCount > 2
+                betSelectionAdapter.submitList(it) {
+                    if (forceUpdateLayoutHeight) {
+                        mBinding.rvBet.post {
+                            adjustLayoutHeight(false)
+                        }
+                    }
+                }
             }
         }
         mViewModel.onComboMultiBetBeanListener.observe(viewLifecycleOwner) {
@@ -163,29 +170,29 @@ class ComboBetFragment : BaseFragment<ComboBetViewModel, FragmentComboBetBinding
         }
         mViewModel.onLoadDataFinishListener.observe(viewLifecycleOwner) {
             if (it) {
-                setBetSheetView()
-                adjustLayoutHeight()
                 mViewModel.onLoadDataFinishListener.removeObservers(viewLifecycleOwner)
+                mBinding.root.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+                    override fun onGlobalLayout() {
+                        mBinding.root.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                        setBetSheetView()
+                        adjustLayoutHeight(betSelectionAdapter.itemCount > 2)
+                    }
+                })
             }
         }
     }
 
-    private fun adjustLayoutHeight() {
-        mBinding.root.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
-            override fun onGlobalLayout() {
-                mBinding.root.viewTreeObserver.removeOnGlobalLayoutListener(this)
-
-                if (betSelectionAdapter.itemCount == 2) {
-                    val layoutParams = mBinding.rvBet.layoutParams
-                    layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
-                    mBinding.rvBet.layoutParams = layoutParams
-                } else {
-                    val screenHeight = resources.displayMetrics.heightPixels
-                    val maxFragmentHeight = (screenHeight * 0.75).toInt()
-                    mBinding.root.minHeight = maxFragmentHeight
-                }
-            }
-        })
+    private fun adjustLayoutHeight(full: Boolean) {
+        if (full)  {
+            val screenHeight = resources.displayMetrics.heightPixels
+            val maxFragmentHeight = (screenHeight * 0.75).toInt()
+            mBinding.root.minHeight = maxFragmentHeight
+        } else {
+            val layoutParams = mBinding.rvBet.layoutParams
+            layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
+            mBinding.root.minHeight = 0
+            mBinding.rvBet.layoutParams = layoutParams
+        }
     }
 
     private fun setSumBetMoney(data: List<ComboMultiBetBean>) {
