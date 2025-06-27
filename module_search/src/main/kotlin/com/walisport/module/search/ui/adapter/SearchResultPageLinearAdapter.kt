e7e1008df9a02fde2e25f9aad0c4a99624152651
Jeremy <jeremy.huang@skylinetw.com>
2025-06-27 17:28:11 +0800

    fix: adjust top padding for first item and add correct tournament text in search result list page
    #WLS-987 搜尋結果列表頁分類應為「聯賽」而非「聯盟」，且列表與Tab的間距不正確

--- module_search/src/main/kotlin/com/walisport/module/search/ui/adapter/SearchResultPageLinearAdapter.kt
+++ module_search/src/main/kotlin/com/walisport/module/search/ui/adapter/SearchResultPageLinearAdapter.kt
@@ -2,9 +2,11 @@ package com.walisport.module.search.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.updatePadding
 import androidx.viewbinding.ViewBinding
 import arch.cayenne.lib.base.ui.adapter.BaseAdapter
 import arch.cayenne.lib.base.ui.adapter.BaseViewHolder
+import arch.cayenne.lib.common.utils.ext.DimensionExt.dp2px
 import arch.cayenne.lib.skin.res.SkinnableResourceManager
 import com.bumptech.glide.Glide
 import com.walisport.module.search.R
@@ -26,6 +28,9 @@ class SearchResultPageLinearAdapter(private val type: String): BaseAdapter<Searc
     override fun convertPlus(holder: BaseViewHolder, binding: ViewBinding, position: Int) {
         with(binding as ItemSearchResultLinearBinding) {
             with(getItem(position)) {
+                binding.clRoot.updatePadding(
+                    top = if (position == 0) 12.dp2px else binding.clRoot.paddingTop
+                )
                 tvTitle.text = this.name
                 Glide.with(holder.itemView.context).load(this.icon)
                     .placeholder(
