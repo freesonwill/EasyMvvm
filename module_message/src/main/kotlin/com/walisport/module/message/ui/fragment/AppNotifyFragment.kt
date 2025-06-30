7f5f977985f33afaa2d00b2927d77d8738dc3b37
Link Hsieh <link.hsieh@skylinetw.com>
2025-06-30 15:50:12 +0800

    fix : 點擊事件仍須住列處理

--- module_message/src/main/kotlin/com/walisport/module/message/ui/fragment/AppNotifyFragment.kt
+++ module_message/src/main/kotlin/com/walisport/module/message/ui/fragment/AppNotifyFragment.kt
@@ -4,6 +4,9 @@ import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.view.MotionEvent
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -28,6 +31,7 @@ class AppNotifyFragment : BaseFragment<TodayMatchViewModel, FragmentAppNotifyBin
     private var clicklistener: OnClickListener? = null
     private var sportId: Int = 0
     private var matchId: Long = 0L
+    private var mHandler: Handler? = null
 
     companion object {
         fun newInstance(): AppNotifyFragment {
@@ -61,10 +65,13 @@ class AppNotifyFragment : BaseFragment<TodayMatchViewModel, FragmentAppNotifyBin
     }
 
     fun sendNotifyMsg(msg: AppNotifyBean) {
-        showNotifyMsg(msg)
-        mBinding.root.postDelayed({
-            showExitAnimation()
-        }, CLOSE_TIMEOUT)
+        mHandler?.let {
+            val message = Message.obtain()
+            message.what = OPEN_NOTIFY
+            message.obj = msg
+            it.sendMessage(message)
+            it.sendEmptyMessageDelayed(CLOSE_NOTIFY, CLOSE_TIMEOUT)
+        }
     }
 
     private fun showEnterAnimation() {
@@ -89,19 +96,42 @@ class AppNotifyFragment : BaseFragment<TodayMatchViewModel, FragmentAppNotifyBin
 
     @SuppressLint("ClickableViewAccessibility")
     override fun initView(savedInstanceState: Bundle?) {
+        mHandler = object : Handler(Looper.myLooper() ?: Looper.getMainLooper()) {
+            override fun handleMessage(msg: Message) {
+                super.handleMessage(msg)
+                when (msg.what) {
+                    OPEN_NOTIFY -> {
+                        val obj = msg.obj as AppNotifyBean
+                        showNotifyMsg(obj)
+                    }
+
+                    CLICK_EVENT -> { //点击事件弹窗消失并跳转直播详情
+                        showExitAnimation()
+                        gotoMatchLive()
+                    }
+
+                    TOUCH_EVENT -> { //触摸事件只随手指移动并消失，不跳转直播详情
+                        showExitAnimation()
+                    }
+
+                    else -> showExitAnimation()
+                }
+            }
+        }
         setOnItemClickListener(
             object : OnClickListener {
                 override fun onDown() {
-
+                    mHandler?.removeCallbacksAndMessages(null)
                 }
 
                 override fun onTouch() {
-                    showExitAnimation()
+                    mHandler?.removeCallbacksAndMessages(null)
+                    mHandler?.sendEmptyMessage(TOUCH_EVENT)
                 }
 
                 override fun onClick() {
-                    showEnterAnimation()
-                    gotoMatchLive()
+                    mHandler?.removeCallbacksAndMessages(null)
+                    mHandler?.sendEmptyMessage(CLICK_EVENT)
                 }
             }
         )
@@ -148,6 +178,12 @@ class AppNotifyFragment : BaseFragment<TodayMatchViewModel, FragmentAppNotifyBin
 
     private fun setOnItemClickListener(listener: OnClickListener) {
         this.clicklistener = listener
+    }
+
+    override fun onDestroy() {
+        mHandler?.removeCallbacksAndMessages(null)
+        mHandler = null
+        super.onDestroy()
     }
 
     interface OnClickListener {
