190613db5cc28800bcdaf72dc1c51753ec385b4f
wenxi <russelllin465@gmail.com>
2025-07-03 15:13:43 +0700

    feature:提前结算的时money符号修改 #WLS-913

--- module_betslip/src/main/java/arch/cayenne/module/betslip/ui/viewmodel/EarlySettledKeyboardViewModel.kt
+++ module_betslip/src/main/java/arch/cayenne/module/betslip/ui/viewmodel/EarlySettledKeyboardViewModel.kt
@@ -1,9 +1,20 @@
 package arch.cayenne.module.betslip.ui.viewmodel
 
+import android.icu.util.Currency
+import arch.cayenne.lib.common.data.constants.CurrencySymbols
 import arch.cayenne.lib.common.ui.viewmodel.NumberCalculatorViewModel
 import arch.cayenne.lib.common.utils.ext.SportIntExt.percent
 
 class EarlySettledKeyboardViewModel : NumberCalculatorViewModel() {
+
+    var curencySymbol:String = ""
+
+    /*
+    *设置钱标志
+    * */
+    fun setMoneyCurrency(currency: String){
+       curencySymbol = CurrencySymbols.getSymbol(currency)
+    }
 
     fun setAmountMoney(betAmount: Long, minAmount: Long) {
         setNumberLimit(minAmount, betAmount)
