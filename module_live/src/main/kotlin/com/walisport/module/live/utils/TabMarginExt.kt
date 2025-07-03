2e5bb08ec4f954bce42f9aae7035192f14a50734
aquan <aquan@aquan>
2025-07-03 17:51:55 +0700

    WLS-1007 1:直播间-tab,reflexMargin优化 问题: tabView.layoutParams = params会触发 tabView.invalidate() post会引发泄露风险

--- module_live/src/main/kotlin/com/walisport/module/live/utils/TabMarginExt.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/utils/TabMarginExt.kt
@@ -12,29 +12,27 @@ object TabMarginExt {
     //设置tab之间的外边距
     fun TabLayout.reflexMargin(leftMargin: Int, rightMargin: Int, margin: Int) {
         val tabLayout = this
-        tabLayout.post {
-                val mTabStrip = tabLayout.getChildAt(0) as LinearLayout
-                for (i in 0 until mTabStrip.childCount) {
-                    val tabView = mTabStrip.getChildAt(i)
-                    val params = tabView.layoutParams as LinearLayout.LayoutParams
-                    when (i) {
-                        0 -> {//第一个tab
-                            params.leftMargin = leftMargin
-                            params.rightMargin = margin
-                        }
-
-                        mTabStrip.childCount - 1 -> {//最后一个tab
-                            params.leftMargin = margin
-                            params.rightMargin = rightMargin
-                        }
-
-                        else -> {//中间tab
-                            params.leftMargin = margin
-                            params.rightMargin = margin
-                        }
-                    }
-                    tabView.layoutParams = params
+        val mTabStrip = tabLayout.getChildAt(0) as LinearLayout
+        for (i in 0 until mTabStrip.childCount) {
+            val tabView = mTabStrip.getChildAt(i)
+            val params = tabView.layoutParams as LinearLayout.LayoutParams
+            when (i) {
+                0 -> {//第一个tab
+                    params.leftMargin = leftMargin
+                    params.rightMargin = margin
                 }
+
+                mTabStrip.childCount - 1 -> {//最后一个tab
+                    params.leftMargin = margin
+                    params.rightMargin = rightMargin
+                }
+
+                else -> {//中间tab
+                    params.leftMargin = margin
+                    params.rightMargin = margin
+                }
+            }
+            tabView.layoutParams = params
         }
     }
 }
