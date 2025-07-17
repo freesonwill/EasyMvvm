8c3670c2081227ac17acd0c152b8abee70cfd57d
KC.Tsai <KC.Tsai@skylinetw.com>
2025-07-17 17:05:38 +0800

    feat: add fade animation to favorite icon toggle
    - https://linear.app/forgames/issue/MDEV-89/首頁收藏按鈕加入動畫

--- module_home/src/main/java/arch/cayenne/module/home/ui/adapter/MatchItemViewHolder.kt
+++ module_home/src/main/java/arch/cayenne/module/home/ui/adapter/MatchItemViewHolder.kt
@@ -1,7 +1,9 @@
 package arch.cayenne.module.home.ui.adapter
 
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.graphics.Rect
+import android.graphics.drawable.LayerDrawable
 import android.text.TextUtils
 import android.view.Gravity
 import android.view.View
@@ -122,7 +124,7 @@ class MatchItemViewHolder(
             ivLiveVideo.setImageResource(
                 if (liveInfo.liveVideo) R.drawable.ic_live_video else R.drawable.ic_live_video_disabled
             )
-            ivFavorite.isSelected = data.match.collect
+            setFavoriteIcon(data.match.collect, true)
 
             if (basicInfo.status == 5) {
                 tvAwayScore.text = liveInfo.homeScore.toString()
@@ -195,9 +197,25 @@ class MatchItemViewHolder(
                 oddsColumnAdapter.submitList(selectionsGrouped)
             }
             if ("collect" in changes) {
-                ivFavorite.isSelected = item.match.collect
+                setFavoriteIcon(item.match.collect, false)
             }
         }
+    }
+
+    private fun setFavoriteIcon(selected: Boolean, force: Boolean) {
+        val layers = mBinding.ivFavorite.drawable as LayerDrawable
+        val unselected = layers.findDrawableByLayerId(R.id.background)
+        val selectedDrawable = layers.findDrawableByLayerId(R.id.foreground)
+
+        // 做 alpha 淡入淡出動畫
+        val fadeIn = ObjectAnimator.ofInt(selectedDrawable, "alpha", if (selected) 0 else 255, if (selected) 255 else 0)
+        val fadeOut = ObjectAnimator.ofInt(unselected, "alpha", if (selected) 255 else 0, if (selected) 0 else 255)
+
+        fadeIn.duration = if (force) 0 else 200
+        fadeOut.duration = if (force) 0 else 200
+
+        fadeIn.start()
+        fadeOut.start()
     }
 
     private fun liveClock(clock: Int, modified: Long) : String =
