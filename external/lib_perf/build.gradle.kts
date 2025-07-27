cbbb38a66b4b43f1ffd61353be56f377e9d0d2b5
zhangsan <zhangsan@youguess.com>
2025-07-27 14:26:42 +0700

    refactor: config monitor in build.gradle.kts

--- external/lib_perf/build.gradle.kts
+++ external/lib_perf/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
@@ -5,8 +7,16 @@ plugins {
 
 apply(from = rootProject.file("gradle/_sign.gradle"))
 
+// 读取 local.properties
+val localProperties = Properties().apply {
+    val file = rootProject.file("local.properties")
+    if (file.exists()) {
+        file.inputStream().use { load(it) }
+    }
+}
+
 android {
-    namespace = "arch.cayenne.lib.permonitor"
+    namespace = "arch.cayenne.lib.perf"
     compileSdk = 34
 
     defaultConfig {
@@ -14,8 +24,21 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
-    }
 
+        localProperties.getOrDefault("PERF_FPS_MONITOR", "false").let { it as String
+            buildConfigField("Boolean", "PERF_FPS_MONITOR", it)
+        }
+        localProperties.getOrDefault("PERF_MEMORY_MONITOR", "false").let { it as String
+            buildConfigField("Boolean", "PERF_MEMORY_MONITOR", it)
+        }
+        localProperties.getOrDefault("PERF_FRAME_DROP_MONITOR", "true").let { it as String
+            buildConfigField("Boolean", "PERF_FRAME_DROP_MONITOR", it)
+        }
+
+    }
+    buildFeatures {
+        buildConfig = true
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
--- external/lib_perf/src/main/java/arch/cayenne/lib/perf/PerfModuleInitializer.kt
+++ external/lib_perf/src/main/java/arch/cayenne/lib/perf/PerfModuleInitializer.kt
@@ -2,11 +2,11 @@ package arch.cayenne.lib.perf
 
 import android.content.Context
 import android.os.Looper
-import arch.cayenne.lib.base.BuildConfig
 import arch.cayenne.lib.base.data.DefaultInitializer
 import arch.cayenne.lib.base.utils.ext.LogUtilsExt.logd
 import arch.cayenne.lib.perf.monitor.FrameDropLogger
 import arch.cayenne.lib.perf.monitor.FPSMonitor
+import arch.cayenne.lib.perf.monitor.MemoryMonitor
 
 /**
  * @date: 2025/7/27 13:42
@@ -26,17 +26,19 @@ class PerfModuleInitializer : DefaultInitializer<String> {
      * 启动性能监控
      */
     private fun enablePerfMonitor() {
-        if (BuildConfig.DEBUG) {
+        if (BuildConfig.PERF_FPS_MONITOR) {
             val fpsMonitor = FPSMonitor()
             fpsMonitor.start()
-            /*val memoryMonitor = MemoryMonitor(1000)
-            memoryMonitor.start()*/
+        }
+        if (BuildConfig.PERF_MEMORY_MONITOR) {
+            val memoryMonitor = MemoryMonitor(1000)
+            memoryMonitor.start()
         }
     }
 
 
     private fun detectFrameDrop(){
-        if(BuildConfig.DEBUG) {
+        if(BuildConfig.PERF_FRAME_DROP_MONITOR) {
             val frameDropLogger = FrameDropLogger()
             frameDropLogger.start()
             Looper.getMainLooper().setMessageLogging(FrameDropLogger.LooperMonitor())
