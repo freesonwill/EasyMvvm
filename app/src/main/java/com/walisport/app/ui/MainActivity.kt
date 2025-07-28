794e23374ab095e4b01ca52f6f65682e497fb91b
wangzai <dafeix64@gmail.com>
2025-07-28 12:05:46 +0700

    bugfix.  https://linear.app/forgames/issue/WLS-1187 现象：直播间内的【注单-积分榜】的按钮位置，没有做自适应匹配宽度，导致UI（参考H5效果） 原因： 在直播间动画页内， density和scaledDensity被篡改。 解法： MainActivity启动的时候记录density和scaledDensity。  在动画直播页内进行恢复

--- app/src/main/java/com/walisport/app/ui/MainActivity.kt
+++ app/src/main/java/com/walisport/app/ui/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.walisport.app.ui
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.fragment.app.DialogFragment
 import arch.cayenne.lib.base.data.constants.StatusBarMode
 import arch.cayenne.lib.base.data.model.StatusBarConfig
+import arch.cayenne.lib.base.utils.ext.LogUtilsExt.logd
 import arch.cayenne.lib.common.ui.BaseNavActivity
 import arch.cayenne.lib.common.ui.dialog.CommonDialog
 import arch.cayenne.lib.common.ui.view.BetResultToastView
+import arch.cayenne.lib.common.utils.DensityUtil
 import arch.cayenne.lib.common.utils.ImmersionBarUtils.immersionBarColorExt
 import arch.cayenne.lib.common.utils.ImmersionBarUtils.immersionBarSkinTypeExt
 import arch.cayenne.lib.common.utils.ViewUtils
@@ -38,6 +41,10 @@ class MainActivity : BaseNavActivity<MainViewModel>() {
     }
 
     override fun initView(savedInstanceState: Bundle?) {
+
+        val metrics = resources.displayMetrics
+        DensityUtil.density = metrics.density
+        DensityUtil.scaledDensity = metrics.scaledDensity
         super.initView(savedInstanceState)
         fabFragment.show(this)
         notifyFragment.show(this)
--- module_live/src/main/kotlin/com/walisport/module/live/ui/LiveMatchAnimationFragment.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/LiveMatchAnimationFragment.kt
@@ -1,34 +1,25 @@
 package com.walisport.module.live.ui
 
-import android.animation.Animator
 import android.annotation.SuppressLint
+import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
 import android.os.Message
 import android.view.View
-import android.view.animation.LinearInterpolator
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebView
-import androidx.lifecycle.lifecycleScope
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
 import arch.cayenne.lib.base.utils.ext.LogUtilsExt.logd
+import arch.cayenne.lib.common.utils.DensityUtil
 import arch.cayenne.lib.common.utils.ext.ResourceExt.getColor
 import arch.cayenne.lib.common.utils.ext.sharedViewModel
-import arch.cayenne.lib.common.utils.ext.startSafeAnimateSet
-import arch.cayenne.lib.common.utils.ext.startSafeObjectAnimator
 import com.github.lzyzsd.jsbridge.BridgeWebViewClient
 import com.github.lzyzsd.jsbridge.DefaultHandler
-import com.walisport.module.live.R
-import com.walisport.module.live.data.constants.VideoAnimatorConstants.Companion.BUTTONS_ANIMATION_DURATION
-import com.walisport.module.live.data.constants.VideoAnimatorConstants.Companion.HIDE_BUTTONS_TIMER
 import com.walisport.module.live.databinding.FragmentLiveMatchAnimationBinding
 import com.walisport.module.live.ui.viewmodel.LiveMainViewModel
 import com.walisport.module.live.ui.viewmodel.LiveMatchAnimationViewModel
 import com.walisport.module.live.ui.viewmodel.LiveMatchMediaViewModel
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlin.reflect.KClass
 
 
@@ -55,6 +46,16 @@ class LiveMatchAnimationFragment :
 
     override fun initView(savedInstanceState: Bundle?) {
         mBinding.model = mViewModel
+
+        val metrics = resources.displayMetrics
+
+        //density和scaledDensity被篡改，尝试恢复
+        if (metrics.density != DensityUtil.density && DensityUtil.density > 0) {
+            metrics.density = DensityUtil.density
+        }
+        if (metrics.scaledDensity != DensityUtil.scaledDensity && DensityUtil.scaledDensity > 0) {
+            metrics.scaledDensity = DensityUtil.scaledDensity
+        }
 
         initWebView()
 //        scheduleHideButtons()
@@ -199,6 +200,11 @@ class LiveMatchAnimationFragment :
 
             setBackgroundColor(arch.cayenne.lib.common.R.color.color_80000000.getColor())
         }
+    }
+
+    override fun onDestroy() {
+        mBinding.animationView.destroy()
+        super.onDestroy()
     }
 
 
