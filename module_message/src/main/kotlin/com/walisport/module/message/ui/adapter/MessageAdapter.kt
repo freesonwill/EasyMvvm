c007d7ea787508ca5b90c98240b4e2bb4c5fd681
zhangsan <zhangsan@youguess.com>
2025-07-29 20:46:10 +0700

    fix: the delete button should use alpha animation

--- module_message/src/main/kotlin/com/walisport/module/message/ui/adapter/MessageAdapter.kt
+++ module_message/src/main/kotlin/com/walisport/module/message/ui/adapter/MessageAdapter.kt
@@ -31,7 +31,7 @@ class MessageAdapter : BaseAdapter<NotificationBean, BaseViewHolder, ViewBinding
                 binding.tvMsgTime.text = getTime(item.createTime)
                 binding.tvMsgTitle.text = item.title
                 binding.tvMsgContent.text = item.content
-                binding.ivMsgDelete.apply { addScaleOnTouchAnimation() }.setOnClickListener {
+                binding.ivMsgDelete.setOnClickListener {
                     clicklistener?.onDelete(item.id)
                 }
                 binding.layDetail.setOnClickListener {
@@ -43,7 +43,7 @@ class MessageAdapter : BaseAdapter<NotificationBean, BaseViewHolder, ViewBinding
                 binding.tvMsgTime.text = getTime(item.createTime)
                 binding.tvMsgTitle.text = item.title
                 binding.tvMsgContent.text = item.content
-                binding.ivMsgDelete.apply { addScaleOnTouchAnimation() }.setOnClickListener {
+                binding.ivMsgDelete.setOnClickListener {
                     clicklistener?.onDelete(item.id)
                 }
                 binding.layDetail.setOnClickListener {
@@ -55,7 +55,7 @@ class MessageAdapter : BaseAdapter<NotificationBean, BaseViewHolder, ViewBinding
                 binding.tvMsgTime.text = getTime(item.createTime)
                 binding.tvMsgTitle.text = item.title
                 binding.tvMsgContent.text = item.content
-                binding.ivMsgDelete.apply { addScaleOnTouchAnimation() }.setOnClickListener {
+                binding.ivMsgDelete.setOnClickListener {
                     clicklistener?.onDelete(item.id)
                 }
                 binding.layDetail.setOnClickListener {
