951affef95323c954fe7028dc571485bff9b56c3
KC.Tsai <KC.Tsai@skylinetw.com>
2025-07-30 20:19:44 +0800

    fix: 修正首頁進入有DB資料，但是讀取不到api資料時隱藏的聯賽打不開

--- module_home/src/main/java/arch/cayenne/module/home/ui/viewmodel/HomeViewModel.kt
+++ module_home/src/main/java/arch/cayenne/module/home/ui/viewmodel/HomeViewModel.kt
@@ -190,6 +190,7 @@ class HomeViewModel : BaseViewModel() {
                                 setCurrentTournamentId(selectedTournament.id)
                             }
                             tournaments.value = Event(list)
+                            setState(HomeState.Tournament.LoadSuccess)
                         }
                     }
                 }
@@ -276,15 +277,17 @@ class HomeViewModel : BaseViewModel() {
         }, {
             if (it is ApiResponseState.Succeeded<*>) {
                 setState(HomeState.Sport.LoadSuccess)
-            } else if (it is ApiResponseState.Failed && tournaments.value?.peekContent()?.isEmpty() == true) {
-                tournaments.value = Event(
-                    arrayListOf(
-                        TournamentDataModel.createAllItem(
-                            currentPlayTypeId,
-                            currentSportId
+            } else if (it is ApiResponseState.Failed) {
+                if (tournaments.value?.peekContent() == null || tournaments.value?.peekContent()?.isEmpty() == true) {
+                    tournaments.value = Event(
+                        arrayListOf(
+                            TournamentDataModel.createAllItem(
+                                currentPlayTypeId,
+                                currentSportId
+                            )
                         )
                     )
-                )
+                }
                 setState(HomeState.Sport.LoadFailure)
             }
         })
@@ -320,15 +323,17 @@ class HomeViewModel : BaseViewModel() {
         }, {
             if (it is ApiResponseState.Succeeded<*>) {
                 setState(HomeState.Tournament.LoadSuccess)
-            } else if (it is ApiResponseState.Failed && tournaments.value?.peekContent()?.isEmpty() == true) {
-                tournaments.value = Event(
-                    arrayListOf(
-                        TournamentDataModel.createAllItem(
-                            currentPlayTypeId,
-                            currentSportId
+            } else if (it is ApiResponseState.Failed) {
+                if ((tournaments.value?.peekContent() == null || tournaments.value?.peekContent()?.isEmpty() == true)) {
+                    tournaments.value = Event(
+                        arrayListOf(
+                            TournamentDataModel.createAllItem(
+                                currentPlayTypeId,
+                                currentSportId
+                            )
                         )
                     )
-                )
+                }
                 setState(HomeState.Tournament.LoadFailure)
             }
         })
