6d3290d7a6d59dcd1faf12ed8a2fe5bccb8c37c0
zhangsan <zhangsan@youguess.com>
2025-08-01 18:53:15 +0700

    test

--- module_home/src/main/java/arch/cayenne/module/home/ui/fragment/NewHomeFragment.kt
+++ module_home/src/main/java/arch/cayenne/module/home/ui/fragment/NewHomeFragment.kt
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.SimpleItemAnimator
 import arch.cayenne.lib.base.data.constants.StatusBarMode
 import arch.cayenne.lib.base.data.model.StatusBarConfig
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
+import arch.cayenne.lib.base.utils.ext.LogUtilsExt.logd
 import arch.cayenne.lib.common.data.constants.CurrencySymbols
 import arch.cayenne.lib.common.ui.viewmodel.observeEvent
 import arch.cayenne.lib.common.utils.ext.DeeplinkExt.deeplink
@@ -444,7 +445,9 @@ class NewHomeFragment : BaseFragment<HomeViewModel, FragmentNewHomeBinding>() {
                 addScaleOnTouchAnimation(ivWalletAdd)
             }.setOnClickListener {
                 //navigate(Uri.parse("walisport://module_home/homeFragment"))
-                navigate(Uri.parse("walisport://module_topup/topUpFragment"))
+                //navigate(Uri.parse("walisport://module_topup/topUpFragment"))
+                "aaaa----SimpleBottomFragment--click".logd(TAG)
+                SimpleBottomFragment().show(requireActivity().supportFragmentManager,"SimpleBottomFragment")
             }
             llFavoriteEntry.setOnClickListener {
                 navigate(NewHomeFragmentDirections.actionNewHomeFragmentToCollectListFragment())
