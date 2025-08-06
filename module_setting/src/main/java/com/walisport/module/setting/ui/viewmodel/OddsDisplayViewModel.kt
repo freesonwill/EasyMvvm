80c833419260ed63e6149e6dd028356eee5e0f48
xiaoyang <xiaoyang@abc.com>
2025-08-06 16:03:13 +0700

    feature:赔率显示设置改成本地保存，不再提交服务器 - https://linear.app/forgames/issue/MD-3265

--- module_setting/src/main/java/com/walisport/module/setting/ui/viewmodel/OddsDisplayViewModel.kt
+++ module_setting/src/main/java/com/walisport/module/setting/ui/viewmodel/OddsDisplayViewModel.kt
@@ -3,31 +3,27 @@ package com.walisport.module.setting.ui.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import arch.cayenne.lib.base.data.remote.ApiResponseState
 import arch.cayenne.lib.base.ui.viewmodel.BaseViewModel
 import arch.cayenne.lib.common.data.constants.OddsDisplayEnum
-import com.walisport.module.setting.data.OddsDisplayRepository
+import arch.cayenne.lib.common.data.constants.UserDataKey
+import arch.cayenne.lib.common.data.manager.UserDataManager
 import kotlinx.coroutines.launch
 
-class OddsDisplayViewModel(private val repo: OddsDisplayRepository): BaseViewModel() {
+class OddsDisplayViewModel(private val manager: UserDataManager,): BaseViewModel() {
 
     private val _displayType = MutableLiveData<OddsDisplayEnum>()
     val displayType: LiveData<OddsDisplayEnum> get() = _displayType
 
     init {
         viewModelScope.launch {
-            _displayType.value = repo.getOddsType()
+            val value = manager.getValue(UserDataKey.KEY_ODDS, OddsDisplayEnum.EU.value)
+            _displayType.value = OddsDisplayEnum.entries[value]
         }
     }
 
     //设置赔率方式
     fun setOddsType(type: OddsDisplayEnum) {
-        callApi( {
-            repo.setOddsType(type)
-        }, {
-            if (it is ApiResponseState.Succeeded<*>) {
-                _displayType.value = type
-            }
-        })
+        manager.setKeyValue(UserDataKey.KEY_ODDS, type.value)
+        _displayType.value = type
     }
 }
