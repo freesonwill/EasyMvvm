37da1b5df89bf1c18c69b78fe5d6c1adddcf81e0
Link Hsieh <link.hsieh@skylinetw.com>
2025-08-07 09:36:50 +0800

    fix : 修復預約未顯示可贏

--- lib_common/src/main/java/arch/cayenne/lib/common/utils/ext/SportMoneyOddsExt.kt
+++ lib_common/src/main/java/arch/cayenne/lib/common/utils/ext/SportMoneyOddsExt.kt
@@ -5,8 +5,6 @@ import arch.cayenne.lib.common.data.manager.UserDataManager
 import org.koin.java.KoinJavaComponent.inject
 import java.math.BigDecimal
 import java.math.RoundingMode
-import java.text.NumberFormat
-import java.util.Locale
 
 /**
  * 負責最終顯示於畫面上的賠率與金額，根據歐洲盤或香港盤做變化
@@ -15,24 +13,6 @@ object SportMoneyOddsExt {
 
     private val manager: UserDataManager = inject<UserDataManager>(UserDataManager::class.java).value
     private val oddsType: Int get() = manager.getValue(UserDataKey.KEY_ODDS, 0)
-
-    fun Long.getDisplayFormalMoney(odds: Int): String {
-        if (this == 0L || odds <= 0) return "0"
-
-        val result = this * (odds - oddsType * 100)
-        val decimal = BigDecimal(result).divide(BigDecimal(10000))
-            .setScale(2, RoundingMode.DOWN)
-
-        val stripped = decimal.stripTrailingZeros()
-
-        val numberFormat = NumberFormat.getNumberInstance(Locale.US).apply {
-            maximumFractionDigits = 2
-            minimumFractionDigits = 0
-            isGroupingUsed = true
-        }
-
-        return numberFormat.format(stripped)
-    }
 
     /**
      * @return string: 1234 轉換為 12.34, 1000 轉換為 10.00
--- module_bet/src/main/java/arch/cayenne/module/bet/viewmodel/SingleBetViewModel.kt
+++ module_bet/src/main/java/arch/cayenne/module/bet/viewmodel/SingleBetViewModel.kt
@@ -9,7 +9,7 @@ import arch.cayenne.lib.common.data.constants.CurrencySymbols
 import arch.cayenne.lib.common.data.repo.BalanceRepository
 import arch.cayenne.lib.common.ui.viewmodel.Event
 import arch.cayenne.lib.common.ui.viewmodel.NumberCalculatorViewModel
-import arch.cayenne.lib.common.utils.ext.SportMoneyOddsExt.getDisplayFormalMoney
+import arch.cayenne.lib.common.utils.ext.SportIntExt.getMoney
 import arch.cayenne.lib.common.utils.ext.SportMoneyOddsExt.reserveDisplayOdds
 import arch.cayenne.lib.common.utils.ext.SportStringExt.toMoney
 import arch.cayenne.lib.database.entity.BetSelectionBean
@@ -86,7 +86,7 @@ class SingleBetViewModel(
         addSource(_onBetSheetListener) { data ->
             if (_onReserveOddsListener.value == null) {
                 odds = data.odds
-                value = editValue.toMoney().getDisplayFormalMoney(odds)
+                value = editValue.toMoney().getMoney(odds)
             }
         }
         addSource(_onReserveOddsListener) { reserveOdds ->
@@ -97,7 +97,7 @@ class SingleBetViewModel(
             } else {
                 odds = reserveOdds
             }
-            value = editValue.toMoney().getDisplayFormalMoney(odds)
+            value = editValue.toMoney().getMoney(odds)
         }
         addSource(onEditNumber) {
             val money = if (it.isEmpty()) {
@@ -110,7 +110,7 @@ class SingleBetViewModel(
             value = if (money.isEmpty()) {
                 "0.00"
             } else {
-                money.toMoney().getDisplayFormalMoney(odds)
+                money.toMoney().getMoney(odds)
             }
         }
     }
