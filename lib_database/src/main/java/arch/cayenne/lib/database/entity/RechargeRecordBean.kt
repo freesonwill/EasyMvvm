65405c620a0b522fab3d412cb778a018b6977936
wangzai <dafeix64@gmail.com>
2025-08-08 23:59:22 +0700

    新增充值列表页

--- lib_database/src/main/java/arch/cayenne/lib/database/entity/RechargeRecordBean.kt
+++ lib_database/src/main/java/arch/cayenne/lib/database/entity/RechargeRecordBean.kt
@@ -1,15 +1,11 @@
 package arch.cayenne.lib.database.entity
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
 /**
  *
  * @date: 2025/8/8 17:14
  * @description:
  */
-@Entity
 data class RechargeRecordBean(
-    @PrimaryKey val id: Int, //充值记录id
+    val id: Int, //充值记录id
     val amount: String, //充值金额或数量
 )
