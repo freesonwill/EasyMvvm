f6ae6f1d3cb12c0d83d7260477ebd4e1a3433103
Link Hsieh <link.hsieh@skylinetw.com>
2025-08-11 11:00:03 +0800

    Revert "fix : 修復投注結果頁崩潰"
    This reverts commit 31080f2fdfd0bdb2665b9cd99b8018da65033388.

--- lib_base/src/main/java/arch/cayenne/lib/base/ui/fragment/BasePreLoadBottomSheerFragment.kt
+++ lib_base/src/main/java/arch/cayenne/lib/base/ui/fragment/BasePreLoadBottomSheerFragment.kt
@@ -110,22 +110,18 @@ abstract class BasePreLoadBottomSheerFragment<VM : BaseViewModel, VB : ViewBindi
 
     @CallSuper
     protected open fun setCustomCollapseSetting() {
+        dialog?.window?.decorView?.visibility = View.INVISIBLE
         sheetContainer?.translationY = 0f
+        backgroundView?.visibility = View.INVISIBLE
+        sheetContainer?.visibility = View.INVISIBLE
+        mBinding.root.visibility = View.INVISIBLE
 
-        if (sheetContainer?.visibility == View.VISIBLE) {
-            onEndListener?.invoke()
-            dialog?.window?.decorView?.visibility = View.INVISIBLE
-
-            backgroundView?.visibility = View.INVISIBLE
-            sheetContainer?.visibility = View.INVISIBLE
-            mBinding.root.visibility = View.INVISIBLE
-
-            sheetContainer?.let {
-                val behavior = BottomSheetBehavior.from(it)
-                behavior.isHideable = false
-                behavior.state = BottomSheetBehavior.STATE_COLLAPSED
-            }
+        sheetContainer?.let {
+            val behavior = BottomSheetBehavior.from(it)
+            behavior.isHideable = false
+            behavior.state = BottomSheetBehavior.STATE_COLLAPSED
         }
+        onEndListener?.invoke()
     }
 
     override fun playExitAnimations() {
