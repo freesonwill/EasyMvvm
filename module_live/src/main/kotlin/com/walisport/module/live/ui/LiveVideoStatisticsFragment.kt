3b9e7b1b782df7ed78b133e56e014b25ed07b20d
xiaoyang <xiaoyang@abc.com>
2025-08-13 14:33:10 +0700

    bugfix:直播间内的【赛况】的数据拉取和渲染有问题，包含横竖屏（竖屏加载完了赛况，横屏下赛况又需要拉取渲染很久），甚至赛况数据会丢失，显示无数据 - https://linear.app/forgames/issue/MD-1228

--- module_live/src/main/kotlin/com/walisport/module/live/ui/LiveVideoStatisticsFragment.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/LiveVideoStatisticsFragment.kt
@@ -2,8 +2,10 @@ package com.walisport.module.live.ui
 
 import android.os.Bundle
 import android.view.View
+import arch.cayenne.lib.base.data.constants.DataState
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
 import arch.cayenne.lib.common.ui.view.DynamicStateLayout
+import arch.cayenne.lib.common.ui.view.DynamicStateLayout.States
 import arch.cayenne.lib.common.utils.ext.ResourceExt.getString
 import arch.cayenne.lib.common.utils.ext.sharedViewModel
 import com.walisport.module.live.R
@@ -35,13 +37,13 @@ class LiveVideoStatisticsFragment :
         mViewModel.setMatchId(matchId)
         mBinding.viewTechStatic.setFullScreenMode()
         mBinding.viewTechEvent.setFullScreenMode()
+        mBinding.mainLayout.setState(DynamicStateLayout.States.LOADING, "")
     }
 
     override fun initData() {
         super.initData()
         mViewModel.getMainMatch(mViewModel.matchId())
         mainViewModel.registerStatisticsNotify(mViewModel.matchId())
-        mainViewModel.observeMatchStaticsNotify()
     }
 
     override fun initListener() {
@@ -54,7 +56,19 @@ class LiveVideoStatisticsFragment :
                 val awayName = it.basicInfo.awayTeam
                 val homeLogo = it.basicInfo.homeTeamIcon
                 val awayLogo = it.basicInfo.awayTeamIcon
+                mBinding.viewTechStatic.setScore(it.liveInfo.score)
+                mBinding.viewTechEvent.setTeamInfo(homeName, awayName, homeLogo, awayLogo)
                 mBinding.viewTechStatic.setTeamInfo(homeName, awayName, homeLogo, awayLogo)
+            }
+        }
+        mainViewModel.apiStateListener.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                DataState.NetworkUnavailable -> {
+                    mBinding.mainLayout.setState(
+                        States.NETWORK_ANOMALY,
+                        arch.cayenne.lib.common.R.string.error_net.getString()
+                    )
+                }
             }
         }
         mainViewModel.statisticData.observe(viewLifecycleOwner) {
