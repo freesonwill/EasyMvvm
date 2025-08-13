e94193d3665f0a8e0f5db5389f2c85c8a2fb5ec8
ricky <ricky.chang@skylinetw.com>
2025-08-13 15:57:42 +0800

    feat: - MD-3344 登录模组UI调整-登录，设置新密码，注册新用户，手机号验证,注册正式账号 - https://linear.app/forgames/issue/MD-3344

--- module_login/src/main/java/com/walisport/module/login/ui/fragment/LoginFragment.kt
+++ module_login/src/main/java/com/walisport/module/login/ui/fragment/LoginFragment.kt
@@ -1,12 +1,18 @@
 package com.walisport.module.login.ui.fragment
 
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import arch.cayenne.lib.base.data.constants.StatusBarMode
+import arch.cayenne.lib.base.data.model.StatusBarConfig
 import arch.cayenne.lib.base.ui.viewmodel.EmptyViewModel
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
 import arch.cayenne.lib.base.utils.ext.LogUtilsExt.logd
 import arch.cayenne.lib.common.ui.dialog.CommonDialog
+import arch.cayenne.lib.common.utils.ext.DimensionExt.dp2px
 import arch.cayenne.lib.common.utils.ext.NavigationExt.navigate
 import arch.cayenne.lib.common.utils.ext.clickNoRepeat
 import com.walisport.module.login.R
@@ -29,6 +35,15 @@ class LoginFragment : BaseFragment<EmptyViewModel, FragmentLoginBinding>() {
         val userId = args.userId
         "LoginFragment--->$userId,arguments:$arguments,args:$args".logd(TAG)
         mBinding.etUsername.setText(userId)
+        //val userId = arguments?.getString("userId")
+        val statusBarHeight =
+            ViewCompat.getRootWindowInsets(requireView())
+                ?.getInsets(WindowInsetsCompat.Type.statusBars())?.top ?: 0
+        val params = mBinding.ivLoginTitle.layoutParams as ViewGroup.MarginLayoutParams
+        // 設定 topMargin
+        params.topMargin = statusBarHeight + 77.dp2px
+        // 將修改後的 LayoutParams 重新應用到 maskView
+        mBinding.ivLoginTitle.layoutParams = params
     }
 
     override fun initListener() {
@@ -37,7 +52,8 @@ class LoginFragment : BaseFragment<EmptyViewModel, FragmentLoginBinding>() {
                 findNavController().navigateUp()
             }
             tvRegister.clickNoRepeat {
-                navigate(R.id.registerFragment)
+                navigate(R.id.registerNewAccountFragment)
+//                navigate(R.id.registerFragment)
             }
             tvGuest.clickNoRepeat {
 //                findNavController().navigateUp()
@@ -48,12 +64,25 @@ class LoginFragment : BaseFragment<EmptyViewModel, FragmentLoginBinding>() {
                 showPicVerifyDialog()
             }
             tvVerify.clickNoRepeat {
-               navigate(R.id.identifyVerifyFragment)
+//               navigate(R.id.identifyVerifyFragment)
+                navigate(R.id.newMobileVerifyFragment)
+            }
+            tvSetNewPassword.clickNoRepeat {
+                navigate(R.id.setNewPasswordFragment)
+            }
+            tvRegisterFormalAccount.clickNoRepeat {
+                navigate(R.id.registerFormalAccountFragment)
             }
         }
     }
 
     override suspend fun createObserver() {
+    }
+    override fun onStart() {
+        mBinding.root.fitsSystemWindows = false
+        StatusBarConfig.statusBarType = StatusBarMode.DRAW_BEHIND()
+        setStatusBar(StatusBarConfig,mBinding.ivLogo)
+        super.onStart()
     }
     private fun showConfirmDialog(id: Long) {
         CommonDialog.newInstance(
