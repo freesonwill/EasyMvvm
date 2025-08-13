fcd1530d37b48b92dce982b3954272f378958494
KC.Tsai <KC.Tsai@skylinetw.com>
2025-08-13 12:45:43 +0800

    feat: 優化MatchListAdapter

--- module_home/src/main/java/arch/cayenne/module/home/ui/adapter/MatchItemViewHolder.kt
+++ module_home/src/main/java/arch/cayenne/module/home/ui/adapter/MatchItemViewHolder.kt
@@ -15,6 +15,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.RecycledViewPool
 import arch.cayenne.lib.base.ui.adapter.BaseViewHolder
+import arch.cayenne.lib.base.utils.ext.LogUtilsExt.logd
 import arch.cayenne.lib.common.utils.ext.DimensionExt.dp2px
 import arch.cayenne.lib.common.utils.ext.SportStringExt.limitTitleLength
 import arch.cayenne.lib.common.utils.ext.toLocalDateTimeString
@@ -30,7 +31,7 @@ class MatchItemViewHolder(
     private val onMatchItemClickListener: OnMatchItemClickListener?,
     private val viewPool: RecycledViewPool
 ) : BaseViewHolder(mBinding) {
-    private var oddsColumnAdapter: OddsColumnAdapter = OddsColumnAdapter(onMatchItemClickListener)
+    private val oddsColumnAdapter: OddsColumnAdapter  by lazy { OddsColumnAdapter(onMatchItemClickListener) }
 
     init {
         //右半盤口
@@ -99,6 +100,10 @@ class MatchItemViewHolder(
             val basicInfo = data.match.basicInfo
             val liveInfo = data.match.liveInfo
 
+            val selectionsGrouped = data.markets.map { it.market to it.selections }
+            "KC_ oddsColumnAdapter.submitList ${data.match.basicInfo.matchName}".logd()
+            oddsColumnAdapter.submitList(selectionsGrouped)
+
             //賽事資訊
             setIconWithDefault(
                 basicInfo.tournamentIcon,
@@ -143,9 +148,6 @@ class MatchItemViewHolder(
                 tvAwayScore.text = ""
                 tvHomeScore.text = ""
             }
-
-            val selectionsGrouped = data.markets.map { it.market to it.selections }
-            oddsColumnAdapter.submitList(selectionsGrouped)
         }
     }
 
--- module_home/src/main/java/arch/cayenne/module/home/ui/viewmodel/MatchListViewModel.kt
+++ module_home/src/main/java/arch/cayenne/module/home/ui/viewmodel/MatchListViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.viewModelScope
 import arch.cayenne.lib.base.data.constants.DataState
 import arch.cayenne.lib.base.data.remote.ApiResponseState
 import arch.cayenne.lib.base.data.remote.ApiResponseState.Start.dataAs
+import arch.cayenne.lib.base.utils.ext.LogUtilsExt.logd
 import arch.cayenne.lib.base.utils.ext.LogUtilsExt.logi
 import arch.cayenne.lib.database.entity.MatchWithMarkets
 import arch.cayenne.module.home.data.constants.HomeState
@@ -71,7 +72,7 @@ class MatchListViewModel : BaseMatchViewModel<MatchListRepository>() {
 
     fun startObserveMatch() {
         if (observeJob != null) return
-        observeJob = viewModelScope.launch {
+        observeJob = viewModelScope.launch(Dispatchers.IO) {
             combine(
                 _selectedDate,
                 repository.observeMatchChange(_playType, _tournamentId).distinctUntilChanged()
@@ -82,7 +83,7 @@ class MatchListViewModel : BaseMatchViewModel<MatchListRepository>() {
                 val currentDateRefs = refs.filter { it.date == selectedDate }
                 if (currentDateRefs.isEmpty()) {
                     if (apiStateListener.value == null) {
-                        setState(HomeState.Match.Loading)
+                        launch(Dispatchers.Main) { setState(HomeState.Match.Loading) }
                     }
                     "Collect observeMatchChange TournamentMatchRef is NULL!  getMatchListData again!".logi(this@MatchListViewModel::class.java.simpleName)
                     getMatchListData()
@@ -95,7 +96,9 @@ class MatchListViewModel : BaseMatchViewModel<MatchListRepository>() {
                     currentDateRefs.map { it.matchId }
                 )
                 "Collect observeMatchChange result：${list.map { it.match.matchId }}".logi(this@MatchListViewModel::class.java.simpleName)
+                val start = System.currentTimeMillis()
                 withContext(Dispatchers.Main) {
+                    "KC_ 準備送出match list的live data ${System.currentTimeMillis() - start}".logd()
                     matchListChange.value = list
                 }
             }
@@ -105,53 +108,50 @@ class MatchListViewModel : BaseMatchViewModel<MatchListRepository>() {
     //取得分頁的比賽列表
     override fun getMatchListData() {
         viewModelScope.launch {
-            withContext(Dispatchers.Main) {
-
-                val (startTime, endTime) = if (_selectedDate.value == 0L) { //ALL
-                    if (_playType == PlayType.EARLY.id) {
-                        DateUtils.getFutureDays(1, Locale.getDefault())[0].third.let {
-                            Pair(it, it + BaseMatchRepository.THIRTY_DAY_TIME_STAMP)
-                        }
-                    } else {
-                        Pair(0L, 0L)
+            val (startTime, endTime) = if (_selectedDate.value == 0L) { //ALL
+                if (_playType == PlayType.EARLY.id) {
+                    DateUtils.getFutureDays(1, Locale.getDefault())[0].third.let {
+                        Pair(it, it + BaseMatchRepository.THIRTY_DAY_TIME_STAMP)
                     }
                 } else {
-                    Pair(
-                        _selectedDate.value,
-                        _selectedDate.value + BaseMatchRepository.ONE_DAY_TIME_STAMP
-                    )
+                    Pair(0L, 0L)
                 }
-                "取得比賽資料  PlayType = $_playType sportId = $_sportId tournamentId = $_tournamentId page = $page startTime = $startTime endTime = $endTime".logi(
-                    TAG
-                )
-                callApi(
-                    {
-                        repository.getAllMatch(
-                            playType = _playType,
-                            sportId = _sportId,
-                            tournamentId = _tournamentId,
-                            page = page,
-                            date = _selectedDate.value,
-                            startTime = startTime,
-                            endTime = endTime,
-                        )
-                    },
-                    {
-                        if (it is ApiResponseState.Failed) {
-                            matchListChange.value = arrayListOf()
-                        } else if (it is ApiResponseState.Succeeded<*>) {
-                            val size = it.dataAs<List<Common.Match>>()?.size ?: 0
-                            val isEmpty = size == 0
-                            if (page == 1 && isEmpty) {
-                                matchListChange.value = arrayListOf()
-                                setState(HomeState.Match.DataEmpty)
-                            } else if (size < BaseMatchRepository.DEFAULT_MATCH_SIZE) {   //如果返回成功，但是数据size小于10，则表明列表已经加载到底部
-                                setState(DataState.NoMoreData)
-                            }
-                        }
-                    }
+            } else {
+                Pair(
+                    _selectedDate.value,
+                    _selectedDate.value + BaseMatchRepository.ONE_DAY_TIME_STAMP
                 )
             }
+            "取得比賽資料  PlayType = $_playType sportId = $_sportId tournamentId = $_tournamentId page = $page startTime = $startTime endTime = $endTime".logi(
+                TAG
+            )
+            callApi(
+                {
+                    repository.getAllMatch(
+                        playType = _playType,
+                        sportId = _sportId,
+                        tournamentId = _tournamentId,
+                        page = page,
+                        date = _selectedDate.value,
+                        startTime = startTime,
+                        endTime = endTime,
+                    )
+                },
+                {
+                    if (it is ApiResponseState.Failed) {
+                        matchListChange.value = arrayListOf()
+                    } else if (it is ApiResponseState.Succeeded<*>) {
+                        val size = it.dataAs<List<Common.Match>>()?.size ?: 0
+                        val isEmpty = size == 0
+                        if (page == 1 && isEmpty) {
+                            matchListChange.value = arrayListOf()
+                            setState(HomeState.Match.DataEmpty)
+                        } else if (size < BaseMatchRepository.DEFAULT_MATCH_SIZE) {   //如果返回成功，但是数据size小于10，则表明列表已经加载到底部
+                            setState(DataState.NoMoreData)
+                        }
+                    }
+                }
+            )
         }
     }
 
