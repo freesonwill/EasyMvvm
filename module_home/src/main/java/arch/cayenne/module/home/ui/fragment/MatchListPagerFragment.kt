6a433c0f19db59e71b151c258aff3e02721609e0
xiaoyang <xiaoyang@abc.com>
2025-08-14 11:31:39 +0700

    bugfix:在light模式下，toast提示的背景色不符合设计规范（参考UI设计图样式） - https://linear.app/forgames/issue/MD-1289

--- module_home/src/main/java/arch/cayenne/module/home/ui/fragment/MatchListPagerFragment.kt
+++ module_home/src/main/java/arch/cayenne/module/home/ui/fragment/MatchListPagerFragment.kt
@@ -65,6 +65,7 @@ class MatchListPagerFragment :
                 }
 
                 override fun onFavoriteClick(item: MatchWithMarkets) {
+                    showToast("测试Toast弹窗")
                     mViewModel.addMatchCollect(item, !item.match.collect)
                 }
 
