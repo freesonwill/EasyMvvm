2f2f8081396ffac669f07dfd5e7edd50f0fac76b
Jeremy <jeremy.huang@skylinetw.com>
2025-08-27 16:08:40 +0800

    style: update selected tab text style
    #MD-3542 搜索页tab在选中时， 文字变色效果不明显， 请确认

--- module_search/src/main/kotlin/com/walisport/module/search/ui/fragment/SearchResultListFragment.kt
+++ module_search/src/main/kotlin/com/walisport/module/search/ui/fragment/SearchResultListFragment.kt
@@ -1,14 +1,19 @@
 package com.walisport.module.search.ui.fragment
 
+import android.graphics.Typeface
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.core.view.children
+import androidx.core.view.doOnLayout
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import arch.cayenne.lib.common.utils.ext.animateIndicatorToPosition
 import arch.cayenne.lib.common.utils.ext.setupViewPagerScroll
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -18,7 +23,7 @@ import com.walisport.module.search.ui.adapter.SearchResultPagerAdapter
 import com.walisport.module.search.ui.viewmodel.SearchResultListViewModel
 import java.util.Locale
 import kotlin.reflect.KClass
-import arch.cayenne.lib.common.utils.ext.animateIndicatorToPosition
+
 class SearchResultListFragment :
     SearchBaseFragment<SearchResultListViewModel, FragmentSearchResultListBinding>() {
     override val vmClass: KClass<SearchResultListViewModel>
@@ -32,26 +37,35 @@ class SearchResultListFragment :
     override fun initView(savedInstanceState: Bundle?) {
         super.initView(savedInstanceState)
         setViewPager()
-        // 自定義滑動行為
-        contentBinding.viewPager.setupViewPagerScroll(contentBinding.tlSearch,contentBinding.customIndicator,0.20f){
-            skipAnyAnim = it
+
+        with(contentBinding) {
+            // 自定義滑動行為
+            viewPager.setupViewPagerScroll(tlSearch, customIndicator, 0.20f) {
+                skipAnyAnim = it
+            }
+            tlSearch.apply {
+                clearOnTabSelectedListeners()
+                addOnTabSelectedListener(object :
+                    TabLayout.OnTabSelectedListener {
+                    override fun onTabSelected(tab: TabLayout.Tab?) {
+                        if (skipAnyAnim) {
+                            // 动画更新指示器位置
+                            customIndicator.animateIndicatorToPosition(
+                                tab?.position ?: 0, 0
+                            )
+                            viewPager.setCurrentItem(tab?.position ?: 0, false)
+                        }
+                        tab?.let { updateTabTypeface(it, true) }
+                    }
+                    override fun onTabUnselected(tab: TabLayout.Tab?) {
+                        tab?.let { updateTabTypeface(it, false) }
+                    }
+                    override fun onTabReselected(tab: TabLayout.Tab?) {
+                        tab?.let { updateTabTypeface(it, true) }
+                    }
+                })
+            }
         }
-        contentBinding.tlSearch.clearOnTabSelectedListeners()
-        contentBinding.tlSearch.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                if (skipAnyAnim) {
-                    // 动画更新指示器位置
-                    contentBinding.customIndicator.animateIndicatorToPosition(tab?.position?:0, 0)
-                    contentBinding.viewPager.setCurrentItem(tab?.position?:0, false)
-                }
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-            }
-        })
     }
 
     override fun initData() {
@@ -78,6 +92,16 @@ class SearchResultListFragment :
         tabMediator?.detach()
         tabMediator = null
         super.onDestroyView()
+    }
+
+    private fun updateTabTypeface(tab: TabLayout.Tab, isBold: Boolean) {
+        tab.view.children.find { it is TextView }?.let {
+            (it as TextView).apply {
+                post {
+                    setTypeface(null, if (isBold) Typeface.BOLD else Typeface.NORMAL)
+                }
+            }
+        }
     }
 
     private fun setViewPager() {
@@ -121,6 +145,7 @@ class SearchResultListFragment :
                     }
 
                 switchTab(0, false)
+                tlSearch.getTabAt(0)?.let { updateTabTypeface(it, true) }
             }
         }
     }
