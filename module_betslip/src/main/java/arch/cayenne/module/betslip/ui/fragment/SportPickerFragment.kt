99e2941f776b58df531208c33602fc0d604767f8
Link Hsieh <link.hsieh@skylinetw.com>
2025-08-28 17:11:27 +0800

    fix : 修正注單體育遮罩未遮住導航欄

--- module_betslip/src/main/java/arch/cayenne/module/betslip/ui/fragment/SportPickerFragment.kt
+++ module_betslip/src/main/java/arch/cayenne/module/betslip/ui/fragment/SportPickerFragment.kt
@@ -1,21 +1,28 @@
 package arch.cayenne.module.betslip.ui.fragment
 
-import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.content.Context
+import android.graphics.Color
+import android.graphics.PixelFormat
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
-import androidx.core.animation.addListener
+import android.view.WindowManager
+import androidx.core.animation.doOnEnd
+import androidx.core.animation.doOnStart
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.SimpleItemAnimator
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
 import arch.cayenne.lib.common.data.constants.AnimationConstants
+import arch.cayenne.lib.common.utils.ViewUtils
 import arch.cayenne.module.betslip.data.constants.Config
 import arch.cayenne.module.betslip.databinding.FragmentSportPickerBinding
 import arch.cayenne.module.betslip.ui.adapter.SportPickerAdapter
 import arch.cayenne.module.betslip.ui.viewmodel.SportPickerViewModel
 import kotlin.reflect.KClass
 
-class SportPickerFragment private constructor(): BaseFragment<SportPickerViewModel, FragmentSportPickerBinding>() {
+class SportPickerFragment private constructor() :
+    BaseFragment<SportPickerViewModel, FragmentSportPickerBinding>() {
 
     companion object {
         fun newInstance(sportIds: List<Int>): SportPickerFragment {
@@ -41,15 +48,17 @@ class SportPickerFragment private constructor(): BaseFragment<SportPickerViewMod
         })
     }
 
+    private var dimView: View? = null
+
     override fun initView(savedInstanceState: Bundle?) {
-        mBinding.clFilter.visibility = View.INVISIBLE
-        mBinding.maskView.visibility = View.INVISIBLE
+        mBinding.root.visibility = View.INVISIBLE
+        initDim()
         mBinding.rvSport.adapter = sportAdapter
         (mBinding.rvSport.itemAnimator as? SimpleItemAnimator)?.supportsChangeAnimations = false
     }
 
     override fun initData() {
-        requireArguments().getIntArray(Config.VALUE_SELECTED_SPORT_ID)?.let {  sportIds ->
+        requireArguments().getIntArray(Config.VALUE_SELECTED_SPORT_ID)?.let { sportIds ->
             mViewModel.setSelectedById(sportIds)
         }
     }
@@ -62,21 +71,68 @@ class SportPickerFragment private constructor(): BaseFragment<SportPickerViewMod
         mBinding.tvConfirm.setOnClickListener {
             sendResult()
         }
-        mBinding.maskView.setOnClickListener {
-            collapseView()
-        }
     }
 
     override suspend fun createObserver() {
-        mViewModel.onSportListener.observe(viewLifecycleOwner){
+        mViewModel.onSportListener.observe(viewLifecycleOwner) {
             sportAdapter.submitList(it) {
-                if (mBinding.clFilter.visibility != View.VISIBLE) {
-                    mBinding.clFilter.post {
+                if (mBinding.root.visibility != View.VISIBLE) {
+                    mBinding.root.post {
                         expandView()
                     }
                 }
             }
         }
+    }
+
+    private fun initDim() {
+        val windowManager = requireContext().getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+        // 取得目標 View 在螢幕上的位置
+        val location = IntArray(2)
+        val targetView = mBinding.root
+        mBinding.root.getLocationOnScreen(location)
+        val targetViewBottomY = location[1] + targetView.height
+
+        // 獲取螢幕總高度
+        val screenHeight = resources.displayMetrics.heightPixels
+
+        // 創建 dim View
+        dimView = View(requireActivity()).apply {
+            setBackgroundColor(Color.BLACK)
+            alpha = 0.75f
+            visibility = View.INVISIBLE
+            setOnClickListener {
+                collapseView()
+            }
+        }
+
+
+        val params = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.MATCH_PARENT, // 寬度填滿
+            screenHeight - targetViewBottomY + ViewUtils.getNavigationBarHeight(requireContext()), // 高度：從目標 View 底部到螢幕底部
+            WindowManager.LayoutParams.TYPE_APPLICATION_PANEL, // 視窗類型，適合附加在 Activity 上
+            // flags
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                    or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                    or WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+            PixelFormat.TRANSLUCENT
+        )
+        params.gravity = Gravity.TOP or Gravity.START
+        params.y = targetViewBottomY
+
+        // 6. 將 View 添加到 window
+        windowManager.addView(dimView, params)
+    }
+
+    private fun removeDim() {
+        val v = dimView ?: return
+        dimView = null
+        v.alpha = 0f
+        mBinding.maskView.alpha = 0f
+        val windowManager = requireContext().getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        windowManager.removeView(v)
+        mBinding.maskView.visibility = View.GONE
     }
 
     fun dismiss() {
@@ -92,39 +148,38 @@ class SportPickerFragment private constructor(): BaseFragment<SportPickerViewMod
     }
 
     private fun expandView() {
-        val clContent = mBinding.clContent
-        val height = clContent.height
-        val contentAnimate =
-            ObjectAnimator.ofFloat(clContent, "translationY", -height.toFloat(), 0f).apply {
-                addListener(onStart = {
-                    mBinding.clFilter.visibility = View.VISIBLE
-                    mBinding.maskView.visibility = View.VISIBLE
-                })
-            }
-        val maskAlphaAnimate = ObjectAnimator.ofFloat(mBinding.maskView, "alpha", 0f, 0.75f)
-        AnimatorSet().apply {
+        mBinding.root.bringToFront()
+        val root = mBinding.root
+        val height = root.height
+        ObjectAnimator.ofFloat(root, "translationY", -height.toFloat(), 0f).apply {
             duration = AnimationConstants.DIALOG_POPUP_DURATION
-            playTogether(contentAnimate, maskAlphaAnimate)
+            addUpdateListener {
+                val value = it.animatedValue as Float
+                dimView?.translationY = height + value
+                if (dimView?.visibility != View.VISIBLE) {
+                    dimView?.visibility = View.VISIBLE
+                }
+            }
+            doOnStart{
+                mBinding.root.visibility = View.VISIBLE
+            }
             start()
         }
     }
 
     fun collapseView() {
         mBinding.root.bringToFront()
-        val clContent = mBinding.clContent
-        val targetHeight = clContent.height
-        val contentAnimate =
-            ObjectAnimator.ofFloat(clContent, "translationY", 0f, -targetHeight.toFloat()).apply {
-                addListener(onEnd = {
-                    mBinding.clFilter.visibility = View.INVISIBLE
-                    mBinding.maskView.visibility = View.INVISIBLE
-                    dismiss()
-                })
-            }
-        val maskAlphaAnimate = ObjectAnimator.ofFloat(mBinding.maskView, "alpha", 0.75f, 0f)
-        AnimatorSet().apply {
+        val root = mBinding.root
+        val targetHeight = root.height
+        ObjectAnimator.ofFloat(root, "translationY", 0f, -targetHeight.toFloat()).apply {
             duration = AnimationConstants.DIALOG_POPUP_DURATION
-            playTogether(contentAnimate, maskAlphaAnimate)
+            addUpdateListener {
+                removeDim()
+            }
+            doOnEnd {
+                mBinding.root.visibility = View.INVISIBLE
+                dismiss()
+            }
             start()
         }
     }
@@ -141,7 +196,15 @@ class SportPickerFragment private constructor(): BaseFragment<SportPickerViewMod
 
     private fun sendResult() {
         val bean = mViewModel.getSelectedSportBean()
-        resultBundle.putIntArray(Config.VALUE_SELECTED_SPORT_ID, bean.map { it.sportId }.toIntArray())
+        resultBundle.putIntArray(
+            Config.VALUE_SELECTED_SPORT_ID,
+            bean.map { it.sportId }.toIntArray()
+        )
         collapseView()
+    }
+
+    override fun onBackPressed(): Boolean {
+        collapseView()
+        return super.onBackPressed()
     }
 }
