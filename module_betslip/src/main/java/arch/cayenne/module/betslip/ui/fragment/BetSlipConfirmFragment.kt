b90ce0189725233c4a50dd0e69b0b50d89cc377c
xiaoyang <xiaoyang@abc.com>
2025-08-28 16:37:27 +0700

    bugfix:列表滑动到底部之前进行预加载功能 - https://linear.app/forgames/issue/MD-3557

--- module_betslip/src/main/java/arch/cayenne/module/betslip/ui/fragment/BetSlipConfirmFragment.kt
+++ module_betslip/src/main/java/arch/cayenne/module/betslip/ui/fragment/BetSlipConfirmFragment.kt
@@ -3,7 +3,6 @@ package arch.cayenne.module.betslip.ui.fragment
 import android.net.Uri
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import arch.cayenne.lib.common.utils.copyToClipboard
 import arch.cayenne.lib.common.utils.ext.NavigationExt.navigate
 import arch.cayenne.lib.common.utils.helper.showToast
