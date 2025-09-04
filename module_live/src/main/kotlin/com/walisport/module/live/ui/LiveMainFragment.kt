f40d0152a8d7a8789976ad8414030ed4cfeab883
zhangsan <zhangsan@youguess.com>
2025-09-04 15:30:28 +0700

    chore: migrate bet_title and related strings into lib/res

--- module_live/src/main/kotlin/com/walisport/module/live/ui/LiveMainFragment.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/LiveMainFragment.kt
@@ -374,7 +374,7 @@ class LiveMainFragment : BaseFragment<LiveMainViewModel, FragmentLiveMainBinding
         val tabSelectPosition = 1
         with(mBinding) {
             val list = listOf(
-                PagerBean(R.string.live_note_order.getString()) { BetSlipFragment() },
+                PagerBean(arch.cayenne.lib.res.R.string.bet_title.getString()) { BetSlipFragment() },
                 PagerBean(R.string.live_bet_on.getString()) { LiveBetOnFragment() },
                 PagerBean(R.string.live_chat.getString()) { LiveChatFragment() },
                 PagerBean(R.string.live_outs.getString()) { LiveOutsFragment() },
