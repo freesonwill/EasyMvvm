00e930c718841c2695bd4d25a326680edd638cec
wenxi <russelllin465@gmail.com>
2025-09-05 15:47:34 +0700

    feature：注单除了加载更多，recylerView不会滚动

--- module_betslip/src/main/java/arch/cayenne/module/betslip/ui/fragment/BetSlipSettledFragment.kt
+++ module_betslip/src/main/java/arch/cayenne/module/betslip/ui/fragment/BetSlipSettledFragment.kt
@@ -77,8 +77,10 @@ class BetSlipSettledFragment :
         super.createObserver()
         mViewModel.orderLiveData.observe(viewLifecycleOwner) {
             betSlipAdapter.submitList(it) {
-                val position = if (mViewModel.loadDataType == LoadDataType.LOAD_MORE) betSlipAdapter.itemCount - 1 else 0
-                mBinding.recyclerView.scrollToPosition(position)
+                if (mViewModel.loadDataType == LoadDataType.LOAD_MORE){
+                    val position =  betSlipAdapter.itemCount - 1
+                    mBinding.recyclerView.scrollToPosition(position)
+                }
                 mViewModel.loadDataType = LoadDataType.NONE
                 registerListener()
             }
