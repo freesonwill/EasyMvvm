a52d64ce475ed62b1ac9c357b9429d962299e4e4
zhangsan <zhangsan@youguess.com>
2025-09-05 16:30:31 +0700

    fix: msg was treated as error even though response was successful

--- lib_websocket/src/main/java/arch/cayenne/lib/websocket/extension/ProtoExtension.kt
+++ lib_websocket/src/main/java/arch/cayenne/lib/websocket/extension/ProtoExtension.kt
@@ -38,23 +38,26 @@ inline fun <reified T: GeneratedMessageLite<*,*>> WebSocketManager.observeProtoM
     .filter { it.mid == apiCode.mid && it.sid == apiCode.sid }
     .map {
         try {
+            val TAG = WebSocketManager::class.java.simpleName
             val proto = it.originProto?.let { byteArray ->
                 T::class.java.getMethod("parseFrom", ByteArray::class.java)
                     .invoke(null, byteArray) as T
             }
-            val error = runCatching { T::class.java.getMethod("getMessage").invoke(proto) as String }
-                .getOrNull().let{ if( it.isNullOrEmpty()) null else it }
-            if(error == null) {
-                "sendAndWaitProtoMessageResponse map proto apiCode:$apiCode,rid:${it.rid},success".logi(WebSocketManager::class.java.simpleName)
+            val success:Boolean = runCatching { T::class.java.getMethod("getSuccess").invoke(proto) as Boolean }
+                .getOrNull() ?: true
+            val error:String = runCatching { T::class.java.getMethod("getMessage").invoke(proto) as String }
+                .getOrNull() ?: ""
+            if(success) {
+                "sendAndWaitProtoMessageResponse map proto apiCode:$apiCode,rid:${it.rid},success".logi(TAG)
             } else {
-                "sendAndWaitProtoMessageResponse map proto apiCode:$apiCode,rid:${it.rid},error:$error".loge(WebSocketManager::class.java.simpleName)
+                "sendAndWaitProtoMessageResponse map proto apiCode:$apiCode,rid:${it.rid},error:$error".loge(TAG)
             }
             return@map SocketResponseData(
                 mid = it.mid,
                 sid = it.sid,
                 rid = it.rid,
-                data = error ?.let { null } ?:proto,
-                error = error ?.let { SimpleResponseError(it) }
+                data = if(success) proto else null,
+                error = if(!success) SimpleResponseError(error) else null
             )
         } catch (e: Exception) {
             e.printStackTrace()
