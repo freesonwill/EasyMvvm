d8fc66707873d1f4cc310f6895dc37a0fb1a8901
Link Hsieh <link.hsieh@skylinetw.com>
2025-09-10 11:07:44 +0800

    fix : 修復彈窗展示時，關閉螢幕會強制收起彈窗

--- lib_base/src/main/java/arch/cayenne/lib/base/ui/fragment/BasePreLoadBottomSheerFragment.kt
+++ lib_base/src/main/java/arch/cayenne/lib/base/ui/fragment/BasePreLoadBottomSheerFragment.kt
@@ -10,10 +10,8 @@ import android.view.WindowManager
 import androidx.annotation.CallSuper
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.animation.doOnEnd
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
 import androidx.viewbinding.ViewBinding
 import arch.cayenne.lib.base.ui.view.UnhideableBottomSheetDialog
 import arch.cayenne.lib.base.ui.viewmodel.BaseViewModel
@@ -49,6 +47,9 @@ abstract class BasePreLoadBottomSheetFragment<VM : BaseViewModel, VB : ViewBindi
             override fun show() {
                 if (systemHide) {
                     systemHide = false
+                    if (!isDismissing) {
+                        setCustomExpendSetting()
+                    }
                     return
                 }
                 super.show()
@@ -57,7 +58,6 @@ abstract class BasePreLoadBottomSheetFragment<VM : BaseViewModel, VB : ViewBindi
 
             override fun hide() {
                 systemHide = true
-                setSystemHide()
             }
 
         }
@@ -208,22 +208,6 @@ abstract class BasePreLoadBottomSheetFragment<VM : BaseViewModel, VB : ViewBindi
                 setCustomCollapseSetting()
             }
         }
-    }
-
-    private fun setSystemHide() {
-        fun findDialogFragment(fragmentManager: FragmentManager) {
-            fragmentManager.fragments.forEach {
-                if (it is DialogFragment) {
-                    it.dismissAllowingStateLoss()
-                } else {
-                    findDialogFragment(it.childFragmentManager)
-                }
-            }
-        }
-        findDialogFragment(childFragmentManager)
-        customHide()
-//        setCustomCollapseSetting()
-//        hideDim()
     }
 }
 
