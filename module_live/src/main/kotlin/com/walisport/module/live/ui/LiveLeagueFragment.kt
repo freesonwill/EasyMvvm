f0c2a2f9a30d7f04f765ea7a3f5c1e305f8ac5ad
xiaoyang <xiaoyang@abc.com>
2025-09-10 15:59:47 +0700

    bugfix:联赛日程列表吸顶效果优化 - https://linear.app/forgames/issue/MD-3695

--- module_live/src/main/kotlin/com/walisport/module/live/ui/LiveLeagueFragment.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/LiveLeagueFragment.kt
@@ -148,8 +148,6 @@ class LiveLeagueFragment : BaseFragment<LeagueViewModel, FragmentLeagueBinding>(
                 mBinding.refreshLayout.setEnableLoadMore(false)
                 mBinding.recyclerLeague.visibility = View.GONE //网络异常时需隐藏列表
                 mBinding.leagueRoot.background = arch.cayenne.lib.common.R.color.black.getDrawable()
-                mBinding.leagueBar.background = null
-                mBinding.leagueBody.background = null
                 mBinding.leagueMain.setState(
                     States.NETWORK_ANOMALY(),
                     arch.cayenne.lib.common.R.string.error_net.getString()
