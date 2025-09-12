ca4f5a81b643974518016d1f1ae7d95682dd1994
zhangsan <zhangsan@youguess.com>
2025-09-12 16:26:22 +0700

    feat: split getValue() into 2 functions, one for primitive types and another for object types

--- lib_common/src/main/java/arch/cayenne/lib/common/data/manager/UserDataManager.kt
+++ lib_common/src/main/java/arch/cayenne/lib/common/data/manager/UserDataManager.kt
@@ -34,10 +34,16 @@ class UserDataManager {
         flows.remove(key)
     }
 
+    /**
+     * 获取基础类型
+     * @param T
+     * @param key
+     * @param default
+     * @return 非空
+     */
     inline fun <reified T> getValue(
         key: UserDataKey,
-        default: T? = null,
-        noinline deserializer: (String?) -> T? = { GsonUtils.fromJson(it, T::class.java) }
+        default: T,
     ): T {
         return when (default) {
             is String -> mmkv.decodeString(key.key, default) as T
@@ -45,8 +51,23 @@ class UserDataManager {
             is Int -> mmkv.decodeInt(key.key, default) as T
             is Long -> mmkv.decodeLong(key.key, default) as T
             is Float -> mmkv.decodeFloat(key.key, default) as T
-            else -> deserializer(mmkv.decodeString(key.key)) as T
+            else -> throw IllegalStateException("❌ Unsupported primitive type: ${T::class} ")
         }
+    }
+
+    /**
+     * 获取对象类型
+     *
+     * @param T
+     * @param key
+     * @param deserializer
+     * @return 可空
+     */
+    inline fun <reified T> getValue(
+        key: UserDataKey,
+        noinline deserializer: (String?) -> T? = { GsonUtils.fromJson(it, T::class.java) }
+    ): T? {
+        return deserializer(mmkv.decodeString(key.key))
     }
 
     private fun notifyChanged(key: UserDataKey, value: Any?) {
