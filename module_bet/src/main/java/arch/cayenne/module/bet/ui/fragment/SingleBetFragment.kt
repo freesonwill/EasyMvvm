c1ea75548830fd6531c14fe1594023dc08b15069
Link Hsieh <link.hsieh@skylinetw.com>
2025-09-15 14:43:12 +0800

    feat : 移除import

--- module_bet/src/main/java/arch/cayenne/module/bet/ui/fragment/SingleBetFragment.kt
+++ module_bet/src/main/java/arch/cayenne/module/bet/ui/fragment/SingleBetFragment.kt
@@ -6,13 +6,10 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewTreeObserver
 import android.view.animation.LinearInterpolator
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import arch.cayenne.lib.base.data.constants.DataState
-import arch.cayenne.lib.base.ui.animation.IInterpolatorOption
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
 import arch.cayenne.lib.common.ui.fragment.ReserveDialogFragment
 import arch.cayenne.lib.common.ui.view.NumberKeyboardView
