087187d4b9618ea92194b99a44f0259560b9be92
Link Hsieh <link.hsieh@skylinetw.com>
2025-09-15 18:14:29 +0800

    feat : rv藉由設置isNestedScrollingEnabled控制彈窗是否自適應

--- module_bet/src/main/java/arch/cayenne/module/bet/ui/fragment/SingleBetFragment.kt
+++ module_bet/src/main/java/arch/cayenne/module/bet/ui/fragment/SingleBetFragment.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewTreeObserver
 import android.view.animation.LinearInterpolator
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
