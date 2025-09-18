8726137b6371092cbee97c9db5caaf58e62c8941
Link Hsieh <link.hsieh@skylinetw.com>
2025-09-18 17:58:01 +0800

    feat : 調整鍵盤點擊響應

--- lib_common/src/main/java/arch/cayenne/lib/common/ui/view/NumberKeyboardView.kt
+++ lib_common/src/main/java/arch/cayenne/lib/common/ui/view/NumberKeyboardView.kt
@@ -1,8 +1,11 @@
 package arch.cayenne.lib.common.ui.view
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
 import android.widget.LinearLayout
 import arch.cayenne.lib.common.databinding.LayoutNumberKeyboardBinding
 
@@ -12,25 +15,41 @@ class NumberKeyboardView : LinearLayout {
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
-    private val mBinding: LayoutNumberKeyboardBinding
+    val mBinding: LayoutNumberKeyboardBinding
     private var mListener: OnCalculatorClickListener? = null
 
     init {
         val layoutInflater = LayoutInflater.from(context)
         mBinding = LayoutNumberKeyboardBinding.inflate(layoutInflater, this, true)
         mBinding.apply {
-            btnZero.setOnClickListener { mListener?.onNumberClick(0) }
-            btnOne.setOnClickListener { mListener?.onNumberClick(1) }
-            btnTwo.setOnClickListener { mListener?.onNumberClick(2) }
-            btnThree.setOnClickListener { mListener?.onNumberClick(3) }
-            btnFour.setOnClickListener { mListener?.onNumberClick(4) }
-            btnFive.setOnClickListener { mListener?.onNumberClick(5) }
-            btnSix.setOnClickListener { mListener?.onNumberClick(6) }
-            btnSeven.setOnClickListener { mListener?.onNumberClick(7) }
-            btnEight.setOnClickListener { mListener?.onNumberClick(8) }
-            btnNine.setOnClickListener { mListener?.onNumberClick(9) }
-            btnDot.setOnClickListener { mListener?.onDotClick() }
-            btnOther.setOnClickListener { mListener?.onOtherClick() }
+            setNumberTouch(btnOne) { mListener?.onNumberClick(0) }
+            setNumberTouch(btnOne) { mListener?.onNumberClick(1) }
+            setNumberTouch(btnTwo) { mListener?.onNumberClick(2) }
+            setNumberTouch(btnThree) { mListener?.onNumberClick(3) }
+            setNumberTouch(btnFour) { mListener?.onNumberClick(4) }
+            setNumberTouch(btnFive) { mListener?.onNumberClick(5) }
+            setNumberTouch(btnSix) { mListener?.onNumberClick(6) }
+            setNumberTouch(btnSeven) { mListener?.onNumberClick(7) }
+            setNumberTouch(btnEight) { mListener?.onNumberClick(8) }
+            setNumberTouch(btnNine) { mListener?.onNumberClick(9) }
+            setNumberTouch(btnDot) { mListener?.onDotClick() }
+            setNumberTouch(btnOther) { mListener?.onOtherClick() }
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setNumberTouch(v: View, actionUp: () -> Unit) {
+        v.setOnTouchListener { v, event ->
+            when (event.actionMasked) {
+                MotionEvent.ACTION_DOWN -> {
+                    v.isPressed = true
+                }
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    v.isPressed = false
+                    actionUp.invoke()
+                }
+            }
+            true
         }
     }
 
