aa0be991f59539895f425989e30f5bda46775d7f
xiaoyang <xiaoyang@abc.com>
2025-09-19 10:42:45 +0700

    bugfix:积分榜在无数据情况下隐藏列表title - https://linear.app/forgames/issue/MD-3779

--- module_live/src/main/kotlin/com/walisport/module/live/ui/LiveStandingsFragment.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/ui/LiveStandingsFragment.kt
@@ -31,6 +31,7 @@ class LiveStandingsFragment : BaseFragment<LiveStandingsViewModel, FragmentLiveS
     override val vbClass: KClass<FragmentLiveStandingsBinding> = FragmentLiveStandingsBinding::class
     override val vmClass: KClass<LiveStandingsViewModel> = LiveStandingsViewModel::class
     private var standsAdapter = StandingsAdapter()
+    private var tournamentName = ""
 
     class StandingsItemDecoration(
         private val spacing: Int = 12.dp2px,         // 常规间距大小（像素）
@@ -69,7 +70,7 @@ class LiveStandingsFragment : BaseFragment<LiveStandingsViewModel, FragmentLiveS
             mainViewModel.apiStateListener.observe(viewLifecycleOwner) { state ->
                 when (state) {
                     DataState.NetworkUnavailable -> {
-                        mBinding.mainLayout.setState(
+                            mBinding.mainLayout.setState(
                             States.NETWORK_ANOMALY(),
                             arch.cayenne.lib.common.R.string.error_net.getString()
                         )
@@ -83,6 +84,7 @@ class LiveStandingsFragment : BaseFragment<LiveStandingsViewModel, FragmentLiveS
                         R.string.lineup_empty.getString()
                     )
                 } else {
+                    mBinding.tvStandingsName.text = tournamentName
                     mBinding.mainLayout.setVisibilityGone()
                     standsAdapter.submitList(it)
                 }
@@ -90,7 +92,7 @@ class LiveStandingsFragment : BaseFragment<LiveStandingsViewModel, FragmentLiveS
             mainViewModel.mainMatch.observe(viewLifecycleOwner) {
                 it?.let {
                     val leagueID = it.basicInfo.tournamentId
-                    mBinding.tvStandingsName.text = it.basicInfo.tournamentName
+                    tournamentName = it.basicInfo.tournamentName
                     mViewModel.getCompetitionData(leagueID)
                 }
             }
