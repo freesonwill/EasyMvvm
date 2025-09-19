1079045d8d9ba5af7f62728bdf0d4c9d5c672e0e
Link Hsieh <link.hsieh@skylinetw.com>
2025-09-19 11:34:38 +0800

    fix : 修復其他快速輸入點擊遮罩

--- lib_common/src/main/java/arch/cayenne/lib/common/ui/viewholder/ComboQuickAmountViewHolder.kt
+++ lib_common/src/main/java/arch/cayenne/lib/common/ui/viewholder/ComboQuickAmountViewHolder.kt
@@ -6,6 +6,6 @@ import arch.cayenne.lib.common.utils.ext.DimensionExt.dp2px
 class ComboQuickAmountViewHolder(mBinding: ItemQuickAmountBinding): DefaultQuickAmountViewHolder(mBinding) {
 
     override fun initView() {
-        mBinding.tvTitle.setPadding(0, 9.dp2px, 0, 9.dp2px)
+        mBinding.tvTitle.setPadding(0, 9.dp2px, 0, 8.dp2px)
     }
 }
