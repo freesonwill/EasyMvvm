dcd273d7d241c58c01908d98a206ee5ff2a0b931
aquan <aquan@aquan>
2025-09-24 21:34:21 +0700

    MD-3838 直播间-视频播放区域事件冲突,点击视频区域无法显示视频底部按钮

--- lib_qyplayer/src/main/kotlin/arch/cayenne/lib/qyplayer/ui/widget/LivePlayerView.kt
+++ lib_qyplayer/src/main/kotlin/arch/cayenne/lib/qyplayer/ui/widget/LivePlayerView.kt
@@ -32,7 +32,6 @@ class LivePlayerView @JvmOverloads constructor(
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
     private lateinit var mRenderView: QYRenderView
     private lateinit var mGestureView: GestureView
-    private lateinit var mTouchBackView: View
 
     private var mOnPlayStateBtnClickListener: (() -> Unit)? = null
     private var mOnUpdateStatisticsListener: ((category: String, json: String) -> Unit)? = null
@@ -191,10 +190,11 @@ class LivePlayerView @JvmOverloads constructor(
 
     private fun initGestureView() {
         mGestureView = findViewById(R.id.gesture_view)
-        mTouchBackView =  findViewById(R.id.touch_back_view)
-        mTouchBackView.touchBackPressed()
         mGestureView.apply {
             setOnGestureListener(object : GestureListener {
+                override fun onBack() {
+
+                }
                 override fun onHorizontalDistance(downX: Float, nowX: Float) {
                     if (!isGestureEnable()) {
                         return
