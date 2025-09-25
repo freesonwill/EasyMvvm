03ca943f82ff02d6d6414a7d5cab667b678aa38d
xiaoyang <xiaoyang@abc.com>
2025-09-25 16:01:25 +0700

    bugfix:提现记录页面UI以及业务逻辑 - https://linear.app/forgames/issue/MD-3821

--- module_topup/src/main/java/com/walisport/module/topup/ui/fragment/WithdrawRecordsFragment.kt
+++ module_topup/src/main/java/com/walisport/module/topup/ui/fragment/WithdrawRecordsFragment.kt
@@ -2,35 +2,106 @@ package com.walisport.module.topup.ui.fragment
 
 import android.os.Bundle
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import arch.cayenne.lib.base.data.constants.DataState
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
 import arch.cayenne.lib.common.utils.ext.ResourceExt.getString
 import com.walisport.module.topup.R
+import com.walisport.module.topup.data.constants.LoadingState
 import com.walisport.module.topup.databinding.FragmentWithdrawRecordsBinding
+import com.walisport.module.topup.ui.adapter.WithdrawAdapter
 import com.walisport.module.topup.ui.viewmodel.WithdrawRecordsViewModel
 import kotlin.reflect.KClass
 
 /**
- * 充值记录记录列表页
+ * 充值记录列表页
  */
 
-class WithdrawRecordsFragment :
-    BaseFragment<WithdrawRecordsViewModel, FragmentWithdrawRecordsBinding>() {
+class WithdrawRecordsFragment : BaseFragment<WithdrawRecordsViewModel, FragmentWithdrawRecordsBinding>() {
 
-    override val vbClass: KClass<FragmentWithdrawRecordsBinding> =
-        FragmentWithdrawRecordsBinding::class
+    override val vbClass: KClass<FragmentWithdrawRecordsBinding> = FragmentWithdrawRecordsBinding::class
     override val vmClass: KClass<WithdrawRecordsViewModel> = WithdrawRecordsViewModel::class
+    private val mAdapter by lazy { WithdrawAdapter() }
 
     override fun initView(savedInstanceState: Bundle?) {
         with(mBinding) {
             titleBar.loadGeneralTitleBar(R.string.withdrawal_record.getString(), {
                 findNavController().navigateUp()
             })
+            refreshLayout.setEnableLoadMore(true)
+            refreshLayout.setEnableScrollContentWhenLoaded(true)
+            refreshLayout.setOnRefreshListener {
+                mViewModel.reload()
+            }
+            refreshLayout.setOnLoadMoreListener {
+                mViewModel.loadNextPage()
+            }
         }
+        mBinding.withdrawList.apply {
+            itemAnimator = null
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = mAdapter
+        }
+    }
+
+    override fun initData() {
+        super.initData()
+        mViewModel.reload()
     }
 
     override fun initListener() {
     }
 
     override suspend fun createObserver() {
+        mViewModel.onDateFilter.observe(viewLifecycleOwner) {
+            mBinding.tvDateFilter.text = it.title
+        }
+        mViewModel.onWithdrawFilter.observe(viewLifecycleOwner) { list ->
+            val selectedNames = list.map { it.txName }
+            val displayText = when (selectedNames.size) {
+                1 -> selectedNames.first()
+                else -> selectedNames.joinToString("/")
+            }
+            mBinding.tvSportFilter.text = displayText
+        }
+        mViewModel.recordListChange.observe(viewLifecycleOwner) { list ->
+            mAdapter.submitList(list)
+        }
+        mViewModel.apiStateListener.observe(viewLifecycleOwner) { state ->
+            with(mBinding) {
+                when (state) {
+                    DataState.NetworkUnavailable -> {
+                        refreshLayout.finishRefresh()
+                        refreshLayout.finishLoadMore()
+                        refreshLayout.setEnableLoadMore(false)
+                    }
+
+                    DataState.NoMoreData -> {
+                        refreshLayout.finishLoadMore()
+                        refreshLayout.setEnableLoadMore(false)
+                    }
+
+                    LoadingState.DataEmpty -> {
+                        refreshLayout.finishRefresh()
+                    }
+
+                    LoadingState.Loading -> {
+                        refreshLayout.setEnableLoadMore(true)
+                    }
+
+                    LoadingState.Refreshing -> {
+                        refreshLayout.setEnableLoadMore(true)
+                    }
+
+                    LoadingState.LoadingNext -> {
+                    }
+
+                    DataState.LoadSuccess -> {
+                        if (refreshLayout.isRefreshing) refreshLayout.finishRefresh()
+                        refreshLayout.finishLoadMore()
+                    }
+                }
+            }
+        }
     }
 }
--- module_topup/src/main/java/com/walisport/module/topup/ui/viewmodel/WithdrawRecordsViewModel.kt
+++ module_topup/src/main/java/com/walisport/module/topup/ui/viewmodel/WithdrawRecordsViewModel.kt
@@ -1,11 +1,76 @@
 package com.walisport.module.topup.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import arch.cayenne.lib.base.data.model.UnPeekLiveData
+import arch.cayenne.lib.base.data.remote.ApiResponseState
+import arch.cayenne.lib.base.data.remote.ApiResponseState.Start.dataAs
 import arch.cayenne.lib.base.ui.viewmodel.BaseViewModel
 import com.walisport.module.topup.data.TopUpMainRepository
+import com.walisport.module.topup.data.constants.LoadingState
+import com.walisport.module.topup.data.entity.DateFilterBean
+import com.walisport.module.topup.data.entity.DateFilterEnum
+import com.walisport.module.topup.data.entity.RechargeRecordBean
+import com.walisport.module.topup.data.entity.WithdrawFilterBean
+import kotlinx.coroutines.launch
 
 class WithdrawRecordsViewModel(private val repo: TopUpMainRepository) : BaseViewModel() {
 
-    override fun initViewModel() {
-        super.initViewModel()
+    private val _recordListChange: UnPeekLiveData<List<RechargeRecordBean>?> = UnPeekLiveData()
+    val recordListChange: UnPeekLiveData<List<RechargeRecordBean>?> = _recordListChange
+
+    private var page = 1
+    var customTime: Long? = null
+        private set
+
+    private val _onDateFilter = MutableLiveData<DateFilterBean>()
+    val onDateFilter: LiveData<DateFilterBean> get() = _onDateFilter
+
+    private val _onWithdrawFilter = MutableLiveData<List<WithdrawFilterBean>>()
+    val onWithdrawFilter: LiveData<List<WithdrawFilterBean>> get() = _onWithdrawFilter
+
+    fun reload() {
+        page = 1
+        getListData()
+    }
+
+    init {
+        _onDateFilter.value = DateFilterBean(
+            title = DateFilterEnum.ALL.title,
+            date = DateFilterEnum.ALL
+        )
+        _onWithdrawFilter.value = listOf(WithdrawFilterBean.getAllTypeBean())
+    }
+
+    fun loadNextPage() {
+        if (apiStateListener.value != LoadingState.LoadSuccess) {
+            return
+        }
+        page++
+        setState(LoadingState.LoadingNext)
+        getListData()
+    }
+
+    private fun getListData() {
+        viewModelScope.launch {
+            callApi({
+                repo.getListData(page)
+            }, {
+                if (it is ApiResponseState.Failed) {
+                    _recordListChange.value = arrayListOf()
+                } else if (it is ApiResponseState.Succeeded<*>) {
+                    val list = it.dataAs<List<RechargeRecordBean>>()
+                    val size = list?.size ?: 0
+                    if (page == 1 && size == 0) {
+                        _recordListChange.value = arrayListOf()
+                        setState(LoadingState.DataEmpty)
+                    } else {
+                        setState(LoadingState.LoadSuccess)
+                        _recordListChange.value = list
+                    }
+                }
+            })
+        }
     }
 }
