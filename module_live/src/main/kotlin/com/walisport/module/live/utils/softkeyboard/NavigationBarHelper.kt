b43e32b38b3da6203dba8d403b8934a57853adb8
wenxi <russelllin465@gmail.com>
2025-09-26 10:25:19 +0700

    feature:重装后的软件盘计算高度不正确

--- module_live/src/main/kotlin/com/walisport/module/live/utils/softkeyboard/NavigationBarHelper.kt
+++ module_live/src/main/kotlin/com/walisport/module/live/utils/softkeyboard/NavigationBarHelper.kt
@@ -24,6 +24,7 @@ class NavigationBarHelper(
     private var isNavigationBarVisible: Boolean = false
     private var isKeyBoardVisible: Boolean = false
     private var dbKeyboardHeight: Int = 0
+
     //重启后进入时，首次检测的软件盘高度不对，弹出后又会更新正确软件盘高度
     private var secondCheck: Boolean = false
     private var lastKeyBoardHeight: Int = 0
@@ -79,9 +80,10 @@ class NavigationBarHelper(
         val keyboardInsets = insets.getInsets(WindowInsetsCompat.Type.ime())
         val height = keyboardInsets.bottom
 
-        if (secondCheck) { //保存高度和计算高度不一致时第二次检查
+        if (secondCheck && height > 150) { //保存高度和计算高度不一致时第二次检查
             secondCheck = false
-            val keyBoardHeight = if (isNavigationBarVisible) height - navigationBarHeight else height
+            val keyBoardHeight =
+                if (isNavigationBarVisible) height - navigationBarHeight else height
 //            "second keyBoardHeight:$keyBoardHeight lastHeight:$lastKeyBoardHeight dbHeight:$dbKeyboardHeight".logd(TAG)
             if (keyBoardHeight != lastKeyBoardHeight) { // 第二次计算高度和第一次计算高度不一致，以第二次为准
                 if (dbKeyboardHeight != keyBoardHeight) { //保存的高度和第二次高度不一致更新保存的高度
@@ -94,14 +96,15 @@ class NavigationBarHelper(
 
         if (height > 150 && !isKeyBoardVisible) { //软件盘打开
             isKeyBoardVisible = true
-            val keyBoardHeight = if (isNavigationBarVisible) height - navigationBarHeight else height
+            val keyBoardHeight =
+                if (isNavigationBarVisible) height - navigationBarHeight else height
 //            "frist keyBoardHeight:$keyBoardHeight dbHeight:$dbKeyboardHeight".logd(TAG)
             if (keyBoardHeight == dbKeyboardHeight) { //计算的软件盘高度和保存的软件盘高度一致
                 navigationListener?.onSoftKeyBoardShow(keyBoardHeight)
             } else { //计算的软件盘高度和保存的软件盘高度不一致
                 secondCheck = true
                 lastKeyBoardHeight = keyBoardHeight
-                navigationListener?.onSoftKeyBoardShow(if (dbKeyboardHeight == 0) keyBoardHeight else dbKeyboardHeight)
+                navigationListener?.onSoftKeyBoardShow(if (dbKeyboardHeight <= 0) keyBoardHeight else dbKeyboardHeight)
             }
 
         } else if (height < 150 && isKeyBoardVisible) { //软件盘关闭
