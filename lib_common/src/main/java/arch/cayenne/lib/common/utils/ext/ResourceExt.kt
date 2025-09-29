52ca1def0489152446deeb0dc0bb250e74c46957
zhangsan <zhangsan@youguess.com>
2025-09-29 15:45:17 +0700

    fix: use View's context in ResourceExt when in edit mode to fix layout rendering

--- lib_common/src/main/java/arch/cayenne/lib/common/utils/ext/ResourceExt.kt
+++ lib_common/src/main/java/arch/cayenne/lib/common/utils/ext/ResourceExt.kt
@@ -1,6 +1,7 @@
 package arch.cayenne.lib.common.utils.ext
 
 import android.app.Application
+import android.content.Context
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import androidx.annotation.ColorRes
@@ -8,6 +9,7 @@ import androidx.annotation.DimenRes
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
+import arch.cayenne.lib.base.utils.log.Utils
 import arch.cayenne.lib.skin.res.SkinnableResourceManager
 import org.koin.java.KoinJavaComponent.getKoin
 
@@ -17,68 +19,72 @@ import org.koin.java.KoinJavaComponent.getKoin
  * @description: 资源扩展
  */
 object ResourceExt {
-    private val application: Application by lazy { getKoin().get<Application>() }
-
-    /***
-     *  無Context狀態下取得String
-     */
-    fun @receiver:StringRes Int.getString(vararg formatArgs: Any): String {
-        return SkinnableResourceManager.getString(application.applicationContext,this,formatArgs = *formatArgs)
+    private val app: Application by lazy {
+        // 占位对象，避免编辑器崩溃
+        if(Utils.isInEditMode()) return@lazy Application()
+        getKoin().get<Application>()
     }
 
     /***
      *  無Context狀態下取得String
      */
-    fun @receiver:StringRes Int.getStringArray(): List<String> {
-        return application.resources.getStringArray(this).toList()
+    fun @receiver:StringRes Int.getString(vararg formatArgs: Any,ctx: Context = app): String {
+        return SkinnableResourceManager.getString(ctx,this,formatArgs = formatArgs)
+    }
+
+    /***
+     *  無Context狀態下取得String
+     */
+    fun @receiver:StringRes Int.getStringArray(ctx: Context = app): List<String> {
+        return ctx.resources.getStringArray(this).toList()
     }
 
 
     /***
      *  获取Color
      */
-    fun @receiver:ColorRes Int.getColor(): Int {
-        return ContextCompat.getColor(application, this)
+    fun @receiver:ColorRes Int.getColor(ctx: Context = app): Int {
+        return ContextCompat.getColor(ctx, this)
     }
 
     /**
      * 获取ColorDrawable
      * @return
      */
-    fun @receiver:ColorRes Int.getColorDrawable(): ColorDrawable {
-        return ColorDrawable(ContextCompat.getColor(application, this))
+    fun @receiver:ColorRes Int.getColorDrawable(ctx: Context = app): ColorDrawable {
+        return ColorDrawable(ContextCompat.getColor(ctx, this))
     }
 
     /**
      * 获取Drawable
      * @return
      */
-    fun @receiver:DrawableRes Int.getDrawable(): Drawable {
-        return ContextCompat.getDrawable(application, this)!!
+    fun @receiver:DrawableRes Int.getDrawable(ctx: Context = app): Drawable {
+        return ContextCompat.getDrawable(ctx, this)!!
     }
 
     /**
      * 获取getDimension
      * @return
      */
-    fun @receiver:DimenRes Int.getDimension(): Float {
-        return application.resources.getDimension(this)
+    fun @receiver:DimenRes Int.getDimension(ctx: Context = app): Float {
+        return ctx.resources.getDimension(this)
     }
 
     /**
      * getDimensionPixelSize
      * @return
      */
-    fun @receiver:DimenRes Int.getDimensionPixelSize(): Int {
-        return application.resources.getDimensionPixelSize(this)
+    fun @receiver:DimenRes Int.getDimensionPixelSize(ctx: Context = app): Int {
+        return ctx.resources.getDimensionPixelSize(this)
     }
 
     /**
      * getDimensionPixelOffset
      * @return
      */
-    fun @receiver:DimenRes Int.getDimensionPixelOffset(): Int {
-        return application.resources.getDimensionPixelOffset(this)
+    fun @receiver:DimenRes Int.getDimensionPixelOffset(ctx: Context = app): Int {
+        return ctx.resources.getDimensionPixelOffset(this)
     }
 
 }
