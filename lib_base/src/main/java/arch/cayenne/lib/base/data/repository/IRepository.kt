7879e5e0f437b7f2a7eb60b47b5c51b9d33f5a88
zhangsan <zhangsan@youguess.com>
2025-09-30 11:35:07 +0700

    feat: make IRepository extends KoinComponent to simplify injection

--- lib_base/src/main/java/arch/cayenne/lib/base/data/repository/IRepository.kt
+++ lib_base/src/main/java/arch/cayenne/lib/base/data/repository/IRepository.kt
@@ -1,12 +1,13 @@
 package arch.cayenne.lib.base.data.repository
 
 import kotlinx.coroutines.CoroutineScope
+import org.koin.core.component.KoinComponent
 
 /**
  * @author: zhangsan
  * @date: 2025/3/14 14:00
  * @description:
  */
-interface IRepository {
+interface IRepository: KoinComponent {
     val scope: CoroutineScope
 }
