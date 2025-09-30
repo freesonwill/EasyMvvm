3c0e9ba7f681a795781e535d8d992f335c4a3406
xiaoyang <xiaoyang@abc.com>
2025-09-30 17:26:04 +0700

    feature:主题皮肤只保留黑篮和白蓝两种其余删除 - https://linear.app/forgames/issue/MD-3870

--- module_setting/src/main/java/com/walisport/module/setting/ui/viewmodel/SettingViewModel.kt
+++ module_setting/src/main/java/com/walisport/module/setting/ui/viewmodel/SettingViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import arch.cayenne.lib.base.ui.viewmodel.BaseViewModel
 import arch.cayenne.lib.common.data.constants.LanguageType
-import arch.cayenne.lib.common.data.constants.SkinType
 import arch.cayenne.lib.skin.LanguageManager
 import arch.cayenne.lib.skin.SkinnableManager
 import arch.cayenne.lib.common.data.constants.OddsDisplayEnum
@@ -58,10 +57,6 @@ class SettingViewModel : BaseViewModel() {
         return repository.getSkinType()
     }
 
-    fun getOddsType(): OddsDisplayEnum {
-        return repository.getOddsType()
-    }
-
     fun getLanguageType(): LanguageType {
         return repository.getLanguageType()
     }
@@ -69,16 +64,14 @@ class SettingViewModel : BaseViewModel() {
     //设置皮肤背景，只换肤不写入记录，写入记录得调用setSkinRecord
     fun setSkinType(type: String) {
         viewModelScope.launch {
-            val logicSkin = SkinType.getLogicSkinType(type)
-            skinManager.loadSkin(logicSkin)
+            skinManager.loadSkin(type)
             _skinType.value = type
         }
     }
 
     fun resetSkinType(type: String) {
         viewModelScope.launch {
-            val logicSkin = SkinType.getLogicSkinType(type)
-            skinManager.loadSkin(logicSkin)
+            skinManager.loadSkin(type)
         }
     }
 
