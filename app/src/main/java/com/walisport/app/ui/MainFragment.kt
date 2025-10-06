45ce94cf4b04fc0e6ec25dd0e935cdf52921b6e7
wenxi <russelllin465@gmail.com>
2025-10-06 17:04:51 +0700

    feature：首页注单加载statusBar

--- app/src/main/java/com/walisport/app/ui/MainFragment.kt
+++ app/src/main/java/com/walisport/app/ui/MainFragment.kt
@@ -2,18 +2,15 @@ package com.walisport.app.ui
 
 import android.os.Bundle
 import android.util.SparseArray
+import androidx.core.os.bundleOf
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import arch.cayenne.lib.base.data.constants.StatusBarMode
 import arch.cayenne.lib.base.data.model.StatusBarConfig
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
 import arch.cayenne.lib.base.ui.viewmodel.EmptyViewModel
 import arch.cayenne.lib.common.ui.fragment.EmptyFragment
-import arch.cayenne.lib.database.entity.LiveMatchBasicInfoBean
-import arch.cayenne.lib.database.entity.LiveMatchBean
 import arch.cayenne.module.betslip.ui.fragment.HomeBetSlipFragment
 import arch.cayenne.module.chat.ui.fragment.ChatHomeFragment
 import arch.cayenne.module.home.ui.fragment.NewHomeFragment
@@ -131,9 +128,17 @@ class MainFragment : BaseFragment<EmptyViewModel, FragmentMainBinding>() {
         if (f == null) {
             f = when (position) {
                 1 -> NewHomeFragment()
-                2 -> HomeBetSlipFragment()
+                2 -> HomeBetSlipFragment().apply{
+                    val bundle = Bundle().apply {
+                        putBoolean("statusBar",true)
+                    }
+                    arguments = bundle
+                }
                 3 -> ChatHomeFragment().apply {
-                    setMainChatStatus()
+                    val bundle = Bundle().apply {
+                        putBoolean("chat",true)
+                    }
+                    arguments = bundle
                 }
                 4 -> MeFragment()
                 else -> EmptyFragment()
