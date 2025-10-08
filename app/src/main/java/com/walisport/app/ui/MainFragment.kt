12fd82b0a756f51248f05481a5a5a4e92b857bf3
wenxi <russelllin465@gmail.com>
2025-10-08 15:12:29 +0700

    feature: 首页聊天页面调整

--- app/src/main/java/com/walisport/app/ui/MainFragment.kt
+++ app/src/main/java/com/walisport/app/ui/MainFragment.kt
@@ -11,9 +11,6 @@ import arch.cayenne.lib.base.data.model.StatusBarConfig
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
 import arch.cayenne.lib.base.ui.viewmodel.EmptyViewModel
 import arch.cayenne.lib.common.ui.fragment.EmptyFragment
-import arch.cayenne.module.betslip.ui.fragment.HomeBetSlipFragment
-import arch.cayenne.module.chat.ui.fragment.ChatHomeFragment
-import arch.cayenne.module.chat.ui.fragment.MainChatFragment
 import arch.cayenne.module.chat.ui.fragment.MainChatFragment
 import arch.cayenne.module.home.ui.fragment.NewHomeFragment
 import arch.cayenne.module.order.ui.fragment.HomeOrderFragment
