b3354308bf28dd183977e5cb918d3dfe35064c7e
KC.Tsai <KC.Tsai@skylinetw.com>
2025-10-13 10:14:15 +0800

    feat: 大廳頂部UI

--- module_hall/src/main/java/com/walisport/module/hall/ui/fragment/HallFragment.kt
+++ module_hall/src/main/java/com/walisport/module/hall/ui/fragment/HallFragment.kt
@@ -1,9 +1,11 @@
 package com.walisport.module.hall.ui.fragment
 
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 import arch.cayenne.lib.base.data.constants.StatusBarMode
 import arch.cayenne.lib.base.data.model.StatusBarConfig
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
+import arch.cayenne.lib.common.utils.ViewUtils
 import arch.cayenne.lib.common.utils.ext.touchBackPressed
 import com.walisport.module.hall.databinding.FragmentHallBinding
 import com.walisport.module.hall.ui.viewmodel.HallViewModel
@@ -23,6 +25,12 @@ class HallFragment : BaseFragment<HallViewModel, FragmentHallBinding>() {
         mBinding.root.touchBackPressed()
 
         mBinding.balanceView.init(childFragmentManager)
+
+        var barHeight = ViewUtils.getStatusBarHeight(requireContext())
+        val params = mBinding.guideline.layoutParams as ConstraintLayout.LayoutParams
+        params.guideBegin = barHeight
+        mBinding.guideline.layoutParams = params
+
     }
 
     override fun initListener() {
@@ -34,10 +42,18 @@ class HallFragment : BaseFragment<HallViewModel, FragmentHallBinding>() {
     }
 
     override fun onStart() {
-        super.onStart()
+//        mBinding.ivLogo.post {
+//            var barHeight = ViewUtils.getStatusBarHeight(requireContext())
+//            var toBarHeight = mBinding.ivLogo.height
+//
+//            val paramsLin = mBinding.homeBarIcon.layoutParams as LayoutParams
+//            paramsLin.height = barHeight+toBarHeight
+//            mBinding.homeBarIcon.layoutParams = paramsLin
+//        }
         mBinding.root.fitsSystemWindows = false
-        StatusBarConfig.statusBarType = StatusBarMode.DRAW_BEHIND()
-        setStatusBar(StatusBarConfig, mBinding.root)
+        StatusBarConfig.statusBarType = StatusBarMode.DRAW_BEHIND(autoPadding = false, autoIsNavigation = true)
+        setStatusBar(StatusBarConfig, mBinding.clMain)
+        super.onStart()
     }
 
 
