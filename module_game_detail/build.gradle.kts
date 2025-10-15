72106a3d82b143619c4289cfc2ddc3c3ce4e3851
Jeremy <jeremy.huang@skylinetw.com>
2025-10-15 11:00:38 +0800

    feat: implement game detail basic data model

--- module_game_detail/build.gradle.kts
+++ module_game_detail/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-parcelize")
 }
 
 apply(from = rootProject.file("gradle/flavor.gradle"))
@@ -41,6 +42,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- module_game_detail/src/main/java/com/walisport/module/gamedetail/ui/fragment/GameDetailFragment.kt
+++ module_game_detail/src/main/java/com/walisport/module/gamedetail/ui/fragment/GameDetailFragment.kt
@@ -1,11 +1,18 @@
 package com.walisport.module.gamedetail.ui.fragment
 
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
 import arch.cayenne.lib.base.data.constants.StatusBarMode
 import arch.cayenne.lib.base.data.model.StatusBarConfig
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
+import com.bumptech.glide.Glide
+import com.walisport.module.gamedetail.R
+import com.walisport.module.gamedetail.data.model.GameDetailBean
 import com.walisport.module.gamedetail.databinding.FragmentGameDetailBinding
 import com.walisport.module.gamedetail.ui.viewmodel.GameDetailViewModel
+import java.util.Locale
 import kotlin.reflect.KClass
 
 class GameDetailFragment: BaseFragment<GameDetailViewModel, FragmentGameDetailBinding>() {
@@ -15,6 +22,39 @@ class GameDetailFragment: BaseFragment<GameDetailViewModel, FragmentGameDetailBi
     override fun initView(savedInstanceState: Bundle?) {
         with(mBinding) {
             viewBalance.init(childFragmentManager)
+
+            val mockData = GameDetailBean(
+                id = 1,
+                name = "Slot Game 1",
+                type = 1,
+                supplier = "Supplier A",
+                avatar = "https://example.com/game1.png",
+                reward = 0.95,
+                maxOdds = 1000,
+                online = 1234,
+                score = 4.5,
+                comments = 150,
+                tryIt = true,
+                hasMore = true,
+                collect = false,
+                materials = true,
+                currency = emptyList()
+            )
+
+            Glide.with(root.context).load(mockData.avatar).placeholder(R.mipmap.img_game_cover).into(ivGameCover)
+            tvGameName.text = mockData.name
+            // todo type enum待確認
+            tvGameType.text = mockData.type.toString()
+            tvGameVendor.text = mockData.supplier
+            tvRtpValue.text = String.format(Locale.getDefault(), "%.1f%%", mockData.reward * 100)
+            tvMaxRewardValue.text = String.format(Locale.getDefault(), "%dx", mockData.maxOdds)
+            tvOnlineValue.text = mockData.online.toString()
+            tvScore.text = mockData.score.toString()
+            tvScoreCountValue.text = mockData.comments.toString()
+            // todo 效果待確認
+            ivFavorite.isSelected = mockData.collect
+            ivStartTrial.visibility = if (mockData.tryIt) View.VISIBLE else View.GONE
+            groupMoreGame.visibility = if (mockData.hasMore) View.VISIBLE else View.GONE
         }
     }
 
