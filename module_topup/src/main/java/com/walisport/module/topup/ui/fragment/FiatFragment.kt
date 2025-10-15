fa1f909cbb72baf4c137568bf4556118621b74cf
xiaoyang <xiaoyang@abc.com>
2025-10-15 18:08:34 +0700

    feature:充值-法币页面UI - https://linear.app/forgames/issue/MD-4049

--- module_topup/src/main/java/com/walisport/module/topup/ui/fragment/FiatFragment.kt
+++ module_topup/src/main/java/com/walisport/module/topup/ui/fragment/FiatFragment.kt
@@ -2,25 +2,56 @@ package com.walisport.module.topup.ui.fragment
 
 import android.os.Bundle
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
-import arch.cayenne.lib.base.ui.viewmodel.EmptyViewModel
 import com.walisport.module.topup.databinding.FragmentFiatBinding
+import com.walisport.module.topup.ui.adapter.PayMethodAdapter
+import com.walisport.module.topup.ui.adapter.PayMoneyAdapter
+import com.walisport.module.topup.ui.viewmodel.FiatViewModel
 import kotlin.reflect.KClass
 
 /**
  * 充值-法币页面
  */
 
-class FiatFragment : BaseFragment<EmptyViewModel, FragmentFiatBinding>() {
+class FiatFragment : BaseFragment<FiatViewModel, FragmentFiatBinding>() {
 
     override val vbClass: KClass<FragmentFiatBinding> = FragmentFiatBinding::class
-    override val vmClass: KClass<EmptyViewModel> = EmptyViewModel::class
+    override val vmClass: KClass<FiatViewModel> = FiatViewModel::class
+
+    private val payTypeAdapter: PayMethodAdapter by lazy {
+        PayMethodAdapter(object : PayMethodAdapter.PaTypeListener {
+            override fun onSelectPayType(id: Int) {
+                mViewModel.selectPayType(id)
+            }
+        })
+    }
+
+    private val payMoneyAdapter: PayMoneyAdapter by lazy {
+        PayMoneyAdapter(object : PayMoneyAdapter.PayMoneyListener {
+            override fun onSelectPayMoney(id: Int) {
+                mViewModel.selectPayMoney(id)
+            }
+        })
+    }
 
     override fun initView(savedInstanceState: Bundle?) {
+        mBinding.rvPayType.adapter = payTypeAdapter
+        mBinding.rvRecharge.adapter = payMoneyAdapter
+    }
+
+    override fun initData() {
+        super.initData()
+        mViewModel.getPayTypeList()
     }
 
     override fun initListener() {
     }
 
     override suspend fun createObserver() {
+        mViewModel.onPayMethodListener.observe(viewLifecycleOwner) {
+            payTypeAdapter.submitList(it)
+        }
+        mViewModel.onPayMoneyListener.observe(viewLifecycleOwner) {
+            payMoneyAdapter.submitList(it)
+        }
     }
 }
