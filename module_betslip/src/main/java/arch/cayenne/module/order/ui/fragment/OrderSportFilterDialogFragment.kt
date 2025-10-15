e3b3c75655ae955c043122e18d81d76508849ede
Link Hsieh <link.hsieh@skylinetw.com>
2025-10-15 14:43:52 +0800

    feat : 新增體育篩選器彈窗選擇交互

--- module_betslip/src/main/java/arch/cayenne/module/order/ui/fragment/OrderSportFilterDialogFragment.kt
+++ module_betslip/src/main/java/arch/cayenne/module/order/ui/fragment/OrderSportFilterDialogFragment.kt
@@ -3,21 +3,31 @@ package arch.cayenne.module.order.ui.fragment
 import android.os.Bundle
 import androidx.recyclerview.widget.SimpleItemAnimator
 import arch.cayenne.lib.base.ui.fragment.BaseBottomSheetFragment
+import arch.cayenne.module.betslip.R
 import arch.cayenne.module.betslip.data.constants.Config
 import arch.cayenne.module.betslip.databinding.FragmentOrderSportFilterDialogBinding
 import arch.cayenne.module.betslip.ui.adapter.SportPickerAdapter
 import arch.cayenne.module.betslip.ui.viewmodel.SportPickerViewModel
 import kotlin.reflect.KClass
 
-class OrderSportFilterDialogFragment private constructor(): BaseBottomSheetFragment<SportPickerViewModel, FragmentOrderSportFilterDialogBinding>() {
+class OrderSportFilterDialogFragment private constructor() :
+    BaseBottomSheetFragment<SportPickerViewModel, FragmentOrderSportFilterDialogBinding>() {
 
     companion object {
-        fun newInstance(): OrderSportFilterDialogFragment {
-            return OrderSportFilterDialogFragment()
+        const val KEY_SELECTED_SPORT_ID = "key_selected_sport_id"
+        fun newInstance(sportIds: List<Int> = emptyList()): OrderSportFilterDialogFragment {
+            return OrderSportFilterDialogFragment().apply {
+                if (sportIds.isNotEmpty()) {
+                    val bundle = Bundle()
+                    bundle.putIntArray(KEY_SELECTED_SPORT_ID, sportIds.toIntArray())
+                    arguments = bundle
+                }
+            }
         }
     }
 
-    override val vbClass: KClass<FragmentOrderSportFilterDialogBinding> = FragmentOrderSportFilterDialogBinding::class
+    override val vbClass: KClass<FragmentOrderSportFilterDialogBinding> =
+        FragmentOrderSportFilterDialogBinding::class
     override val vmClass: KClass<SportPickerViewModel> = SportPickerViewModel::class
 
     private val sportAdapter: SportPickerAdapter by lazy {
@@ -29,8 +39,17 @@ class OrderSportFilterDialogFragment private constructor(): BaseBottomSheetFragm
     }
 
     override fun initView(savedInstanceState: Bundle?) {
+        isHorizontalGestureEnable = false
         mBinding.rvSport.adapter = sportAdapter
         (mBinding.rvSport.itemAnimator as? SimpleItemAnimator)?.supportsChangeAnimations = false
+    }
+
+    override fun initData() {
+        super.initData()
+        val selectedIds = arguments?.getIntArray(KEY_SELECTED_SPORT_ID)?.toList()
+        if (!selectedIds.isNullOrEmpty()) {
+            mViewModel.setSelectedById(selectedIds.toIntArray())
+        }
     }
 
     override fun initListener() {
@@ -38,7 +57,7 @@ class OrderSportFilterDialogFragment private constructor(): BaseBottomSheetFragm
             mViewModel.reset()
             sendResult()
         }
-        mBinding.btnReset.setOnClickListener {
+        mBinding.btnConfirm.setOnClickListener {
             sendResult()
         }
     }
@@ -46,7 +65,12 @@ class OrderSportFilterDialogFragment private constructor(): BaseBottomSheetFragm
     override suspend fun createObserver() {
         super.createObserver()
         mViewModel.onSportListener.observe(viewLifecycleOwner) {
-            sportAdapter.submitList(it)
+            val lastDataSize = sportAdapter.currentList.size
+            sportAdapter.submitList(it) {
+                if (lastDataSize != 0) {
+                    mBinding.btnConfirm.text = getString(R.string.btn_sport_filter_confirm)
+                }
+            }
         }
     }
 
