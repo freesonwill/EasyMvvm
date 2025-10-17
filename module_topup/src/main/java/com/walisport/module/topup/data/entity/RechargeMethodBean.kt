cf63998924cf03cd961acb7a11a8fab751189c42
xiaoyang <xiaoyang@abc.com>
2025-10-17 17:39:01 +0700

    feature:提现-法币页面UI - https://linear.app/forgames/issue/MD-4051

---
+++ module_topup/src/main/java/com/walisport/module/topup/data/entity/RechargeMethodBean.kt
@@ -0,0 +1,8 @@
+package com.walisport.module.topup.data.entity
+
+data class RechargeMethodBean(
+    val id: Int,
+    val payType: String,
+    val isSelect: Boolean,
+    val isRecommend:Boolean,
+)
