9e84c58e3571303eaab9803bd1826056af0150da
KC.Tsai <KC.Tsai@skylinetw.com>
2025-10-23 00:20:10 +0800

    feat: 共用的tablayout group元件 - https://linear.app/forgames/issue/MD-4111

--- lib_common/src/main/java/arch/cayenne/lib/common/ui/view/CustomGameTabGroupLayout.kt
+++ lib_common/src/main/java/arch/cayenne/lib/common/ui/view/CustomGameTabGroupLayout.kt
@@ -3,8 +3,14 @@ package arch.cayenne.lib.common.ui.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
+import android.widget.LinearLayout
+import arch.cayenne.lib.common.R
+import arch.cayenne.lib.common.databinding.ItemCustomGameTabBinding
 import arch.cayenne.lib.common.databinding.ViewCustomGameTabGroupBinding
+import arch.cayenne.lib.common.utils.ext.DimensionExt.dp2px
+import com.bumptech.glide.Glide
 
 class CustomGameTabGroupLayout : FrameLayout {
     private lateinit var binding: ViewCustomGameTabGroupBinding
@@ -24,4 +30,57 @@ class CustomGameTabGroupLayout : FrameLayout {
         binding = ViewCustomGameTabGroupBinding.inflate(LayoutInflater.from(context), this, true)
     }
 
+    fun submitTabList(list : List<SimpleTabDataModel>) {
+        with(binding) {
+            list.forEachIndexed { i, data ->
+                val tab = tlVendorList.newTab()
+
+                tab.customView = createTabView(data)
+                tab.view.setPadding(0, 0, 6f.dp2px, 0)
+                if (data.id == 0) {
+                    tab.view.minimumWidth = 0
+                }
+                tlVendorList.addTab(tab)
+            }
+        }
+    }
+
+    private fun createTabView(
+        tabDataModel: SimpleTabDataModel
+    ): View {
+        val context = rootView.context
+        val tabBinding =
+            ItemCustomGameTabBinding.inflate(LayoutInflater.from(context), null, false)
+
+        tabBinding.apply {
+            tvName.text = if (tabDataModel.id == 0) {
+                resources.getString(R.string.custom_tab_all)
+            }else {
+                tabDataModel.simpleName
+            }
+
+            if (tabDataModel.id == 0) {
+                ivIcon.visibility = View.GONE
+                // 設置「全部」tab 的寬度
+                root.layoutParams = LinearLayout.LayoutParams(51.dp2px, 32.dp2px)
+            } else {
+                Glide.with(context)
+                    .load(tabDataModel.icon.ifEmpty { R.drawable.ic_wali_demo })
+                    .placeholder(R.drawable.ic_wali_demo)
+                    .error(R.drawable.ic_wali_demo)
+                    .into(ivIcon)
+            }
+            root.setBackgroundResource(R.drawable.selector_custom_game_tab_bg)
+        }
+        return tabBinding.root
+    }
+
+
 }
+
+//TODO 先給一個簡單的
+data class SimpleTabDataModel(
+    val id: Int,
+    val simpleName: String,
+    val icon: String,
+)
