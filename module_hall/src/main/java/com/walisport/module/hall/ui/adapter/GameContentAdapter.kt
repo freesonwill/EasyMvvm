0d930e953e7b5a29694d98b73b693e10be2dc386
Jeremy <jeremy.huang@skylinetw.com>
2025-10-22 17:46:07 +0800

    feat: connect game hall and game detail

--- module_hall/src/main/java/com/walisport/module/hall/ui/adapter/GameContentAdapter.kt
+++ module_hall/src/main/java/com/walisport/module/hall/ui/adapter/GameContentAdapter.kt
@@ -7,6 +7,9 @@ import androidx.recyclerview.widget.DiffUtil
 import arch.cayenne.lib.base.ui.adapter.BaseAdapter
 import arch.cayenne.lib.base.ui.adapter.BaseViewHolder
 import arch.cayenne.lib.common.utils.ext.DimensionExt.dp2px
+import arch.cayenne.lib.common.utils.ext.clickNoRepeat
+import arch.cayenne.lib.common.utils.ext.requireActivity
+import com.walisport.module.gamedetail.ui.fragment.GameDetailFragment
 import com.walisport.module.hall.data.GameContentData
 import com.walisport.module.hall.databinding.ItemGameContentBinding
 
@@ -43,6 +46,20 @@ class GameContentViewHolder(val item: ItemGameContentBinding): BaseViewHolder(it
         item.llCount.apply {
             val params = this.layoutParams as ConstraintLayout.LayoutParams
             params.topMargin = 6.dp2px
+        }
+
+        // TODO 暫時串接遊戲詳情
+        item.root.clickNoRepeat {
+            item.root.requireActivity().supportFragmentManager.beginTransaction()
+                .setCustomAnimations(
+                    arch.cayenne.lib.common.R.anim.slide_in_right,
+                    arch.cayenne.lib.common.R.anim.no_anim,
+                    arch.cayenne.lib.common.R.anim.no_anim,
+                    arch.cayenne.lib.common.R.anim.slide_out_right
+                )
+                .add(arch.cayenne.lib.base.R.id.nav_host, GameDetailFragment())
+                .addToBackStack(null)
+                .commit()
         }
     }
 }
