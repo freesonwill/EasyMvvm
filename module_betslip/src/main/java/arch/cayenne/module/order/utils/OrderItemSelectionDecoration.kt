1c39be06fb859af6b192c4a8107a7eea07812cd7
Link Hsieh <link.hsieh@skylinetw.com>
2025-10-29 16:00:29 +0800

    feat : 新增收起/展開動畫

---
+++ module_betslip/src/main/java/arch/cayenne/module/order/utils/OrderItemSelectionDecoration.kt
@@ -0,0 +1,21 @@
+package arch.cayenne.module.order.utils
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class OrderItemSelectionDecoration(private val space: Int): RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        val position = parent.getChildAdapterPosition(view) // 獲取當前項目的位置
+
+        if (position != 0) {
+            outRect.top = space
+        }
+    }
+}
