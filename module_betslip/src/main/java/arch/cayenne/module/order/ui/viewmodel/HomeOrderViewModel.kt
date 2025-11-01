015ed10cc1cb2926503890b202ec9d03602961d9
zhangsan <zhangsan@youguess.com>
2025-11-01 14:31:29 +0700

    Revert "Revert "Merge branch 'feature/MD-4141-dev' into 'dev'""
    This reverts commit fd6df3be

--- module_betslip/src/main/java/arch/cayenne/module/order/ui/viewmodel/HomeOrderViewModel.kt
+++ module_betslip/src/main/java/arch/cayenne/module/order/ui/viewmodel/HomeOrderViewModel.kt
@@ -1,6 +1,14 @@
 package arch.cayenne.module.order.ui.viewmodel
 
 import arch.cayenne.lib.base.ui.viewmodel.BaseViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class HomeOrderViewModel : BaseViewModel() {
+    val betModelFlow = MutableStateFlow<BetMode?>(null)
+
+}
+
+enum class BetMode {
+    BET_RECORD, //投注记录，可查看游戏和体育的注单
+    BET_SLIP, //注单,只可查看体育的注单
 }
