b95e1e23be5fc87ed8b3fd52162733526c583d3a
Link <>
2025-12-09 11:47:54 +0800

    feat : 新增自定義點擊效果

--- module_betslip/src/main/java/arch/cayenne/module/order/ui/fragment/OrderDateCustomFragment.kt
+++ module_betslip/src/main/java/arch/cayenne/module/order/ui/fragment/OrderDateCustomFragment.kt
@@ -1,6 +1,7 @@
 package arch.cayenne.module.order.ui.fragment
 
 import android.os.Bundle
+import android.view.View
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
 import arch.cayenne.module.betslip.databinding.FragmentOrderDateCustomBinding
 import arch.cayenne.module.order.ui.viewmodel.OrderDateCustomViewModel
@@ -16,14 +17,42 @@ class OrderDateCustomFragment: BaseFragment<OrderDateCustomViewModel, FragmentOr
     }
 
     override fun initListener() {
-
+        mBinding.tvYesterday.setOnClickListener {
+            setSelected(it)
+        }
+        mBinding.tvLastWeek.setOnClickListener {
+            setSelected(it)
+        }
+        mBinding.tvLastMonth.setOnClickListener {
+            setSelected(it)
+        }
+        mBinding.tvStartTime.setOnClickListener {
+            setSelected(it)
+        }
+        mBinding.tvEndTime.setOnClickListener {
+            setSelected(it)
+        }
     }
 
     override suspend fun createObserver() {
-
+        mViewModel.onStartTimeListener.observe(viewLifecycleOwner) { date ->
+            mBinding.tvStartTime.text = date
+        }
+        
+        mViewModel.onEndTimeListener.observe(viewLifecycleOwner) { date ->
+            mBinding.tvEndTime.text = date
+        }
     }
 
     override fun getResult(): LongArray {
         return longArrayOf(0, 0)
+    }
+
+    private fun setSelected(v: View) {
+        mBinding.tvYesterday.isSelected = v == mBinding.tvYesterday
+        mBinding.tvLastWeek.isSelected = v == mBinding.tvLastWeek
+        mBinding.tvLastMonth.isSelected = v == mBinding.tvLastMonth
+        mBinding.tvStartTime.isSelected = v == mBinding.tvStartTime
+        mBinding.tvEndTime.isSelected = v == mBinding.tvEndTime
     }
 }
--- module_betslip/src/main/java/arch/cayenne/module/order/ui/viewmodel/OrderDateCustomViewModel.kt
+++ module_betslip/src/main/java/arch/cayenne/module/order/ui/viewmodel/OrderDateCustomViewModel.kt
@@ -1,5 +1,32 @@
 package arch.cayenne.module.order.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import arch.cayenne.lib.base.ui.viewmodel.BaseViewModel
+import java.util.Calendar
 
-class OrderDateCustomViewModel: BaseViewModel()
+class OrderDateCustomViewModel: BaseViewModel() {
+
+    private val _onStartTimeListener = MutableLiveData<String>()
+    val onStartTimeListener: LiveData<String> get() = _onStartTimeListener
+
+    private val _onEndTimeListener = MutableLiveData<String>()
+    val onEndTimeListener: LiveData<String> get() = _onEndTimeListener
+
+    init {
+        initDefaultTime()
+    }
+
+    private fun initDefaultTime() {
+        val c = Calendar.getInstance()
+        c.timeInMillis = System.currentTimeMillis()
+        
+        val year = c.get(Calendar.YEAR)
+        val month = String.format("%02d", c.get(Calendar.MONTH) + 1)
+        val day = String.format("%02d", c.get(Calendar.DAY_OF_MONTH))
+        
+        val currentDate = "$year-$month-$day"
+        _onStartTimeListener.value = currentDate
+        _onEndTimeListener.value = currentDate
+    }
+}
