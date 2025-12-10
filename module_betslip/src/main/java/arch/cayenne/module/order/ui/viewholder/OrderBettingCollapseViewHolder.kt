d329a4462fd5c3785801454356b93650fc7c7bd7
Link <>
2025-12-10 11:00:49 +0800

    feat : 新增注單收起後UI

---
+++ module_betslip/src/main/java/arch/cayenne/module/order/ui/viewholder/OrderBettingCollapseViewHolder.kt
@@ -0,0 +1,23 @@
+package arch.cayenne.module.order.ui.viewholder
+
+import arch.cayenne.lib.base.ui.adapter.BaseViewHolder
+import arch.cayenne.lib.common.data.constants.CurrencySymbols
+import arch.cayenne.lib.common.utils.ext.SportIntExt.getFormalMoney
+import arch.cayenne.lib.database.entity.BetSlipOrderBean
+import arch.cayenne.module.betslip.R
+import arch.cayenne.module.betslip.databinding.ItemOrderSportBettingCollapseBinding
+import arch.cayenne.module.order.data.constants.OrderSportPageEnum
+
+class OrderBettingCollapseViewHolder(private val mBinding: ItemOrderSportBettingCollapseBinding): BaseViewHolder(mBinding) {
+
+    fun init(item: BetSlipOrderBean, type: OrderSportPageEnum) {
+
+        val betAmount = "${CurrencySymbols.getSymbol(item.currency)}${item.betAmount.getFormalMoney()}"
+        mBinding.tvBetMoney.text = betAmount
+        mBinding.tvCombo.text = if (item.selectionsList.size == 1) {
+            mBinding.root.context.getString(arch.cayenne.lib.res.R.string.title_single_bet)
+        } else {
+            mBinding.root.context.getString(R.string.title_combo_bet_odds, item.comboK, item.comboV)
+        }
+    }
+}
