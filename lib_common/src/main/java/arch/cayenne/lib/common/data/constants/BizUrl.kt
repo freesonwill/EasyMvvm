e55026dcd5df83fb9059966c57daed8adcb0a30f
wangzai <dafeix64@gmail.com>
2025-12-13 00:47:51 +0700

    合伙人页面

--- lib_common/src/main/java/arch/cayenne/lib/common/data/constants/BizUrl.kt
+++ lib_common/src/main/java/arch/cayenne/lib/common/data/constants/BizUrl.kt
@@ -5,11 +5,13 @@ package arch.cayenne.lib.common.data.constants
  * @date: 2025/12/10 16:44
  * @description:
  */
-enum class BizUrl(val url:String) {
-    FUND_DETAIL("https://dev.ra781.com/web-3n1/record"),
-    FEEDBACK("https://dev.ra781.com/web-3n1/feedback"),
+enum class BizUrl(val url: String) {
+    FUND_DETAIL("https://dev.ra781.com/web-3n1/record") ,
+    FEEDBACK("https://dev.ra781.com/web-3n1/feedback") ,
+
     //    GAME_BET_SHARE("http://192.168.10.37:5173/orderDetail?settleId=s3&name=南京红姐&level=90"),
-    GAME_BET_SHARE("https://dev.ra781.com/web-3n1/orderDetail?settleId=s3&name=南京红姐&level=90"),
-    REBATE("https://dev.ra781.com/web-3n1/rebate"),
-    INVITE("https://dev.ra781.com/web-3n1/invite")
+    GAME_BET_SHARE("https://dev.ra781.com/web-3n1/orderDetail?settleId=s3&name=南京红姐&level=90") ,
+    REBATE("https://dev.ra781.com/web-3n1/rebate") ,
+    INVITE("https://dev.ra781.com/web-3n1/invite") ,
+    PARTNER("https://dev.ra781.com/web-3n1/partner")
 }
