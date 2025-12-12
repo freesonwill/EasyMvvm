36d3f05bba6e5d30dc3e4d085d6eaaa5e1d580de
wangzai <dafeix64@gmail.com>
2025-12-12 17:10:54 +0700

    feature. https://gitlab.iptco.me/tio/wip/-/issues/24 【Android】 游戏大厅- 游戏列表页面-列表排序

--- module_hall/src/main/java/com/walisport/module/hall/ui/fragment/GameContentFragment.kt
+++ module_hall/src/main/java/com/walisport/module/hall/ui/fragment/GameContentFragment.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import arch.cayenne.lib.base.data.constants.DataState
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
 import arch.cayenne.lib.common.ui.adapter.GridSpacingItemDecoration
+import arch.cayenne.lib.common.ui.view.DynamicStateLayout.States
 import arch.cayenne.lib.common.ui.view.SimpleTabDataModel
 import arch.cayenne.lib.common.utils.ext.DeeplinkExt.deeplink
 import arch.cayenne.lib.common.utils.ext.DimensionExt.dp2px
@@ -112,6 +113,41 @@ class GameContentFragment : BaseFragment<GameContentViewModel , FragmentGameCont
     }
 
     override suspend fun createObserver() {
+        mViewModel.apiStateListener.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                DataState.LoadSuccess -> {
+                    mBinding.rvGame.visibility = View.VISIBLE
+                    mBinding.clDynamics.visibility = View.GONE
+                }
+
+                DataState.DataEmpty -> {
+                    mBinding.rvGame.visibility = View.GONE
+                    mBinding.clDynamics.visibility = View.VISIBLE
+                    mBinding.clDynamics.setState(
+                        States.DATA_EMPTY ,
+                        arch.cayenne.lib.common.R.string.data_empty.getString()
+                    )
+
+                }
+
+                DataState.NoMoreData -> {
+                    mBinding.rvGame.visibility = View.VISIBLE
+                    mBinding.clDynamics.visibility = View.GONE
+                }
+
+                DataState.NetworkUnavailable -> {
+                    mBinding.rvGame.visibility = View.GONE
+                    mBinding.clDynamics.visibility = View.VISIBLE
+                    mBinding.clDynamics.setState(
+                        States.NETWORK_ANOMALY() ,
+                        arch.cayenne.lib.common.R.string.error_net.getString()
+                    )
+                }
+
+                else -> {
+                }
+            }
+        }
         mViewModel.gameListLiveData.observe(viewLifecycleOwner) {
             it.let { list ->
                 adapter.submitList(list)
