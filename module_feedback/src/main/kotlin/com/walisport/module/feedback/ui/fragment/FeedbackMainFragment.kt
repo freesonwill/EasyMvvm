11bec863b199e9d29e5996426183fae09f81df06
wenxi <russelllin465@gmail.com>
2025-12-13 11:55:15 +0700

    featute:反馈与建议页面图片选择后返回

--- module_feedback/src/main/kotlin/com/walisport/module/feedback/ui/fragment/FeedbackMainFragment.kt
+++ module_feedback/src/main/kotlin/com/walisport/module/feedback/ui/fragment/FeedbackMainFragment.kt
@@ -1,16 +1,21 @@
 package com.walisport.module.feedback.ui.fragment
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Message
+import android.webkit.PermissionRequest
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebView
+import androidx.activity.result.contract.ActivityResultContracts
 import arch.cayenne.lib.base.data.constants.StatusBarMode
 import arch.cayenne.lib.base.data.model.StatusBarConfig
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
 import arch.cayenne.lib.base.ui.fragment.launch
+import arch.cayenne.lib.base.utils.ext.LogUtilsExt.logd
 import arch.cayenne.lib.common.data.constants.BizUrl
 import arch.cayenne.lib.common.data.manager.UserDataManager
 import arch.cayenne.lib.common.utils.ext.ResourceExt.getColor
@@ -21,6 +26,7 @@ import com.walisport.module.feedback.ui.viewmodel.FeedbackMainViewModel
 import org.koin.java.KoinJavaComponent.inject
 import kotlin.reflect.KClass
 
+
 /**
  * 反馈详情页
  */
@@ -30,6 +36,13 @@ class FeedbackMainFragment : BaseFragment<FeedbackMainViewModel, FragmentFeedbac
     override val vmClass: KClass<FeedbackMainViewModel> = FeedbackMainViewModel::class
 
     private val manager: UserDataManager by inject(UserDataManager::class.java)
+    private var fileChooserCallback: ValueCallback<Array<Uri>>? = null //图片选择后返回h5
+
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            fileChooserCallback?.onReceiveValue(arrayOf(Uri.parse(result.data?.dataString)))
+            fileChooserCallback = null;
+        } //监听图片选择
 
     override fun initView(savedInstanceState: Bundle?) {
         launch {
@@ -44,19 +57,19 @@ class FeedbackMainFragment : BaseFragment<FeedbackMainViewModel, FragmentFeedbac
             webViewClient = object :
                 WLSWebViewClient(this) {
                 override fun onFormResubmission(
-                    view: WebView? ,
-                    dontResend: Message? ,
+                    view: WebView?,
+                    dontResend: Message?,
                     resend: Message
                 ) {
                     super.onFormResubmission(view, dontResend, resend)
                     resend.sendToTarget()
                 }
 
-                override fun onPageStarted(view: WebView? , url: String? , favicon: Bitmap?) {
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
                     super.onPageStarted(view, url, favicon)
                 }
 
-                override fun onPageFinished(view: WebView , url: String?) {
+                override fun onPageFinished(view: WebView, url: String?) {
                     view.settings.apply {
                         blockNetworkImage = false
                         if (!loadsImagesAutomatically) {
@@ -71,14 +84,22 @@ class FeedbackMainFragment : BaseFragment<FeedbackMainViewModel, FragmentFeedbac
             requestFocus()
             webChromeClient = object : WebChromeClient() {
                 override fun onShowFileChooser(
-                    webView: WebView ,
-                    filePathCallback: ValueCallback<Array<Uri>> ,
+                    webView: WebView,
+                    filePathCallback: ValueCallback<Array<Uri>>,
                     fileChooserParams: FileChooserParams
                 ): Boolean {
+                    fileChooserCallback = filePathCallback
+                    chooseImages()
                     return true
                 }
 
-                override fun onProgressChanged(view: WebView , newProgress: Int) {
+                override fun onPermissionRequest(request: PermissionRequest?) {
+                    super.onPermissionRequest(request)
+                    "onPerimssionRequrest ${request?.resources}".logd("aaa")
+                    request?.grant(request.resources)
+                }
+
+                override fun onProgressChanged(view: WebView, newProgress: Int) {
                     super.onProgressChanged(view, newProgress)
                 }
             }
@@ -101,7 +122,7 @@ class FeedbackMainFragment : BaseFragment<FeedbackMainViewModel, FragmentFeedbac
     override fun onStart() {
         StatusBarConfig.statusBarType = StatusBarMode.DRAW_BEHIND()
         StatusBarConfig.statusBarDarkFont = false
-        setStatusBar(StatusBarConfig ,mBinding.root)
+        setStatusBar(StatusBarConfig, mBinding.root)
         super.onStart()
     }
 
@@ -121,4 +142,36 @@ class FeedbackMainFragment : BaseFragment<FeedbackMainViewModel, FragmentFeedbac
         mBinding.webView.destroy()
         super.onDestroy()
     }
+
+    /**
+     * 打开图片选择器
+     * */
+    private fun chooseImages(){
+        val mimeTypes = arrayOf("image/*")
+        val selectionIntent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            setType("image/*")
+            putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes)
+            // 重要：这些标志可以加快处理
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+            // 添加标志，告诉系统不要压缩
+            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false)
+            // 对于 Android 11+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                putExtra(Intent.EXTRA_LOCAL_ONLY, true)
+            }
+        }
+
+
+        val chooserIntent = Intent(Intent.ACTION_CHOOSER).apply {
+            putExtra(Intent.EXTRA_INTENT, selectionIntent)
+            // 添加标志避免系统处理
+            addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        }
+//                    startActivityForResult(chooserIntent, 0)
+        startForResult.launch(chooserIntent)
+    }
+
 }
