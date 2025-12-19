d969e3e0a2007770b877b9cfe4523bffcfd64c95
xiaoyang <xiaoyang@abc.com>
2025-12-19 15:34:21 +0700

    feature:充值提现模块中"充值教程"、"提现教程"H5页面的Native支持 - https://linear.app/forgames/issue/MD-4358

--- lib_common/src/main/java/arch/cayenne/lib/common/data/constants/BizUrl.kt
+++ lib_common/src/main/java/arch/cayenne/lib/common/data/constants/BizUrl.kt
@@ -7,23 +7,21 @@ import arch.cayenne.lib.base.BuildConfig
  * @date: 2025/12/10 16:44
  * @description:
  */
+
 enum class BizUrl(val url: String) {
-    FUND_DETAIL("${BASE_URL}/web-3n1/record") ,
-    FEEDBACK("${BASE_URL}/web-3n1/feedback") ,
-
-//    GAME_BET_SHARE("http://192.168.10.38:5173/orderDetail?settleId=s3&name=南京红姐&level=90"),
-    GAME_BET_SHARE("${BASE_URL}/web-3n1/orderDetail?settleId=s3&name=南京红姐&level=90") ,
-
-    REBATE("${BASE_URL}/web-3n1/rebate") ,
-//    http://192.168.10.38:5173/
-//    INVITE("http://192.168.10.38:5173/invite") ,
-
-    INVITE("${BASE_URL}/web-3n1/invite") ,
-    PARTNER("${BASE_URL}/web-3n1/partner") ,
-    VIP("${BASE_URL}/web-3n1/vip") ,
-    HELP("https://www.ve657.com/help-docs/help/") ,//暂时用这个地址，Web端要重新做帮助
-    TOP_LESSON("${BASE_URL}/web-3n1/help?url=/wallet/tutorial/"), //充值教程
-    ACTIVITY("${BASE_URL}/web-3n1/activity") ,//活动页面
+    FUND_DETAIL("${BASE_URL}/web-3n1/record"),
+    FEEDBACK("${BASE_URL}/web-3n1/feedback"),
+    //GAME_BET_SHARE("http://192.168.10.38:5173/orderDetail?settleId=s3&name=南京红姐&level=90"),
+    GAME_BET_SHARE("${BASE_URL}/web-3n1/orderDetail?settleId=s3&name=南京红姐&level=90"),
+    REBATE("${BASE_URL}/web-3n1/rebate"),
+    //INVITE("http://192.168.10.38:5173/invite") ,
+    INVITE("${BASE_URL}/web-3n1/invite"),
+    PARTNER("${BASE_URL}/web-3n1/partner"),
+    VIP("${BASE_URL}/web-3n1/vip"),
+    HELP("https://www.ve657.com/help-docs/help/"),//暂时用这个地址，Web端要重新做帮助
+    TOP_LESSON("${BASE_URL}/web-help/zh-CN/wallet/tutorial/?backUrl=/web-help/zh-CN/wallet/tutorial/"), //充值教程
+    WITHDRAW_LESSON("${BASE_URL}/web-help/zh-CN/wallet/withdraw/?backUrl=/web-help/zh-CN/wallet/withdraw/"), //提现教程
+    ACTIVITY("${BASE_URL}/web-3n1/activity"),//活动页面
 
 
 }
@@ -34,12 +32,14 @@ val BASE_URL
             "qatest" -> {
                 "https://test.ra781.com"
             }
+
             "release" -> {
                 "https://test.ra781.com"
             }
+
             else -> {
                 "https://dev.ra781.com"
-//                "http://192.168.10.38:5173"
+                //"http://192.168.10.38:5173"
             }
         }
     }
