ee64ac6ec7e95ffdec2cac178a8b0a86677e67ce
zhangsan <zhangsan@iptco.me>
2025-12-21 22:25:05 +0700

    代码提交

--- module_hall/src/main/java/com/walisport/module/hall/ui/adapter/GameAllListInnerAdapter.kt
+++ module_hall/src/main/java/com/walisport/module/hall/ui/adapter/GameAllListInnerAdapter.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import arch.cayenne.lib.base.ui.adapter.BaseAdapter
 import arch.cayenne.lib.base.ui.adapter.BaseViewHolder
+import arch.cayenne.lib.base.utils.LogUtils
 import arch.cayenne.lib.common.utils.ThumbHashUtils
+import arch.cayenne.lib.common.utils.ext.DimensionExt.dp2px
 import arch.cayenne.lib.common.utils.ext.clickNoRepeat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
@@ -19,10 +21,7 @@ class GameAllListInnerAdapter(private val onItemClickListener: (()->Unit)?) : Ba
         binding: ItemGameAllListInnerBinding,
         position: Int
     ) {
-        holder.bind(getItem(position))
-        if (position==0){
-             binding.rvRoot.setPadding(0,binding.rvRoot.paddingTop,binding.rvRoot.paddingRight,binding.rvRoot.paddingEnd)
-        }
+        holder.bind(getItem(position),position)
     }
 
     override fun createViewBinding(
@@ -40,9 +39,24 @@ class GameAllListInnerAdapter(private val onItemClickListener: (()->Unit)?) : Ba
         return GameListInnerViewHolder(binding,onItemClickListener)
     }
 }
-
 class GameListInnerViewHolder(val item: ItemGameAllListInnerBinding,private val onItemClickListener: (()->Unit)?): BaseViewHolder(item) {
-    fun bind(data: GameContentData) {
+    fun bind(data: GameContentData,position: Int) {
+        if (position==0){
+            LogUtils.e("GameListInnerViewHolder----------${position}")
+            item.rvRoot.layoutParams = item.rvRoot.layoutParams.apply {
+                width =124.dp2px
+                height = ViewGroup.LayoutParams.WRAP_CONTENT
+            }
+            item.rvRoot.setPadding(11.dp2px,item.rvRoot.paddingTop,7.dp2px,
+                item.rvRoot.paddingBottom            )
+        }else{
+            item.rvRoot.layoutParams = item.rvRoot.layoutParams.apply {
+                width =112.dp2px
+                height = ViewGroup.LayoutParams.WRAP_CONTENT
+            }
+            item.rvRoot.setPadding(0.dp2px,item.rvRoot.paddingTop,7.dp2px,
+                item.rvRoot.paddingBottom            )
+        }
         val thumbBitmap = ThumbHashUtils.getBitmapFromThumbHash(data.avatar.thumbhash)  // 你之前写好的函数，返回 Bitmap?
         thumbBitmap?.let { bitmap ->
             val placeholderDrawable = BitmapDrawable(item.root.context.resources, bitmap)
