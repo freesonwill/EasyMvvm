54281028e26bb53d4280c1e96193f4cb4cb4a708
zhangsan <zhangsan@iptco.me>
2025-12-22 22:12:09 +0700

    feat: call shareLogFile on ctTopBar long press

--- module_me/src/main/java/com/walisport/module/me/ui/fragment/MeFragment.kt
+++ module_me/src/main/java/com/walisport/module/me/ui/fragment/MeFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.viewModels
 import arch.cayenne.lib.base.data.constants.StatusBarMode
 import arch.cayenne.lib.base.data.model.StatusBarConfig
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
+import arch.cayenne.lib.base.ui.fragment.launch
+import arch.cayenne.lib.base.utils.ext.launch
+import arch.cayenne.lib.base.utils.log.Utils
 import arch.cayenne.lib.common.data.constants.DrawerAction.ACTION_OPEN
 import arch.cayenne.lib.common.data.constants.DrawerAction.KEY_ACTION
 import arch.cayenne.lib.common.data.constants.DrawerAction.REQUEST_KEY_DRAWER
@@ -97,6 +100,10 @@ class MeFragment : BaseFragment<MeViewModel, FragmentMeBinding>() {
             ivSetting.addScaleOnTouchAnimation()
             ivSetting.clickNoRepeat {
                 navigate(arch.cayenne.lib.res.R.string.nav_module_setting_fragment.deeplink())
+            }
+            ctTopBar.setOnLongClickListener {
+                launch { Utils.shareLogFile(requireContext()) }
+                true
             }
 
             ctUserInfo.clickNoRepeat {
