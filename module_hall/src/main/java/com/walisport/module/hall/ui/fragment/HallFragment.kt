cc1051fb2e9c41a343413ee6ae7602943fd0b0c5
wangzai <dafeix64@gmail.com>
2025-12-22 12:57:29 +0700

    保护代码

--- module_hall/src/main/java/com/walisport/module/hall/ui/fragment/HallFragment.kt
+++ module_hall/src/main/java/com/walisport/module/hall/ui/fragment/HallFragment.kt
@@ -291,10 +291,10 @@ class HallFragment : BaseFragment<HallViewModel, FragmentHallBinding>() {
                             arch.cayenne.lib.common.R.color.white
                         )
                     )
-                    textView.setRoundedBackground(
-                        backgroundColor = tabList[tab.position].colorRes,
-                        show = true
-                    )
+//                    textView.setRoundedBackground(
+//                        backgroundColor = tabList[tab.position].colorRes,
+//                        show = true
+//                    )
                     textView.typeface = Typeface.DEFAULT_BOLD
                 }
             }
