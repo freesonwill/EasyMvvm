0964ba938a74d9354125cbd575eec9bc9a3f59a0
wangzai <wangzai@iptco.me>
2025-12-26 21:43:49 +0700

    feature. https://gitlab.iptco.me/tio/wip/-/issues/174 收藏列表

--- module_business_common/src/main/java/com/walisport/module/business/common/data/IGameFavouriteApi.kt
+++ module_business_common/src/main/java/com/walisport/module/business/common/data/IGameFavouriteApi.kt
@@ -3,7 +3,9 @@ package com.walisport.module.business.common.data
 import arch.cayenne.lib.http._interface.IApi
 import arch.cayenne.lib.http.data.HttpApiResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 /**
@@ -15,7 +17,15 @@ interface IGameFavouriteApi : IApi {
 
     @GET("api/game/collect")
     suspend fun getGameCollectList(
-        @Query("page") page: Int ,
+        @Query("page") page: Int,
         @Query("pageSize") pageSize: Int
     ): Response<HttpApiResponse<ProfilePlayedPageVo>>
+
+
+    // 更新游戏收藏状态
+    //important: 和拉取收藏列表是同一个url, 但是请求方式是POST, f**k
+    @POST("api/game/collect")
+    suspend fun updateGameCollect(
+        @Body body: ProfileCollectEditVo
+    ): Response<HttpApiResponse<String>>
 }
