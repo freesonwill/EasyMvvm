8c737463a3d2de6ef25396cef2a84566d3b63350
xiaoyang <xiaoyang@iptco.me>
2025-12-28 23:01:37 +0700

    feature:升级所需投注额巨额金额应予以简写 - https://linear.app/forgames/issue/MD-4420

--- lib_common/src/main/java/arch/cayenne/lib/common/data/constants/CurrencySymbols.kt
+++ lib_common/src/main/java/arch/cayenne/lib/common/data/constants/CurrencySymbols.kt
@@ -1,5 +1,6 @@
 package arch.cayenne.lib.common.data.constants
 
+import android.annotation.SuppressLint
 import arch.cayenne.lib.common.R
 
 /**
@@ -27,6 +28,34 @@ object CurrencySymbols {
             "BTC" -> R.drawable.ic_btc
             "ETH" -> R.drawable.ic_eth
             else -> null
+        }
+    }
+
+    @SuppressLint("DefaultLocale")
+    fun getFormatAmount(currency: String, amount: Float): String {
+        return when (currency) {
+            "CNY" -> getFormatCNY(amount)
+            "USD" -> getFormatUSD(amount)
+            else -> String.format("%.2f", amount)
+        }
+    }
+
+    @SuppressLint("DefaultLocale")
+    fun getFormatUSD(amount: Float): String {
+        return when {
+            amount >= 1_000_000_000 -> String.format("%.2fB", amount / 1_000_000_000f)
+            amount >= 1_000_000 -> String.format("%.2fM", amount / 1_000_000f)
+            amount >= 1_000 -> String.format("%.2fK", amount / 1_000f)
+            else -> String.format("%.2f", amount)
+        }
+    }
+
+    @SuppressLint("DefaultLocale")
+    fun getFormatCNY(amount: Float): String {
+        return when {
+            amount >= 100_000_000 -> String.format("%.2f亿元", amount / 100_000_000)
+            amount >= 10_000 -> String.format("%.2f万元", amount / 10_000)
+            else -> String.format("%.2f元", amount)
         }
     }
 }
