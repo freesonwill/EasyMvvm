330c382adfd4616746c2394182aeb587afa25b02
wangzai <wangzai@iptco.me>
2025-12-28 15:59:54 +0700

    用户在游戏详情页点击透明色 icon, icon 立即变为填充色, 立即 toast 提示：收藏成功

--- module_game_detail/src/main/java/com/walisport/module/gamedetail/ui/fragment/GameDetailFragment.kt
+++ module_game_detail/src/main/java/com/walisport/module/gamedetail/ui/fragment/GameDetailFragment.kt
@@ -12,6 +12,7 @@ import arch.cayenne.lib.base.ui.fragment.BaseFragment
 import arch.cayenne.lib.common.ui.viewmodel.BalanceViewModel
 import arch.cayenne.lib.common.utils.ext.NavigationExt.navigateUp
 import arch.cayenne.lib.common.utils.ext.clickNoRepeat
+import arch.cayenne.lib.common.utils.helper.showToast
 import com.walisport.module.gamedetail.databinding.FragmentGameDetailBinding
 import com.walisport.module.gamedetail.ui.viewmodel.GameDetailPageViewModel
 import kotlinx.coroutines.launch
@@ -67,6 +68,9 @@ class GameDetailFragment : BaseFragment<GameDetailPageViewModel, FragmentGameDet
                 navigateUp()
             }
             ivFavorite.clickNoRepeat {
+                if (mViewModel.isCollected.value != true) {
+                    showToast("收藏成功")
+                }
                 mViewModel.toggleCollectStatus(args.gameId)
 
                 lifecycleScope.launch {
