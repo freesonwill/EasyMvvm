66f41f379ef762c33c560c673b67808d95a53ee8
wangzai <wangzai@iptco.me>
2025-12-28 16:51:33 +0700

    feature. https://gitlab.iptco.me/tio/wip/-/issues/174 【Android】游戏-收藏游戏

--- module_hall/src/main/java/com/walisport/module/hall/ui/fragment/GameFavouriteFragment.kt
+++ module_hall/src/main/java/com/walisport/module/hall/ui/fragment/GameFavouriteFragment.kt
@@ -47,10 +47,11 @@ class GameFavouriteFragment : BaseFragment<GameFavouriteViewModel, FragmentGameF
         GridSpacingItemDecoration(
             spanCount = 3,
             horizontalSpacing = 9.dp2px,
-            verticalSpacing = 12.dp2px,
+            verticalSpacing = 17.dp2px,
             includeEdge = false // 確保邊緣沒有空隙
         )
     }
+
 
     override fun initView(savedInstanceState: Bundle?) {
         with(mBinding) {
