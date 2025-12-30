b4c04294d5eeb6ca1e30fa2e988f3701183ea290
wangzai <wangzai@iptco.me>
2025-12-30 22:29:37 +0700

    feature. https://gitlab.iptco.me/tio/wip/-/issues/219 点击小弹窗广告位，跳转站外URL链接

--- module_popup_slot/src/main/java/com/walisport/module/popup/slot/ui/fragment/PopupSlotFragment.kt
+++ module_popup_slot/src/main/java/com/walisport/module/popup/slot/ui/fragment/PopupSlotFragment.kt
@@ -12,6 +12,7 @@ import arch.cayenne.lib.common.utils.ext.DimensionExt.dp2px
 import arch.cayenne.lib.common.utils.ext.clickNoRepeat
 import arch.cayenne.lib.common.utils.ext.startSafeAnimateSet
 import com.walisport.module.popup.slot.R
+import com.walisport.module.popup.slot.data.PopupSlotDataModel
 import com.walisport.module.popup.slot.databinding.FragmentPopupSlotBinding
 import com.walisport.module.popup.slot.ui.adapter.BannerUrlImageAdapter
 import com.walisport.module.popup.slot.ui.viewmodel.PopUpSlotViewModel
@@ -127,12 +128,18 @@ class PopupSlotFragment :
                     isAutoLoop(true)
                     setOnBannerListener { data, position ->
                         // 这里处理点击事件，比如：
-//                    val url =
-//                        ((data as Pair<PopupSlotDataModel, Int>).first as PopupSlotDataModel).operateParams[0]
+                        val url =
+                            ((data as Pair<PopupSlotDataModel, Int>).first as PopupSlotDataModel).operateParams[0]
 //                    navigate(
 //                        arch.cayenne.lib.res.R.string.nav_module_web_fragment
 //                            .deeplink("url" to url)
 //                    )
+
+                        val intent = android.content.Intent(
+                            android.content.Intent.ACTION_VIEW,
+                            android.net.Uri.parse(url)
+                        )
+                        startActivity(intent)
                     }
                     start()
 
@@ -151,12 +158,18 @@ class PopupSlotFragment :
                     isAutoLoop(true)
                     setOnBannerListener { data, position ->
                         // 这里处理点击事件，比如：
-//                    val url =
-//                        ((data as Pair<PopupSlotDataModel, Int>).first as PopupSlotDataModel).operateParams[0]
+                        val url =
+                            ((data as Pair<PopupSlotDataModel, Int>).first as PopupSlotDataModel).operateParams[0]
 //                    navigate(
 //                        arch.cayenne.lib.res.R.string.nav_module_web_fragment
 //                            .deeplink("url" to url)
 //                    )
+
+                        val intent = android.content.Intent(
+                            android.content.Intent.ACTION_VIEW,
+                            android.net.Uri.parse(url)
+                        )
+                        startActivity(intent)
                     }
                     start()
                 }
