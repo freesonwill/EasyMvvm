8bc8ee06e78826c9c0251101fc89a5b1c494791c
aquan <aquan@iptco.me>
2026-01-05 17:33:55 +0700

    bugfix：【Android】 用户资料编辑页 點擊更換頭像後，還可繼續點擊其他系統頭像

--- module_account/src/main/java/arch/cayenne/module/account/ui/adapter/PersonalInfoAdapter.kt
+++ module_account/src/main/java/arch/cayenne/module/account/ui/adapter/PersonalInfoAdapter.kt
@@ -15,29 +15,42 @@ import arch.cayenne.module.account.ui.adapter.compare.PersonalInfoCompare
  * @date: 2025/6/10 下午2:35
  * @description:
  */
-class PersonalInfoAdapter: BaseAdapter<SystemAvatarBean, PersonalInfoItemViewHolder, ItemPersonalInfoBinding>(
-    PersonalInfoCompare()
-) {
-    private var selectedPosition : Int = -1
+class PersonalInfoAdapter :
+    BaseAdapter<SystemAvatarBean, PersonalInfoItemViewHolder, ItemPersonalInfoBinding>(
+        PersonalInfoCompare()
+    ) {
+    private var isUpAvatar = true
+    private var selectedPosition: Int = -1
     private var onItemClickListener: ((SystemAvatarBean) -> Unit)? = null
-    override fun convertPlus(holder: PersonalInfoItemViewHolder, binding: ItemPersonalInfoBinding, position: Int) {
+    override fun convertPlus(
+        holder: PersonalInfoItemViewHolder,
+        binding: ItemPersonalInfoBinding,
+        position: Int
+    ) {
+
         if (getItem(position).id == selectedPosition) {
             binding.ivPersonalHeadSelected.visibility = View.VISIBLE
         } else {
             binding.ivPersonalHeadSelected.visibility = View.INVISIBLE
         }
         holder.bind(getItem(position)) { data ->
-            // 2. 在 onBindViewHolder 中更新 UI
-            // 檢查點擊的項目是否是已選中的項目
-            if (selectedPosition == data.id) {
-                return@bind
+            if (isUpAvatar) {
+                // 2. 在 onBindViewHolder 中更新 UI
+                // 檢查點擊的項目是否是已選中的項目
+                if (selectedPosition == data.id) {
+                    return@bind
+                }
+                // 4. 在點擊事件中更新狀態
+                val oldPosition = selectedPosition
+                selectedPosition = data.id
+                notifyDataSetChanged()
+                onItemClickListener?.invoke(data)
             }
-            // 4. 在點擊事件中更新狀態
-            val oldPosition = selectedPosition
-            selectedPosition = data.id
-           notifyDataSetChanged()
-            onItemClickListener?.invoke(data)
         }
+    }
+
+    fun setIsUpAvatar(isUpAvatar: Boolean) {
+        this.isUpAvatar = isUpAvatar
     }
 
     override fun createViewBinding(
@@ -48,9 +61,13 @@ class PersonalInfoAdapter: BaseAdapter<SystemAvatarBean, PersonalInfoItemViewHol
         return ItemPersonalInfoBinding.inflate(inflater, parent, false)
     }
 
-    override fun createViewHolder(binding: ItemPersonalInfoBinding, viewType: Int): PersonalInfoItemViewHolder {
+    override fun createViewHolder(
+        binding: ItemPersonalInfoBinding,
+        viewType: Int
+    ): PersonalInfoItemViewHolder {
         return PersonalInfoItemViewHolder(binding)
     }
+
     fun getSelectedResId(): Int {
         return if (selectedPosition == -1) {
             0
@@ -58,9 +75,11 @@ class PersonalInfoAdapter: BaseAdapter<SystemAvatarBean, PersonalInfoItemViewHol
             getItem(selectedPosition).id
         }
     }
+
     fun getSelectedPosition(): Int {
         return selectedPosition
     }
+
     fun setSelectedPosition(position: Int) {
         if (position < 0 || position >= itemCount) {
             return
@@ -70,6 +89,7 @@ class PersonalInfoAdapter: BaseAdapter<SystemAvatarBean, PersonalInfoItemViewHol
         notifyItemChanged(oldPosition) // 通知舊的項目更新 UI (取消選中狀態)
         notifyItemChanged(selectedPosition) // 通知新的項目更新 UI (變為選中狀態)
     }
+
     fun setOnItemClickListener(listener: (SystemAvatarBean) -> Unit) {
         onItemClickListener = listener
     }
