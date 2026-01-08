47610b1935d93de4ff9ac533edbbf18bd721c624
wangzai <wangzai@iptco.me>
2026-01-08 16:33:33 +0700

    feature. 登录和注册页面

--- module_account/src/main/java/arch/cayenne/module/account/ui/fragment/EmailFragment.kt
+++ module_account/src/main/java/arch/cayenne/module/account/ui/fragment/EmailFragment.kt
@@ -2,16 +2,18 @@ package arch.cayenne.module.account.ui.fragment
 
 import android.os.Bundle
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
+import arch.cayenne.module.account.databinding.FragmentEmailBinding
 import arch.cayenne.module.account.databinding.FragmentPhoneBinding
+import arch.cayenne.module.account.ui.viewmodel.EmailViewModel
 import arch.cayenne.module.account.ui.viewmodel.PhoneViewModel
 import arch.cayenne.module.account.ui.viewmodel.SystemAvatarViewModel
 import kotlin.reflect.KClass
 
 class EmailFragment :
-    BaseFragment<PhoneViewModel, FragmentPhoneBinding>() {
-    override val vbClass: KClass<FragmentPhoneBinding> =
-        FragmentPhoneBinding::class
-    override val vmClass: KClass<PhoneViewModel> = PhoneViewModel::class
+    BaseFragment<EmailViewModel, FragmentEmailBinding>() {
+    override val vbClass: KClass<FragmentEmailBinding> =
+        FragmentEmailBinding::class
+    override val vmClass: KClass<EmailViewModel> = EmailViewModel::class
 
 
     override fun initView(savedInstanceState: Bundle?) {
