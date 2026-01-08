b591afee3f52ed695222b01b79080d1160faf791
wangzai <wangzai@iptco.me>
2026-01-08 23:29:08 +0700

    feature. 登录和注册页面

--- module_account/src/main/java/arch/cayenne/module/account/ui/fragment/PhoneNumberFragment.kt
+++ module_account/src/main/java/arch/cayenne/module/account/ui/fragment/PhoneNumberFragment.kt
@@ -2,9 +2,9 @@ package arch.cayenne.module.account.ui.fragment
 
 import android.os.Bundle
 import arch.cayenne.lib.base.ui.fragment.BaseFragment
+import arch.cayenne.lib.common.utils.ext.clickNoRepeat
 import arch.cayenne.module.account.databinding.FragmentPhoneBinding
 import arch.cayenne.module.account.ui.viewmodel.PhoneViewModel
-import arch.cayenne.module.account.ui.viewmodel.SystemAvatarViewModel
 import kotlin.reflect.KClass
 
 class PhoneNumberFragment :
@@ -15,6 +15,9 @@ class PhoneNumberFragment :
 
 
     override fun initView(savedInstanceState: Bundle?) {
+        with(mBinding) {
+            tvCountryCode.text = "+86" //先做*86
+        }
 
     }
 
@@ -23,7 +26,9 @@ class PhoneNumberFragment :
     }
 
     override fun initListener() {
-
+        mBinding.llWrapper.clickNoRepeat {
+            //TODO 选择国家地区
+        }
 
     }
 
