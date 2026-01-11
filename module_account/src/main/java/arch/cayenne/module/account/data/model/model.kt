ba2a0ad28068be3ae78891f56fb62532a5145814
wangzai <wangzai@iptco.me>
2026-01-11 21:13:50 +0700

    bugfix

--- module_account/src/main/java/arch/cayenne/module/account/data/model/model.kt
+++ module_account/src/main/java/arch/cayenne/module/account/data/model/model.kt
@@ -5,16 +5,19 @@ package arch.cayenne.module.account.data.model
  * @date: 2026/1/9 11:15
  * @description:
  */
+/**
+ * 用户登录/注册请求
+ */
 data class LoginVo(
-    val countryCode: String, // 区号
-    val nationalNumber: String, // 国家号码
+    val countryCode: String, // 区号，例如: 86
+    val nationalNumber: String, // 国家号码，例如: 18812345678
     val pwd: String, // 密码
-    val mobileType: String, // 设备型号
+    val mobileType: String, // 设备型号，例如: iPhone17,1
     val osVer: String, // 操作系统版本
     val jb: Boolean, // 是否越狱
-    val sms: String? = null, // 手机验证码
-    val clientId: String? = null, // 第三方账号平台账号
-    val authSecret: String? = null // 第三方账号平台token
+    val sms: String, // 手机验证码
+    val clientId: String, // 第三方账号平台账号
+    val authSecret: String // 第三方账号平台token
 )
 
 
